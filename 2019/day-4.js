a489796ef45842f84a24cb268ddf33f02bca0d6d
leyanlo <leyanlo@gmail.com>
2020-12-04 00:17:43 -0800

    feat(2019): day 4

---
+++ 2019/day-4.js
@@ -0,0 +1,49 @@
+function process1(input) {
+  const [min, max] = input;
+  let count = 0;
+  for (let i = min; i <= max; i++) {
+    const digits = ("" + i).split("").map(s => +s);
+    let hasAdjacent = false;
+    let neverDecrease = true;
+    let digit = digits[0];
+    for (let j = 1; j < digits.length; j++) {
+      const nextDigit = digits[j];
+      if (nextDigit === digit) hasAdjacent = true;
+      if (nextDigit < digit) neverDecrease = false;
+      digit = nextDigit;
+    }
+    if (hasAdjacent && neverDecrease) count++;
+  }
+  console.log(count);
+}
+
+function process(input) {
+  const [min, max] = input;
+  let count = 0;
+  for (let i = min; i <= max; i++) {
+    const digits = ("" + i).split("").map(s => +s);
+    let hasAdjacent = false;
+    let neverDecrease = true;
+    let digit = digits[0];
+    let adjacentCount = 1;
+    for (let j = 1; j < digits.length; j++) {
+      const nextDigit = digits[j];
+      if (nextDigit === digit) {
+        adjacentCount++;
+      } else {
+        if (adjacentCount === 2) {
+          hasAdjacent = true;
+        }
+        adjacentCount = 1;
+      }
+      if (nextDigit < digit) neverDecrease = false;
+      digit = nextDigit;
+    }
+    if ((hasAdjacent || adjacentCount === 2) && neverDecrease) count++;
+  }
+  console.log(count);
+}
+
+const input = `254032-789860`.split("-").map(s => +s);
+
+process(input);
