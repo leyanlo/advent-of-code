ba16a8b04ef8c1e153222b41fed42ccd8cbe7401
Leyan Lo <leyanlo@gmail.com>
2020-12-05 00:04:16 -0800

    feat(2019): finish day 7

--- 2019/day-7.js
+++ 2019/day-7.js
@@ -1,12 +1,12 @@
-import * as $C from "js-combinatorics";
+require = require("esm")(module);
+const $C = require("js-combinatorics");
 
 function getParamsAndOpcode(n) {
   const paddedN = ("" + n).padStart(5, "0");
   return [+paddedN[0], +paddedN[1], +paddedN[2], +paddedN.substr(3)];
 }
 
-function intcode(firstInput, secondInput, arr) {
-  let i = 0;
+function intcode(firstInput, secondInput, arr, i = 0) {
   let [mode3, mode2, mode1, opcode] = getParamsAndOpcode(arr[i]);
   let inputCount = 0;
   let output = null;
@@ -40,8 +40,8 @@ function intcode(firstInput, secondInput, arr) {
     } else if (opcode === 4) {
       const position = mode1 ? i + 1 : arr[i + 1];
       output = arr[position];
-      return output;
       i += 2;
+      return [output, i];
     } else if (opcode === 5) {
       const first = mode1 ? arr[i + 1] : arr[arr[i + 1]];
       const second = mode2 ? arr[i + 2] : arr[arr[i + 2]];
@@ -68,24 +68,52 @@ function intcode(firstInput, secondInput, arr) {
     }
     [mode3, mode2, mode1, opcode] = getParamsAndOpcode(arr[i]);
   }
-  return output;
+  return [output, -1];
 }
 
-function process(puzzleInput) {
-  const permutations = [...new $C.Permutation("01234")].map(arr => {
-    return arr.map(s => +s);
+function process1(puzzleInput) {
+  const permutations = [...new $C.Permutation("01234")].map((arr) => {
+    return arr.map((s) => +s);
   });
   const maxSignal = permutations.reduce((maxSignal, permutation) => {
     const signal = permutation.reduce((signal, phaseSetting) => {
-      return intcode(phaseSetting, signal, puzzleInput);
+      const [output] = intcode(phaseSetting, signal, puzzleInput);
+      return output;
     }, 0);
     return Math.max(signal, maxSignal);
   }, 0);
   console.log(maxSignal);
 }
 
+function process(puzzleInput) {
+  const permutations = [...new $C.Permutation("56789")].map((arr) => {
+    return arr.map((s) => +s);
+  });
+  const maxSignal = permutations.reduce((maxSignal, permutation) => {
+    let i = 0;
+    let signal = 0;
+    const puzzleIndices = [...Array(5)].map(() => 0);
+    while (true) {
+      const [nextSignal, puzzleIndex] = intcode(
+        permutation[i] || signal,
+        signal,
+        puzzleInput,
+        puzzleIndices[i % 5]
+      );
+      if (nextSignal === null) {
+        break;
+      }
+      signal = nextSignal;
+      puzzleIndices[i % 5] = puzzleIndex;
+      i++;
+    }
+    return Math.max(maxSignal, signal);
+  }, 0);
+  console.log(maxSignal);
+}
+
 const puzzleInput = `3,8,1001,8,10,8,105,1,0,0,21,42,67,88,101,114,195,276,357,438,99999,3,9,101,3,9,9,1002,9,4,9,1001,9,5,9,102,4,9,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,101,2,9,9,102,2,9,9,1001,9,5,9,4,9,99,3,9,102,4,9,9,1001,9,3,9,102,4,9,9,101,4,9,9,4,9,99,3,9,101,2,9,9,1002,9,3,9,4,9,99,3,9,101,4,9,9,1002,9,5,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,99`
   .split(",")
-  .map(s => +s);
+  .map((s) => +s);
 
 process(puzzleInput);
