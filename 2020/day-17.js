77e45c6d4e9b80f8052f335a895857fde4898a7f
Leyan Lo <leyan@uber.com>
2020-12-17 22:50:53 -0800

    chore(2020): day 17 code cleanup
    Rename prev -> curr

--- 2020/day-17.js
+++ 2020/day-17.js
@@ -115,46 +115,46 @@ const dirs4 = [
 ];
 
 function solve1(input) {
-  let prev = [
+  let curr = [
     input.split('\n').map((line) => line.split('').map((char) => char === '#')),
   ];
 
   for (let t = 0; t < 6; t++) {
     const next = [];
-    for (let z = -1; z < prev.length + 1; z++) {
-      for (let y = -1; y < prev[0].length + 1; y++) {
-        for (let x = -1; x < prev[0][0].length + 1; x++) {
+    for (let z = -1; z < curr.length + 1; z++) {
+      for (let y = -1; y < curr[0].length + 1; y++) {
+        for (let x = -1; x < curr[0][0].length + 1; x++) {
           const count = dirs3.reduce((count, [dz, dy, dx]) => {
             return (
               count +
               !!(
-                prev[z + dz] &&
-                prev[z + dz][y + dy] &&
-                prev[z + dz][y + dy][x + dx]
+                curr[z + dz] &&
+                curr[z + dz][y + dy] &&
+                curr[z + dz][y + dy][x + dx]
               )
             );
           }, 0);
           next[z + 1] = next[z + 1] || [];
           next[z + 1][y + 1] = next[z + 1][y + 1] || [];
           next[z + 1][y + 1][x + 1] =
-            prev[z] && prev[z][y] && prev[z][y][x]
+            curr[z] && curr[z][y] && curr[z][y][x]
               ? count === 2 || count === 3
               : count === 3;
         }
       }
     }
-    prev = next;
+    curr = next;
   }
 
   console.log(
-    prev
+    curr
       .flat(2)
       .reduce((count, active) => count + active, 0)
   );
 }
 
 function solve2(input) {
-  let prev = [
+  let curr = [
     [
       input
         .split('\n')
@@ -164,18 +164,18 @@ function solve2(input) {
 
   for (let t = 0; t < 6; t++) {
     const next = [];
-    for (let w = -1; w < prev.length + 1; w++) {
-      for (let z = -1; z < prev[0].length + 1; z++) {
-        for (let y = -1; y < prev[0][0].length + 1; y++) {
-          for (let x = -1; x < prev[0][0][0].length + 1; x++) {
+    for (let w = -1; w < curr.length + 1; w++) {
+      for (let z = -1; z < curr[0].length + 1; z++) {
+        for (let y = -1; y < curr[0][0].length + 1; y++) {
+          for (let x = -1; x < curr[0][0][0].length + 1; x++) {
             const count = dirs4.reduce((count, [dw, dz, dy, dx]) => {
               return (
                 count +
                 !!(
-                  prev[w + dw] &&
-                  prev[w + dw][z + dz] &&
-                  prev[w + dw][z + dz][y + dy] &&
-                  prev[w + dw][z + dz][y + dy][x + dx]
+                  curr[w + dw] &&
+                  curr[w + dw][z + dz] &&
+                  curr[w + dw][z + dz][y + dy] &&
+                  curr[w + dw][z + dz][y + dy][x + dx]
                 )
               );
             }, 0);
@@ -183,18 +183,18 @@ function solve2(input) {
             next[w + 1][z + 1] = next[w + 1][z + 1] || [];
             next[w + 1][z + 1][y + 1] = next[w + 1][z + 1][y + 1] || [];
             next[w + 1][z + 1][y + 1][x + 1] =
-              prev[w] && prev[w][z] && prev[w][z][y] && prev[w][z][y][x]
+              curr[w] && curr[w][z] && curr[w][z][y] && curr[w][z][y][x]
                 ? count === 2 || count === 3
                 : count === 3;
           }
         }
       }
     }
-    prev = next;
+    curr = next;
   }
 
   console.log(
-    prev
+    curr
       .flat(3)
       .reduce((count, active) => count + active, 0)
   );
