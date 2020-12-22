22ab0539f5e312f81f524cf6d7e1dd277d52c62e
Leyan Lo <leyanlo@gmail.com>
2020-12-21 23:54:39 -0800

    chore(2020): day 22 code cleanup

--- 2020/day-22.js
+++ 2020/day-22.js
@@ -4,79 +4,79 @@ const part1 = true;
 const part2 = true;
 
 function solve1(input) {
-  input = input.split('\n\n');
-  const decks = input.map((player) => player.split('\n').slice(1).map(Number));
-  debug && console.log({ decks });
+  const decks = input
+    .split('\n\n')
+    .map((player) => player.split('\n').slice(1).map(Number));
 
+  let round = 0;
   while (decks.every((deck) => deck.length)) {
+    round++;
+    debug && console.log({ round, decks });
+
     const tops = decks.map((deck) => deck.shift());
-    debug && console.log({ tops, decks });
 
     const winner = +(tops[1] > tops[0]);
     tops.sort((a, b) => b - a);
     decks[winner].push(...tops);
-    debug && console.log({ decks });
   }
 
   const score = decks.reduce((score, deck) => {
     return (
       score +
-      deck.reduce((s, card, i) => {
-        return s + card * (deck.length - i);
+      deck.reduce((score, card, i) => {
+        return score + card * (deck.length - i);
       }, 0)
     );
   }, 0);
   console.log({ score });
 }
 
-function getFinalDecks(decks, round, game) {
+let nGames = 0;
+
+function getFinalDecks(decks) {
   const memo = {};
-  let winner;
+  let round = 0;
+  const game = ++nGames;
   while (decks.every((deck) => deck.length)) {
+    round++;
     debug && console.log({ round, game, decks });
     const memoKey = JSON.stringify(decks);
     if (memo[memoKey]) {
-      decks = [[1], []];
-      break;
+      // force player 1 win
+      return [[1], []];
     }
 
     const tops = decks.map((deck) => deck.shift());
 
-    winner = getRoundWinner(tops, decks, round, game);
+    const winner = getRoundWinner(tops, decks);
     memo[memoKey] = [...decks.map((deck) => [...deck])];
     decks[winner].push(tops[winner], tops[+!winner]);
-    round++;
   }
-  debug && console.log('Player', winner + 1, 'wins game', game);
   return decks;
 }
 
-function getRoundWinner(tops, decks, round, game) {
-  let winner;
+function getRoundWinner(tops, decks) {
   if (decks[0].length >= tops[0] && decks[1].length >= tops[1]) {
-    winner = getFinalDecks(
-      decks.map((deck, i) => deck.slice(0, tops[i])),
-      1,
-      game + 1
+    return getFinalDecks(
+      decks.map((deck, i) => deck.slice(0, tops[i]))
     ).findIndex((deck) => !!deck.length);
   } else {
-    winner = +(tops[1] > tops[0]);
+    return +(tops[1] > tops[0]);
   }
-
-  return winner;
 }
 
 function solve2(input) {
-  input = input.split('\n\n');
-  let decks = input.map((player) => player.split('\n').slice(1).map(Number));
+  let decks = input
+    .split('\n\n')
+    .map((player) => player.split('\n').slice(1).map(Number));
 
-  decks = getFinalDecks(decks, 1, 1);
+  decks = getFinalDecks(decks);
 
   const score = decks.reduce((score, deck) => {
     return (
       score +
-      deck.reduce((s, card, i) => {
-        return s + card * (deck.length - i);
+      deck.reduce((score, card, i) => {
+        return score + card * (deck.length - i);
       }, 0)
     );
   }, 0);
