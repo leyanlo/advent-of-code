69bd05f5db6cbbb376fb01bce5f19ed03766f965
Leyan Lo <leyanlo@gmail.com>
2020-12-22 23:39:24 -0800

    feat(2020): day 23 part 2

--- 2020/day-23.js
+++ 2020/day-23.js
@@ -1,7 +1,7 @@
 const inputIdx = 1;
 const debug = false;
 const part1 = true;
-const part2 = false;
+const part2 = true;
 
 function solve1(input) {
   const labels = input.split('').map(Number);
@@ -47,7 +47,39 @@ function solve1(input) {
   );
 }
 
-function solve2(input) {}
+function solve2(input) {
+  const labels = input.split('').map(Number);
+  const max = 1000000;
+  const nextMap = {};
+  for (let i = 0; i < max; i++) {
+    nextMap[labels[i] || i + 1] = labels[i + 1] || i + 2;
+  }
+  nextMap[max] = labels[0];
+  let curr = labels[0];
+
+  for (let i = 0; i < 10000000; i++) {
+    const pickup = [
+      nextMap[curr],
+      nextMap[nextMap[curr]],
+      nextMap[nextMap[nextMap[curr]]],
+    ];
+
+    let dest = ((curr - 2 + max) % max) + 1;
+    while (pickup.includes(dest)) {
+      dest = ((dest - 2 + max) % max) + 1;
+    }
+
+    debug && console.log({ i, nextMap, curr, pickup, dest });
+
+    nextMap[curr] = nextMap[pickup[2]];
+    const tmp = nextMap[dest];
+    nextMap[dest] = pickup[0];
+    nextMap[pickup[2]] = tmp;
+    curr = nextMap[curr];
+  }
+
+  console.log(nextMap[1] * nextMap[nextMap[1]]);
+}
 
 const inputs = [];
 inputs.push(`389125467`);
