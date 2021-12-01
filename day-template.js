312c8220a470e55049432d7507063231d7518fc4
Leyan Lo <leyanlo@gmail.com>
2021-11-30 22:27:44 -0800

    chore(template): trim end from input

--- day-template.js
+++ day-template.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('./day-xx-input.txt', 'utf8');
+const input = fs.readFileSync('./day-xx-input.txt', 'utf8').trimEnd();
 
 function solve(input) {
   console.log(input);
