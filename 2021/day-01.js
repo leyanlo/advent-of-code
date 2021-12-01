b340e84350fab9bd8dbee144ea721042319976a7
Leyan Lo <leyanlo@gmail.com>
2021-11-30 23:02:40 -0800

    chore(2021): day 1 code cleanup

--- 2021/day-01.js
+++ 2021/day-01.js
@@ -1,37 +1,29 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('./day-01-input.txt', 'utf8').trimEnd();
-// const input = `199
-// 200
-// 208
-// 210
-// 200
-// 207
-// 240
-// 269
-// 260
-// 263`;
 
-// function solve(input) {
-//   const depths = input.split('\n').map(Number);
-//   console.log(depths);
-//   let count = 0;
-//   for (let i = 1; i < depths.length; i++) {
-//     if (depths[i] > depths[i - 1]) count++;
-//   }
-//   console.log(count);
-// }
-//
-// solve(input);
+const debug = false;
+
+function solve1(input) {
+  const depths = input.split('\n').map(Number);
+  debug && console.log(depths);
+  let count = 0;
+  for (let i = 1; i < depths.length; i++) {
+    if (depths[i] > depths[i - 1]) count++;
+  }
+  console.log(count);
+}
+
+solve1(input);
 
 function solve2(input) {
   const depths = input.split('\n').map(Number);
-  console.log(depths);
+  debug && console.log(depths);
   const sums = [];
   for (let i = 2; i < depths.length; i++) {
     sums.push(depths[i] + depths[i - 1] + depths[i - 2]);
   }
-  console.log(sums);
+  debug && console.log(sums);
   let count = 0;
   for (let i = 1; i < sums.length; i++) {
     if (sums[i] > sums[i - 1]) count++;
