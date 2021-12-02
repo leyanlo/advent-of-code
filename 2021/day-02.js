ae8f42086515251636e4da1a8ed664f765a503ab
Leyan Lo <leyanlo@gmail.com>
2021-12-01 21:30:40 -0800

    feat(2021): day 2 code cleanup

--- 2021/day-02.js
+++ 2021/day-02.js
@@ -1,53 +1,45 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('./day-02-input.txt', 'utf8').trimEnd();
-// const input = `forward 5
-// down 5
-// forward 8
-// up 3
-// down 8
-// forward 2`;
 
-function solve(input) {
+function solve1(input) {
   const course = input.split('\n').map((line) => {
     const [dir, units] = line.split(' ');
     switch (dir) {
       case 'forward':
-        return [0, +units, 0];
+        return [+units, 0];
+      case 'down':
+        return [0, +units];
+      case 'up':
+        return [0, -+units];
+    }
+  });
+  const [horiz, depth] = course.reduce(([x, y], [dx, dy]) => [x + dx, y + dy], [
+    0,
+    0,
+  ]);
+  console.log(horiz * depth);
+}
+
+solve1(input);
+
+function solve2(input) {
+  const course = input.split('\n').map((line) => {
+    const [dir, units] = line.split(' ');
+    switch (dir) {
+      case 'forward':
+        return [+units, 0, 0];
       case 'down':
         return [0, 0, +units];
       case 'up':
         return [0, 0, -+units];
     }
   });
-  const [depth, horiz] = course.reduce(
-    ([y, x, aim], [dy, dx, daim]) => {
-      return [dx ? y + aim * dx : y + dy, x + dx, aim + daim];
-    },
+  const [horiz, depth] = course.reduce(
+    ([x, y, aim], [dx, dy, daim]) => [x + dx, y + aim * dx, aim + daim],
     [0, 0, 0]
   );
-  console.log(depth * horiz);
+  console.log(horiz * depth);
 }
 
-solve(input);
-
-// function solve(input) {
-//   const course = input.split('\n').map((line) => {
-//     const [dir, units] = line.split(' ');
-//     switch (dir) {
-//       case 'forward':
-//         return [0, +units];
-//       case 'down':
-//         return [+units, 0];
-//       case 'up':
-//         return [-+units, 0];
-//     }
-//   });
-//   const [depth, horiz] = course.reduce(([y, x], [dy, dx]) => [y + dy, x + dx], [
-//     0,
-//     0,
-//   ]);
-//   console.log(depth * horiz);
-// }
-//
-// solve(input);
+solve2(input);
