ee3a4f48429e93b6ee8be2c8b8614275bc9cea73
Leyan Lo <leyanlo@gmail.com>
2021-12-02 21:49:57 -0800

    feat(2021): day 3 more code cleanup

--- 2021/day-03.js
+++ 2021/day-03.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./day-03-input.txt', 'utf8').trimEnd();
 
-const debug = false;
-
 function getRates(bits) {
   const counts = [];
   for (let i = 0; i < bits.length; i++) {
@@ -11,16 +9,13 @@ function getRates(bits) {
       counts[j] = (counts[j] ?? 0) + bits[i][j];
     }
   }
-  debug && console.log(counts);
   const gamma = counts.map((n) => +(n >= bits.length / 2)).join('');
   const epsilon = counts.map((n) => +(n < bits.length / 2)).join('');
   return [gamma, epsilon];
 }
 
 function solve1(input) {
-  debug && console.log(input);
   const bits = input.split('\n').map((line) => line.split('').map(Number));
-  debug && console.log(bits);
   const [gamma, epsilon] = getRates(bits);
   console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
 }
@@ -28,27 +23,24 @@ function solve1(input) {
 solve1(input);
 
 function solve2(input) {
-  debug && console.log(input);
   const bits = input.split('\n').map((line) => line.split('').map(Number));
-  debug && console.log(bits);
+
   let o2Rating = [...bits];
-  let [gamma, epsilon] = getRates(o2Rating);
-  debug && console.log(gamma, epsilon);
+  let [gamma] = getRates(o2Rating);
   for (let i = 0; i < gamma.length; i++) {
     o2Rating = o2Rating.filter((row) => row[i] === +gamma[i]);
-    debug && console.log(o2Rating);
-    [gamma, epsilon] = getRates(o2Rating);
+    [gamma] = getRates(o2Rating);
     if (o2Rating.length === 1) break;
   }
 
   let co2Rating = [...bits];
-  [gamma, epsilon] = getRates(o2Rating);
+  [, epsilon] = getRates(o2Rating);
   for (let i = 0; i < epsilon.length; i++) {
     co2Rating = co2Rating.filter((row) => row[i] === +epsilon[i]);
-    debug && console.log(co2Rating);
-    [gamma, epsilon] = getRates(co2Rating);
+    [, epsilon] = getRates(co2Rating);
     if (co2Rating.length === 1) break;
   }
+
   console.log(
     parseInt(o2Rating[0].join(''), 2) * parseInt(co2Rating[0].join(''), 2)
   );
