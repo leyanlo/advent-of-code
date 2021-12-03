6e44dcc43fcc9f7ce7102776c116f112afecd959
Leyan Lo <leyanlo@gmail.com>
2021-12-02 23:16:32 -0800

    chore(2021): day 1 code cleanup
    Thanks Tushar!

--- 2021/day-01.js
+++ 2021/day-01.js
@@ -2,33 +2,22 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./day-01-input.txt', 'utf8').trimEnd();
 
-const debug = false;
-
 function solve1(input) {
   const depths = input.split('\n').map(Number);
-  debug && console.log(depths);
   let count = 0;
   for (let i = 1; i < depths.length; i++) {
     if (depths[i] > depths[i - 1]) count++;
   }
   console.log(count);
 }
-
 solve1(input);
 
 function solve2(input) {
   const depths = input.split('\n').map(Number);
-  debug && console.log(depths);
-  const sums = [];
-  for (let i = 2; i < depths.length; i++) {
-    sums.push(depths[i] + depths[i - 1] + depths[i - 2]);
-  }
-  debug && console.log(sums);
   let count = 0;
-  for (let i = 1; i < sums.length; i++) {
-    if (sums[i] > sums[i - 1]) count++;
+  for (let i = 3; i < depths.length; i++) {
+    if (depths[i] > depths[i - 3]) count++;
   }
   console.log(count);
 }
-
 solve2(input);
