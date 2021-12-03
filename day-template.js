03414ce443cea158b9aac223fdf7aca58b94ad03
Leyan Lo <leyanlo@gmail.com>
2021-12-03 09:43:19 -0800

    chore(template): update day-template for speed

--- day-template.js
+++ day-template.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('./day-xx-input.txt', 'utf8').trimEnd();
+var input = ``;
+var input = fs.readFileSync('./day-xx-input.txt', 'utf8').trimEnd();
 
 function solve(input) {
   console.log(input);
 }
-
 solve(input);
