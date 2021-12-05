eb7c156d8770b20a8f236be2da736aa9b7f1bb5a
Leyan Lo <leyanlo@gmail.com>
2021-12-04 22:24:57 -0800

    chore(2021): day 5 remove debug code from main logic

--- 2021/day-05.js
+++ 2021/day-05.js
@@ -10,17 +10,13 @@ function solve(input, part) {
     .map((line) =>
       line.split(' -> ').map((coords) => coords.split(',').map(Number))
     );
-  let maxX = 0;
-  let maxY = 0;
   const diagram = [];
   for (const [[x1, y1], [x2, y2]] of lines) {
     const dx = x2 === x1 ? 0 : (x2 - x1) / Math.abs(x2 - x1);
     const dy = y2 === y1 ? 0 : (y2 - y1) / Math.abs(y2 - y1);
-    if (dx !== 0 && dy !== 0 && (part === 1 || Math.abs(dy / dx) !== 1)) {
+    if (dx && dy && (part === 1 || Math.abs(dy / dx) !== 1)) {
       continue;
     }
-    maxX = Math.max(maxX, x1, x2);
-    maxY = Math.max(maxY, y1, y2);
     for (
       let x = x1, y = y1;
       (!dx || x !== x2 + dx) && (!dy || y !== y2 + dy);
@@ -30,7 +26,15 @@ function solve(input, part) {
       diagram[x][y] = (diagram[x][y] ?? 0) + 1;
     }
   }
-  debug &&
+
+  if (debug) {
+    const [maxX, maxY] = lines.reduce(
+      ([maxX, maxY], [[x1, y1], [x2, y2]]) => [
+        Math.max(maxX, x1, x2),
+        Math.max(maxY, y1, y2),
+      ],
+      [0, 0]
+    );
     console.log(
       [...Array(maxY + 1).keys()]
         .map((y) =>
@@ -40,6 +44,8 @@ function solve(input, part) {
         )
         .join('\n')
     );
+  }
+
   console.log(diagram.flat().filter((n) => n >= 2).length);
 }
 
