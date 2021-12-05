ef34f1fb619a7102075e9c74e8f98f74a56f9c4c
Leyan Lo <leyanlo@gmail.com>
2021-12-05 14:40:57 -0800

    feat(2018): day 7 part 1 code cleanup

--- 2018/day-07.js
+++ 2018/day-07.js
@@ -9,37 +9,40 @@ Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.`;
 var input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
 
+function getNextStep(requirements) {
+  console.log({ requirements });
+  const allSteps = new Set(Object.entries(requirements).flat(2));
+  console.log({ allSteps });
+  const unavailableSteps = new Set(Object.values(requirements).flat());
+  console.log({ unavailableSteps });
+  const nextStep = [...allSteps]
+    .filter((step) => !unavailableSteps.has(step))
+    .sort()[0];
+  console.log({ nextStep });
+  return nextStep;
+}
+
 function solve(input) {
   const instructions = input
     .split('\n')
     .map((line) => [...line.matchAll(/step (\w)/gi)].map((match) => match[1]));
-  console.log(instructions);
+  console.log({ instructions });
   const requirements = {};
   for (const [start, end] of instructions) {
     requirements[start] = requirements[start] ?? [];
     requirements[start].push(end);
   }
-
-  let availableSteps = [];
+  let nextStep = getNextStep(requirements);
   const steps = [];
-  do {
-    console.log({ requirements });
-    const allSteps = new Set(Object.entries(requirements).flat(2));
-    console.log({ allSteps });
-    const unavailableSteps = new Set(Object.values(requirements).flat());
-    console.log({ unavailableSteps });
-    availableSteps = [...allSteps]
-      .filter((step) => !unavailableSteps.has(step))
-      .sort();
-    console.log({ availableSteps });
-    if (availableSteps.length) {
-      steps.push(availableSteps[0]);
-      if (availableSteps.length === 1 && unavailableSteps.size === 1) {
-        steps.push(...unavailableSteps);
-      }
-      delete requirements[availableSteps[0]];
+  while (requirements[nextStep]?.length) {
+    steps.push(nextStep);
+    if (Object.values(requirements).length === 1) {
+      steps.push(Object.values(requirements));
+      break;
     }
-  } while (availableSteps.length);
+    delete requirements[nextStep];
+    nextStep = getNextStep(requirements);
+  }
   console.log(steps.join(''));
 }
 solve(input);
