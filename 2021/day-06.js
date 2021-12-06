9332a12714ca13d93fac6f87c588036dde8d6032
Leyan Lo <leyanlo@gmail.com>
2021-12-05 21:33:58 -0800

    feat(2021): day 6 code cleanup

--- 2021/day-06.js
+++ 2021/day-06.js
@@ -1,35 +1,19 @@
 const fs = require('fs');
 
-var input = `3,4,3,1,2`;
-var input = fs.readFileSync('./day-06-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-06-input.txt', 'utf8').trimEnd();
 
-function solve1(input) {
-  let fishes = input.split(',').map(Number);
-  for (let day = 0; day < 80; day++) {
-    const numNewFishes = fishes.filter((f) => f === 0).length;
-    fishes = fishes.map((f) => (f === 0 ? 6 : f - 1));
-    fishes.push(...Array(numNewFishes).fill(8));
-  }
-  console.log(fishes.length)
-}
-solve1(input);
-
-function solve2(input) {
+function solve(input, days) {
   const fishes = input.split(',').map(Number);
-  let fishMap = Array(9).fill(0);
+  const fishMap = Array(9).fill(0);
   for (const fish of fishes) {
     fishMap[fish] = fishMap[fish] + 1;
   }
-  for (let i = 0; i < 256; i++) {
-    const numNewFishes = fishMap[0];
-    const nextFishMap = Array(9).fill(0);
-    nextFishMap[8] += fishMap[0];
-    nextFishMap[6] += fishMap[0];
-    for (let j = 1; j <= 8; j++) {
-      nextFishMap[j - 1] += fishMap[j];
-    }
-    fishMap = nextFishMap;
+  let zeroIdx = 0;
+  for (let i = 0; i < days; i++) {
+    fishMap[(zeroIdx + 7) % 9] += fishMap[zeroIdx];
+    zeroIdx = (zeroIdx + 1) % 9;
   }
   console.log(fishMap.reduce((acc, f) => acc + f, 0));
 }
-solve2(input);
+solve(input, 80);
+solve(input, 256);
