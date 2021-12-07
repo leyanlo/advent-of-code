de556de959a1f157427a57c6dd588d8eb1f97a8e
Leyan Lo <leyanlo@gmail.com>
2021-12-06 21:19:16 -0800

    feat(2021): day 7 code cleanup

--- 2021/day-07.js
+++ 2021/day-07.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
-var input = `16,1,2,0,4,2,7,1,2,14`;
-var input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
 
 function cost(n) {
   return ((n + 1) * n) / 2;
@@ -12,28 +11,9 @@ function solve(input) {
     .split(',')
     .map(Number)
     .sort((a, b) => a - b);
-  console.log(positions);
-  const mean = Math.round(
-    positions.reduce((acc, p) => acc + p, 0) / positions.length
-  );
-  console.log(mean);
+  const median = positions[~~(positions.length / 2)];
+  const mean = ~~(positions.reduce((acc, p) => acc + p, 0) / positions.length);
+  console.log(positions.reduce((acc, p) => acc + Math.abs(p - median), 0));
   console.log(positions.reduce((acc, p) => acc + cost(Math.abs(p - mean)), 0));
-  console.log(
-    positions.reduce((acc, p) => acc + cost(Math.abs(p - mean + 1)), 0)
-  );
-  console.log(
-    positions.reduce((acc, p) => acc + cost(Math.abs(p - mean - 1)), 0)
-  );
 }
 solve(input);
-// function solve(input) {
-//   const positions = input
-//     .split(',')
-//     .map(Number)
-//     .sort((a, b) => a - b);
-//   console.log(positions);
-//   const median = positions[~~(positions.length / 2)];
-//   console.log(median);
-//   console.log(positions.reduce((acc, p) => acc + Math.abs(p - median), 0));
-// }
-// solve(input);
