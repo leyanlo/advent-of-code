c44e62d1cb6b442233822bde09719c33bcb67578
Leyan Lo <leyanlo@gmail.com>
2021-12-10 23:33:51 -0800

    chore(2021): day 11 code cleanup
    reuse logic for incrementing and checking === 10

--- 2021/day-11.js
+++ 2021/day-11.js
@@ -13,28 +13,26 @@ const dirs = [
   [-1, 1],
 ];
 
-function flash(energies, i, j) {
+function incrementOne(energies, i, j) {
   let count = 0;
-  for (const [di, dj] of dirs) {
-    if (energies[i + di]?.[j + dj] === undefined) {
-      continue;
-    }
-    energies[i + di][j + dj]++;
-    if (energies[i + di][j + dj] === 10) {
-      count += 1 + flash(energies, i + di, j + dj);
+  energies[i][j]++;
+  if (energies[i][j] === 10) {
+    count++;
+    for (const [di, dj] of dirs) {
+      if (energies[i + di]?.[j + dj] === undefined) {
+        continue;
+      }
+      count += incrementOne(energies, i + di, j + dj);
     }
   }
   return count;
 }
 
-function increment(energies) {
+function incrementAll(energies) {
   let count = 0;
   for (let i = 0; i < energies.length; i++) {
     for (let j = 0; j < energies[i].length; j++) {
-      energies[i][j]++;
-      if (energies[i][j] === 10) {
-        count += 1 + flash(energies, i, j);
-      }
+      count += incrementOne(energies, i, j);
     }
   }
   for (let i = 0; i < energies.length; i++) {
@@ -49,7 +47,7 @@ function solve1(input) {
   const energies = input.split('\n').map((line) => line.split('').map(Number));
   let count = 0;
   for (let step = 0; step < 100; step++) {
-    count += increment(energies);
+    count += incrementAll(energies);
   }
   console.log(count);
 }
@@ -60,7 +58,7 @@ function solve2(input) {
   let step = 0;
   while (!energies.every((line) => line.every((energy) => energy === 0))) {
     step++;
-    increment(energies);
+    incrementAll(energies);
   }
   console.log(step);
 }
