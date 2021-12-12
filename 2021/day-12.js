54b16602db9950ff6085f3d0548e7245863bf801
Leyan Lo <leyanlo@gmail.com>
2021-12-12 00:03:30 -0800

    chore(2021): day 12 clean up
    preprocess connections for way simpler logic

--- 2021/day-12.js
+++ 2021/day-12.js
@@ -10,60 +10,37 @@ function solve(input, maxDupes) {
   const lines = input.split('\n').map((line) => line.split('-'));
   const connections = {};
   for (const [a, b] of lines) {
-    connections[a] = connections[a] ?? [];
-    connections[a].push(b);
-    connections[b] = connections[b] ?? [];
-    connections[b].push(a);
+    if (b !== 'start' && a !== 'end') {
+      connections[a] = connections[a] ?? [];
+      connections[a].push(b);
+    }
+    if (a !== 'start' && b !== 'end') {
+      connections[b] = connections[b] ?? [];
+      connections[b].push(a);
+    }
   }
 
+  const validPaths = [];
   let paths = [['start']];
-  let nexts = [[...connections.start]];
-  let step = 0;
-  while (nexts.flat().length) {
-    step++;
+  while (paths.length) {
     const nextPaths = [];
-    const nextNexts = [];
-    for (let i = 0; i < paths.length; i++) {
-      const path = paths[i];
-      const next = nexts[i];
-      if (next.length === 0) {
-        nextPaths.push(path);
-        nextNexts.push([]);
-        continue;
-      }
-      for (const cave of next) {
-        nextPaths.push([...path, cave]);
-        const nextNext = [];
-        for (const nextCave of connections[cave]) {
-          const smallCaves = path.filter(
-            (cave) => cave !== 'start' && cave !== 'end' && isSmall(cave)
-          );
-          if (
-            nextCave === 'start' ||
-            cave === 'end' ||
-            (isSmall(nextCave) &&
-              [...smallCaves, nextCave].length >
-                new Set([...smallCaves, nextCave]).size + maxDupes)
-          ) {
-            continue;
-          }
-          nextNext.push(nextCave);
+    for (const path of paths) {
+      const cave = path[path.length - 1];
+      for (const nextCave of connections[cave]) {
+        const nextPath = [...path, nextCave];
+        if (nextCave === 'end') {
+          validPaths.push(nextPath);
+          continue;
         }
-        nextNexts.push(nextNext);
+        const smallCaves = nextPath.filter(isSmall);
+        if (smallCaves.length > new Set(smallCaves).size + maxDupes) {
+          continue;
+        }
+        nextPaths.push(nextPath);
       }
     }
     paths = nextPaths;
-    nexts = nextNexts;
   }
-  const validPaths = paths
-    .filter((path) => path[path.length - 1] === 'end')
-    .filter((path) => {
-      const smallCaves = path.filter(
-        (cave) => cave !== 'start' && cave !== 'end' && isSmall(cave)
-      );
-      return smallCaves.length <= new Set(smallCaves).size + maxDupes;
-    });
-
   console.log(validPaths.length);
 }
 solve(input, 0);
