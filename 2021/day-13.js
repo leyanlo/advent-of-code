8cb2fdd2d1d272a1f7b408d1979352c08df3b293
Leyan Lo <leyanlo@gmail.com>
2021-12-12 21:42:22 -0800

    chore(2021): day 13 code clean up

--- 2021/day-13.js
+++ 2021/day-13.js
@@ -1,31 +1,6 @@
 const fs = require('fs');
 
-var input = `6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
-
-fold along y=7
-fold along x=5`;
-var input = fs.readFileSync('./day-13-input.txt', 'utf8').trimEnd();
-// Not VFAZKMUX
-// Not VRAZKMUX
-// Not VFRZKAVZ
-// UFRZKAUZ
+const input = fs.readFileSync('./day-13-input.txt', 'utf8').trimEnd();
 
 function solve(input) {
   let [dots, folds] = input.split('\n\n');
@@ -35,45 +10,41 @@ function solve(input) {
     fold[1] = +fold[1];
     return fold;
   });
-  // console.log({ dots, folds });
 
   const [maxX, maxY] = dots.reduce(([maxX, maxY], [x, y]) => {
     return [Math.max(x, maxX), Math.max(y, maxY)];
   });
-  // console.log({ maxX, maxY });
 
-  let paper = [...Array(maxY + 1).keys()].map(() =>
-    [...Array(maxX + 1).keys()].fill(0)
-  );
+  const paper = [...Array(maxY + 1)].map(() => Array(maxX + 1).fill(0));
   for (const [x, y] of dots) {
     paper[y][x] = 1;
   }
-  // console.log(paper.map((line) => line.join('')).join('\n'), '\n');
 
-  for (const [axis, idx] of folds) {
+  for (let i = 0; i < folds.length; i++) {
+    const [axis, value] = folds[i];
     if (axis === 'y') {
-      for (let y = 0; y < idx; y++) {
+      for (let y = 0; y < value; y++) {
         for (let x = 0; x < paper[y].length; x++) {
-          paper[y][x] = paper[y][x] | paper[2 * idx - y]?.[x];
+          paper[y][x] = paper[y][x] | paper[2 * value - y]?.[x];
         }
       }
-      paper.length = idx;
-    } else if (axis === 'x') {
+      paper.length = value;
+    } else {
       for (let y = 0; y < paper.length; y++) {
-        for (let x = 0; x < idx; x++) {
-          paper[y][x] = paper[y][x] | paper[y][2 * idx - x];
+        for (let x = 0; x < value; x++) {
+          paper[y][x] = paper[y][x] | paper[y][2 * value - x];
         }
-        paper[y].length = idx;
+        paper[y].length = value;
       }
     }
-    // break;
+    if (i === 0) {
+      console.log(paper.flat().filter(Boolean).length);
+    }
   }
   console.log(
     paper
-      .map((line) => line.map((dot) => (dot ? '##' : '..')).join(''))
-      .join('\n'),
-    '\n'
+      .map((line) => line.map((dot) => (dot ? '#' : ' ')).join(''))
+      .join('\n')
   );
-  console.log(paper.flat().filter(Boolean).length);
 }
 solve(input);
