8ca8b7ddeb4b17b1df12762390fcd326e7b6731a
Leyan Lo <leyanlo@gmail.com>
2021-12-13 22:21:39 -0800

    feat(2021): day 14 code cleanup

--- 2021/day-14.js
+++ 2021/day-14.js
@@ -1,175 +1,42 @@
 const fs = require('fs');
 
-var input = `NNCB
+const input = fs.readFileSync('./day-14-input.txt', 'utf8').trimEnd();
 
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C`;
-var input = fs.readFileSync('./day-14-input.txt', 'utf8').trimEnd();
-// not 2851
-
-// function solve(input) {
-//   let [template, rules] = input.split('\n\n');
-//   rules = rules.split('\n').reduce((acc, rule) => {
-//     const [left, right] = rule.split(' -> ');
-//     acc[left] = right;
-//     return acc;
-//   }, {});
-//   console.log(template, rules);
-//   let result;
-//   for (let step = 0; step < 10; step++) {
-//     result = [template[0]];
-//     for (let i = 0; i < template.length - 1; i++) {
-//       result.push(rules[template.slice(i, i + 2)], template[i + 1]);
-//     }
-//     template = result.join('');
-//   }
-//   const counts = result.reduce((acc, char) => {
-//     acc[char] = (acc[char] ?? 0) + 1;
-//     return acc;
-//   }, {});
-//   console.log(counts);
-//   console.log(
-//     Math.max(...Object.values(counts)) - Math.min(...Object.values(counts))
-//   );
-// }
-// solve(input);
-
-// class Pointer {
-//   constructor(char, next) {
-//     this.char = char;
-//     this.next = next;
-//   }
-// }
-//
-// function print(result) {
-//   let x = [];
-//   let tmp = result;
-//   while (tmp) {
-//     x.push(tmp.char);
-//     tmp = tmp.next;
-//   }
-//   console.log('x', x.join(''));
-// }
-//
-// function solve2(input) {
-//   let [template, rules] = input.split('\n\n');
-//   rules = rules.split('\n').reduce((acc, rule) => {
-//     const [left, right] = rule.split(' -> ');
-//     acc[left] = right;
-//     return acc;
-//   }, {});
-//   console.log(template, rules);
-//
-//   let result = new Pointer(template[template.length - 1], null);
-//   for (const char of template.split('').slice(0, -1).reverse()) {
-//     result = new Pointer(char, result);
-//   }
-//
-//   for (let step = 0; step < 40; step++) {
-//     const nextResult = result;
-//     while (result.next) {
-//       result.next = new Pointer(
-//         rules[result.char + result.next.char],
-//         result.next
-//       );
-//       result = result.next.next;
-//     }
-//     result = nextResult;
-//   }
-//
-//   const counts = {};
-//   let tmp = result;
-//   while (tmp) {
-//     counts[tmp.char] = (counts[tmp.char] ?? 0) + 1;
-//     tmp = tmp.next;
-//   }
-//   console.log(counts);
-//   console.log(
-//     Math.max(...Object.values(counts)) - Math.min(...Object.values(counts))
-//   );
-// }
-// solve2(input);
-
-function solve2(input) {
+function solve(input, nSteps) {
   let [template, rules] = input.split('\n\n');
-  rules = rules.split('\n').reduce((acc, rule) => {
+
+  const nextPairs = rules.split('\n').reduce((acc, rule) => {
     const [left, right] = rule.split(' -> ');
-    acc[left] = right;
+    acc[left] = [left[0] + right, right + left[1]];
     return acc;
   }, {});
-  console.log({ template, rules });
-
-  for (const pair in rules) {
-    rules[pair] = [pair[0] + rules[pair], rules[pair] + pair[1]];
-  }
-  console.log({ rules });
 
   let counts = {};
   for (let i = 0; i < template.length - 1; i++) {
     counts[template.slice(i, i + 2)] =
       (counts[template.slice(i, i + 2)] ?? 0) + 1;
   }
-  console.log({ counts });
 
-  for (let steps = 0; steps < 40; steps++) {
+  for (let step = 0; step < nSteps; step++) {
     const nextCounts = {};
     for (const pair in counts) {
-      for (const newPair of rules[pair]) {
-        nextCounts[newPair] = (nextCounts[newPair] ?? 0) + counts[pair];
+      for (const nextPair of nextPairs[pair]) {
+        nextCounts[nextPair] = (nextCounts[nextPair] ?? 0) + counts[pair];
       }
     }
     counts = nextCounts;
-    console.log({ counts });
   }
-  const letterCounts = {
+
+  const elCounts = {
     [template[0]]: 1,
   };
   for (const pair in counts) {
-    letterCounts[pair[1]] = (letterCounts[pair[1]] ?? 0) + counts[pair];
+    elCounts[pair[1]] = (elCounts[pair[1]] ?? 0) + counts[pair];
   }
-  console.log({ letterCounts });
 
-  // let result = new Pointer(template[template.length - 1], null);
-  // for (const char of template.split('').slice(0, -1).reverse()) {
-  //   result = new Pointer(char, result);
-  // }
-  //
-  // for (let step = 0; step < 40; step++) {
-  //   const nextResult = result;
-  //   while (result.next) {
-  //     result.next = new Pointer(
-  //       rules[result.char + result.next.char],
-  //       result.next
-  //     );
-  //     result = result.next.next;
-  //   }
-  //   result = nextResult;
-  // }
-  //
-  // const counts = {};
-  // let tmp = result;
-  // while (tmp) {
-  //   counts[tmp.char] = (counts[tmp.char] ?? 0) + 1;
-  //   tmp = tmp.next;
-  // }
-  // console.log(counts);
   console.log(
-    Math.max(...Object.values(letterCounts)) -
-      Math.min(...Object.values(letterCounts))
+    Math.max(...Object.values(elCounts)) - Math.min(...Object.values(elCounts))
   );
 }
-solve2(input);
+solve(input, 10);
+solve(input, 40);
