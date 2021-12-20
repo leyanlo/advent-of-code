7a0adf3d9a8552ef90ece8b6290c7788f169d12d
Leyan Lo <leyanlo@gmail.com>
2021-12-19 21:30:22 -0800

    chore(2021): day 20 cleanup

--- 2021/day-20.js
+++ 2021/day-20.js
@@ -1,15 +1,6 @@
 const fs = require('fs');
 
-var input = `..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
-
-#..#.
-#....
-##..#
-..#..
-..###`;
-var input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd();
-// 5686 wrong
-// 6844 wrong
+const input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd();
 
 const neighbors = [
   [-1, -1],
@@ -23,21 +14,21 @@ const neighbors = [
   [1, 1],
 ];
 
-function solve(input) {
+function solve(input, times) {
   let [alg, img] = input.split('\n\n');
   alg = alg.split('').map((char) => +(char === '#'));
   img = img
     .split('\n')
     .map((row) => row.split('').map((char) => +(char === '#')));
 
-  for (let k = 0; k < 50; k++) {
+  for (let t = 0; t < times; t++) {
     const newImg = [];
     for (let i = -1; i < img.length + 1; i++) {
       const newImgRow = [];
       for (let j = -1; j < img.length + 1; j++) {
         const pixels = [];
         for (const [di, dj] of neighbors) {
-          pixels.push(img[i + di]?.[j + dj] ?? k % 2);
+          pixels.push(img[i + di]?.[j + dj] ?? t % 2);
         }
         const num = parseInt(pixels.join(''), 2);
         newImgRow.push(alg[num]);
@@ -45,12 +36,8 @@ function solve(input) {
       newImg.push(newImgRow);
     }
     img = newImg;
-    console.log(
-      img
-        .map((row) => row.map((char) => (char ? '#' : '.')).join(''))
-        .join('\n')
-    );
   }
   console.log(img.flat().filter(Boolean).length);
 }
-solve(input);
+solve(input, 2);
+solve(input, 50);
