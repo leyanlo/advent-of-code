4bca4f5df700f54483c92626c754ed926adf5cc8
Leyan Lo <leyanlo@gmail.com>
2021-12-21 02:48:00 -0800

    feat(2021): day 21

--- 2021/day-21.js
+++ 2021/day-21.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 var input = `Player 1 starting position: 4
 Player 2 starting position: 8`;
-// var input = fs.readFileSync('./day-21-input.txt', 'utf8').trimEnd();
+var input = fs.readFileSync('./day-21-input.txt', 'utf8').trimEnd();
 
 // function solve(input) {
 //   const positions = input.split('\n').map((line) => +line[line.length - 1]);
@@ -30,47 +30,47 @@ Player 2 starting position: 8`;
 
 const rolls = [1, 2, 3];
 
-const memo = {};
+function solve(input) {
+  const positions = input.split('\n').map((line) => +line.split(': ')[1]);
 
-function countWins(p1, p2, score1, score2, wins) {
-  const key = [p1, p2, score1, score2].join();
-  if (memo[key]) {
-    wins[memo[key] - 1]++;
-    return;
-  }
-  if (score1 >= 21) {
-    wins[0]++;
-    memo[key] = 1;
-    console.log(wins);
-    return;
-  }
-  if (score2 >= 21) {
-    wins[1]++;
-    memo[key] = 2;
-    return;
-  }
-  for (const r1a of rolls) {
-    for (const r1b of rolls) {
-      for (const r1c of rolls) {
-        for (const r2a of rolls) {
-          for (const r2b of rolls) {
-            for (const r2c of rolls) {
-              const nextP1 = ((p1 + r1a + r1b + r1c - 1) % 10) + 1;
-              const nextP2 = ((p2 + r2a + r2b + r2c - 1) % 10) + 1;
-              countWins(nextP1, nextP2, score1 + nextP1, score2 + nextP2, wins);
+  const wins = [0, 0];
+
+  let gameCounts = {
+    [[positions, [0, 0]].join(';')]: 1,
+  };
+  while (Object.entries(gameCounts).length > 0) {
+    for (const i of [0, 1]) {
+      const nextGameCounts = {};
+      for (const [state, gameCount] of Object.entries(gameCounts)) {
+        const [positions, scores] = state
+          .split(';')
+          .map((s) => s.split(',').map(Number));
+
+        for (const r1 of rolls) {
+          for (const r2 of rolls) {
+            for (const r3 of rolls) {
+              const nextPositions = [...positions];
+              nextPositions[i] = ((positions[i] + r1 + r2 + r3 - 1) % 10) + 1;
+
+              const nextScores = [...scores];
+              nextScores[i] += nextPositions[i];
+
+              if (nextScores[i] >= 21) {
+                wins[i] += gameCount;
+                continue;
+              }
+
+              const nextState = [nextPositions, nextScores].join(';');
+              nextGameCounts[nextState] =
+                (nextGameCounts[nextState] ?? 0) + gameCount;
             }
           }
         }
       }
+      gameCounts = nextGameCounts;
     }
   }
-}
 
-function solve(input) {
-  const [p1, p2] = input.split('\n').map((line) => +line[line.length - 1]);
-
-  const wins = Array(2).fill(0n);
-  countWins(p1, p2, 0, 0, wins);
-  console.log(wins);
+  console.log(Math.max(...wins));
 }
 solve(input);
