1a708b5326ee1b5c8e721f56eb3df808d695948e
Leyan Lo <leyanlo@gmail.com>
2021-12-21 08:00:38 -0800

    chore(2021): day 21 cleanup

--- 2021/day-21.js
+++ 2021/day-21.js
@@ -1,48 +1,45 @@
 const fs = require('fs');
 
-var input = `Player 1 starting position: 4
-Player 2 starting position: 8`;
-var input = fs.readFileSync('./day-21-input.txt', 'utf8').trimEnd();
-
-// function solve(input) {
-//   const positions = input.split('\n').map((line) => +line[line.length - 1]);
-//   const scores = positions.map(() => 0);
-//
-//   let roll = 0;
-//   let t = 0;
-//   outer: while (true) {
-//     for (let i = 0; i < positions.length; i++) {
-//       for (let r = 0; r < 3; r++) {
-//         roll = (roll + 1) % 100;
-//         positions[i] += roll;
-//         t++;
-//       }
-//       positions[i] = ((positions[i] + 9) % 10) + 1;
-//       scores[i] += positions[i];
-//       if (scores[i] >= 1000) {
-//         console.log(scores[(i + 1) % 2] * t);
-//         break outer;
-//       }
-//     }
-//   }
-// }
-// solve(input);
+const input = fs.readFileSync('./day-21-input.txt', 'utf8').trimEnd();
 
 const rolls = [1, 2, 3];
 
-function solve(input) {
+function solve1(input) {
   const positions = input.split('\n').map((line) => +line.split(': ')[1]);
+  const scores = [0, 0];
+
+  let roll = 0;
+  while (true) {
+    for (const i of [0, 1]) {
+      for (const r of rolls) {
+        roll++;
+        positions[i] += ((roll - 1) % 100) + 1;
+      }
+      positions[i] = ((positions[i] - 1) % 10) + 1;
+      scores[i] += positions[i];
+      if (scores[i] >= 1000) {
+        console.log(scores[(i + 1) % 2] * roll);
+        return;
+      }
+    }
+  }
+}
+solve1(input);
+
+function solve2(input) {
+  let positions = input.split('\n').map((line) => +line.split(': ')[1]);
+  let scores = [0, 0];
 
   const wins = [0, 0];
 
   let gameCounts = {
-    [[positions, [0, 0]].join(';')]: 1,
+    [[positions, scores].join(';')]: 1,
   };
   while (Object.entries(gameCounts).length > 0) {
     for (const i of [0, 1]) {
       const nextGameCounts = {};
       for (const [state, gameCount] of Object.entries(gameCounts)) {
-        const [positions, scores] = state
+        [positions, scores] = state
           .split(';')
           .map((s) => s.split(',').map(Number));
 
@@ -73,4 +70,4 @@ function solve(input) {
 
   console.log(Math.max(...wins));
 }
-solve(input);
+solve2(input);
