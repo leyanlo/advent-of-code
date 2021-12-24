1ce9c4e9a3796ef70cef21382086541ae9edbced
Leyan Lo <leyanlo@gmail.com>
2021-12-24 03:04:30 -0800

    chore(2021): day 24 rename coeffs to terms

--- 2021/day-24.js
+++ 2021/day-24.js
@@ -5,16 +5,16 @@ const input = fs.readFileSync('./day-24-input.txt', 'utf8').trimEnd();
 function solve(input, part) {
   const lines = input.split('\n');
 
-  const coeffs = [];
+  const terms = [];
   for (let i = 0; i < 14; i++) {
-    // coefficients [a, b, c] such that z' = ~~(z / a) * (25 * +(w !== (z % 26) + b) + 1) + (w + c) * +(w !== (z % 26) + b)
-    coeffs.push([4, 5, 15].map((j) => +lines[18 * i + j].split(' ')[2]));
+    // terms [a, b, c] such that z'(a, b, c, w, z) = ~~(z / a) * (25 * +(w !== (z % 26) + b) + 1) + (w + c) * +(w !== (z % 26) + b)
+    terms.push([4, 5, 15].map((j) => +lines[18 * i + j].split(' ')[2]));
   }
 
   const idxStack = [];
   const idxMap = {};
   for (let i = 0; i < 14; i++) {
-    if (coeffs[i][0] === 1) {
+    if (terms[i][0] === 1) {
       idxStack.push(i);
     } else {
       idxMap[i] = idxStack.pop();
@@ -23,7 +23,7 @@ function solve(input, part) {
 
   const digits = [];
   for (const [i, j] of Object.entries(idxMap)) {
-    const complement = coeffs[j][2] + coeffs[i][1];
+    const complement = terms[j][2] + terms[i][1];
     digits[j] =
       part === 1 ? Math.min(9, 9 - complement) : Math.max(1, 1 - complement);
     digits[i] = digits[j] + complement;
