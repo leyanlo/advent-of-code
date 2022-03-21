083c6699696216cf00a561a86c9d6e5368f6fc99
Leyan Lo <leyanlo@gmail.com>
2022-03-20 22:16:28 -0700

    chore(2018): day 11 cleanup

--- 2018/day-11.js
+++ 2018/day-11.js
@@ -1,45 +1,8 @@
 const fs = require('fs');
 
-var input = `42`;
-var input = fs.readFileSync('./day-11-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-11-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const serial = +input;
-//   const grid = [];
-//   for (let y = 1; y <= 300; y++) {
-//     const row = [];
-//     for (let x = 1; x <= 300; x++) {
-//       const rackId = x + 10;
-//       let power = rackId * y;
-//       power += serial;
-//       power *= rackId;
-//       power = ~~(power / 100);
-//       power %= 10;
-//       power -= 5;
-//       row.push(power);
-//     }
-//     grid.push(row);
-//   }
-//   let maxPower;
-//   let maxCoords;
-//   for (let y = 1; y <= 298; y++) {
-//     for (let x = 1; x <= 298; x++) {
-//       let sum = 0;
-//       for (let dy = 0; dy <= 2; dy++) {
-//         for (let dx = 0; dx <= 2; dx++) {
-//           sum += grid[y + dy - 1][x + dx - 1];
-//         }
-//       }
-//       if (!maxPower || sum > maxPower) {
-//         maxPower = sum;
-//         maxCoords = [x, y];
-//       }
-//     }
-//   }
-//   console.log(maxCoords.join());
-// }
-// solve(input);
-function solve(input) {
+function solve(input, part) {
   const serial = +input;
   const grid = [...Array(300)].map(() => [...Array(300)]);
   for (let i = 0; i < 300; i++) {
@@ -71,7 +34,8 @@ function solve(input) {
   let maxPower;
   let maxCoords;
   let maxSize;
-  for (let size = 1; size <= 300; size++) {
+  const sizes = part === 1 ? [3] : [...Array(300)].map((_, i) => i + 1);
+  for (const size of sizes) {
     for (let i = size; i < 300; i++) {
       for (let j = size; j < 300; j++) {
         const x = j + 1 - size + 1;
@@ -90,6 +54,7 @@ function solve(input) {
     }
   }
 
-  console.log([maxCoords, maxSize].flat().join());
+  console.log([maxCoords, part === 1 ? [] : maxSize].flat().join());
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
