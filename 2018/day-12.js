254fa8fc682e821460e3bbd21ddefc13ae2544ba
Leyan Lo <leyanlo@gmail.com>
2022-06-05 19:25:39 -0700

    feat(2018): day 12 part 2

--- 2018/day-12.js
+++ 2018/day-12.js
@@ -17,7 +17,6 @@ var input = `initial state: #..#.#..##......###...###
 ###.# => #
 ####. => #`;
 var input = fs.readFileSync('./day-12-input.txt', 'utf8').trimEnd();
-// 3277 too low
 
 function solve(input) {
   let [state, rules] = input.replace(/\./g, ' ').split('\n\n');
@@ -29,8 +28,10 @@ function solve(input) {
       acc[left] = right;
       return acc;
     }, {});
-  console.log(state, rules);
-  for (let i = 0; i < 20; i++) {
+  let sum = 0;
+  let prevDiff;
+  let sameCount = 0;
+  for (let i = 0; i < 1000; i++) {
     state = state.padStart(state.length + 3, ' ');
     state = state.padEnd(state.length + 3, ' ');
     const nextState = [];
@@ -38,13 +39,22 @@ function solve(input) {
       nextState.push(rules[state.substring(i, i + 5)] ?? ' ');
     }
     state = nextState.join('');
-    console.log(state.replace(/ /g, '.'));
+    const nextSum = nextState
+      .map((c, idx) => (c === '#' ? idx - i - 1 : 0))
+      .reduce((acc, n) => acc + n);
+    const diff = nextSum - sum;
+    console.log(i, nextSum, diff, state);
+    if (diff === prevDiff) {
+      sameCount++;
+      if (sameCount === 10) {
+        console.log(sum + diff * (50000000000 - i));
+        break;
+      }
+    } else {
+      sameCount = 0;
+    }
+    prevDiff = diff;
+    sum = nextSum;
   }
-  console.log(
-    state
-      .split('')
-      .map((c, i) => (c === '#' ? i - 20 : 0))
-      .reduce((acc, n) => acc + n)
-  );
 }
 solve(input);
