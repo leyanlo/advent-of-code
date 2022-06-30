8e1775d28b64f0390821590d08011c23f5d8b397
Leyan Lo <leyanlo@gmail.com>
2022-06-29 23:32:29 -0700

    feat(2018): day 20 part 2

--- 2018/day-20.js
+++ 2018/day-20.js
@@ -1,27 +1,38 @@
 const fs = require('fs');
 
-var input = `^WNE$`;
-var input = `^ENWWW(NEEE|SSE(EE|N))$`;
-var input = `^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$`;
-var input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd();
-// 3280 too high
-// 59 wrong
+const input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd();
 
-const branchRegex = /\([^()]+\)/;
 function solve(input) {
-  console.log(input);
-  let longestPath = input.slice(1, -1);
-  while (branchRegex.test(longestPath)) {
-    longestPath = longestPath.replace(branchRegex, (str) => {
-      return str
-        .split(/\W/)
-        .slice(1, -1)
-        .reduce((acc, s) =>
-          !acc || !s ? '' : acc.length > s.length ? acc : s
+  let x = 0;
+  let y = 0;
+  const map = { [y]: { [x]: 0 } };
+  const prevCoords = [[x, y]];
+  for (let i = 1; i < input.length - 1; i++) {
+    const char = input[i];
+    switch (char) {
+      case '(':
+        prevCoords.push([x, y]);
+        break;
+      case '|':
+        [x, y] = prevCoords[prevCoords.length - 1];
+        break;
+      case ')':
+        [x, y] = prevCoords.pop();
+        break;
+      default:
+        const dy = char === 'N' ? -1 : char === 'S' ? 1 : 0;
+        const dx = char === 'E' ? 1 : char === 'W' ? -1 : 0;
+        map[y + dy] = map[y + dy] ?? {};
+        map[y + dy][x + dx] = Math.min(
+          map[y + dy][x + dx] ?? Infinity,
+          map[y][x] + 1
         );
-    });
+        x += dx;
+        y += dy;
+    }
   }
-  console.log(longestPath);
-  console.log(longestPath.length);
+  const lengths = Object.values(map).flatMap((row) => Object.values(row));
+  console.log(Math.max(...lengths));
+  console.log(lengths.filter((length) => length >= 1000).length);
 }
 solve(input);
