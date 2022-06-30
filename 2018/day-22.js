cda1cf25f551561192dd8ea77c1c2f0752c93457
Leyan Lo <leyanlo@gmail.com>
2022-06-30 16:35:39 -0700

    chore(2018): day 22 part 1 cleanup

--- 2018/day-22.js
+++ 2018/day-22.js
@@ -4,45 +4,41 @@ var input = `depth: 510
 target: 10,10`;
 var input = fs.readFileSync('./day-22-input.txt', 'utf8').trimEnd();
 
-const modulo = 20183;
-
 function solve(input) {
-  console.log(input);
   let [depth, target] = input.split('\n');
   depth = +depth.match(/\d+/g)[0];
   const [targetX, targetY] = target.match(/\d+/g).map(Number);
-  console.log(depth, target);
 
-  function getErosion(geoIdx) {
-    return (geoIdx + depth) % 20183;
+  function getCell(geoIdx) {
+    const erosion = (geoIdx + depth) % 20183;
+    const type = erosion % 3;
+    return {
+      geoIdx,
+      erosion,
+      type,
+    };
   }
 
   const map = [];
   for (let y = 0; y <= targetY; y++) {
-    const geoIdx = y * 48271;
-    const erosion = getErosion(geoIdx);
-    map[y] = [{ geoIdx, erosion }];
+    map[y] = [getCell(y * 48271)];
   }
   for (let x = 1; x <= targetX; x++) {
-    const geoIdx = x * 16807;
-    const erosion = getErosion(geoIdx);
-    map[0][x] = { geoIdx, erosion };
+    map[0][x] = getCell(x * 16807);
   }
   for (let y = 1; y <= targetY; y++) {
     for (let x = 1; x <= targetX; x++) {
-      let geoIdx;
-      if (x === targetX && y === targetY) {
-        geoIdx = 0;
-      } else {
-        geoIdx = map[y][x - 1].erosion * map[y - 1][x].erosion;
-      }
-      const erosion = getErosion(geoIdx);
-      map[y][x] = { geoIdx, erosion };
+      map[y][x] = getCell(
+        x === targetX && y === targetY
+          ? 0
+          : map[y][x - 1].erosion * map[y - 1][x].erosion
+      );
     }
   }
   console.log(
     map
-      .flatMap((row) => row.map(({ erosion }) => erosion % 3))
+      .slice(0, targetY + 1)
+      .flatMap((row) => row.slice(0, targetX + 1).map(({ type }) => type))
       .reduce((acc, type) => acc + type)
   );
 }
