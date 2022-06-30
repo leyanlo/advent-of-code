b6458e6e977560755f511b02ecb96ba5a5cf9585
Leyan Lo <leyanlo@gmail.com>
2022-06-30 16:44:02 -0700

    feat(2018): day 22 part 2 wip

--- 2018/day-22.js
+++ 2018/day-22.js
@@ -2,43 +2,49 @@ const fs = require('fs');
 
 var input = `depth: 510
 target: 10,10`;
-var input = fs.readFileSync('./day-22-input.txt', 'utf8').trimEnd();
+// var input = fs.readFileSync('./day-22-input.txt', 'utf8').trimEnd();
+
+const dirs = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
 
 function solve(input) {
   let [depth, target] = input.split('\n');
   depth = +depth.match(/\d+/g)[0];
   const [targetX, targetY] = target.match(/\d+/g).map(Number);
 
-  function getCell(geoIdx) {
-    const erosion = (geoIdx + depth) % 20183;
-    const type = erosion % 3;
-    return {
-      geoIdx,
-      erosion,
-      type,
-    };
+  function getErosion(geoIdx) {
+    return (geoIdx + depth) % 20183;
   }
 
-  const map = [];
-  for (let y = 0; y <= targetY; y++) {
-    map[y] = [getCell(y * 48271)];
+  const erosions = [];
+  // TODO: test higher bounds
+  for (let y = 0; y <= targetY + 2; y++) {
+    erosions[y] = [getErosion(y * 48271)];
   }
-  for (let x = 1; x <= targetX; x++) {
-    map[0][x] = getCell(x * 16807);
+  // TODO: test higher bounds
+  for (let x = 1; x <= targetX + 2; x++) {
+    erosions[0][x] = getErosion(x * 16807);
   }
   for (let y = 1; y <= targetY; y++) {
     for (let x = 1; x <= targetX; x++) {
-      map[y][x] = getCell(
+      erosions[y][x] = getErosion(
         x === targetX && y === targetY
           ? 0
-          : map[y][x - 1].erosion * map[y - 1][x].erosion
+          : erosions[y][x - 1] * erosions[y - 1][x]
       );
     }
   }
+
+  const types = erosions.map((row) => row.map((erosion) => erosion % 3));
+
   console.log(
-    map
+    types
       .slice(0, targetY + 1)
-      .flatMap((row) => row.slice(0, targetX + 1).map(({ type }) => type))
+      .flatMap((row) => row.slice(0, targetX + 1))
       .reduce((acc, type) => acc + type)
   );
 }
