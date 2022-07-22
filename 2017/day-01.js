6543809d9be5639d84d91b7dcb3594cb31f56c4c
Leyan Lo <leyanlo@gmail.com>
2022-07-21 17:19:33 -0700

    feat(2017): day 1 part 2

--- 2017/day-01.js
+++ 2017/day-01.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 
-var input = `91212129`;
+var input = `12131415`;
 var input = fs.readFileSync('./day-01-input.txt', 'utf8').trimEnd();
 
-function solve(input) {
+function solve(input, offset) {
   console.log(input);
   let sum = 0;
   for (let i = 0; i < input.length; i++) {
-    sum += input[i] * (input[i] === input[(i + 1) % input.length]);
+    sum += input[i] * (input[i] === input[(i + offset) % input.length]);
   }
   console.log(sum);
 }
-solve(input);
+solve(input, 1);
+solve(input, input.length / 2);
