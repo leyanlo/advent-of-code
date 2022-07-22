5bfb6ffb99a3bad50d50543b10eec44390cbbeba
Leyan Lo <leyanlo@gmail.com>
2022-07-22 16:05:40 -0700

    feat(2017): day 5 part 2

--- 2017/day-05.js
+++ 2017/day-05.js
@@ -6,14 +6,24 @@ var input = `0
 1
 -3`;
 var input = fs.readFileSync('./day-05-input.txt', 'utf8').trimEnd();
-// 31 is wrong
 
+// function solve(input) {
+//   const offsets = input.split('\n').map(Number);
+//   let step = 0;
+//   let i = 0;
+//   while (0 <= i && i < offsets.length) {
+//     i += offsets[i]++;
+//     step++;
+//   }
+//   console.log(step, offsets);
+// }
+// solve(input);
 function solve(input) {
   const offsets = input.split('\n').map(Number);
   let step = 0;
   let i = 0;
   while (0 <= i && i < offsets.length) {
-    i += offsets[i]++;
+    i += offsets[i] >= 3 ? offsets[i]-- : offsets[i]++;
     step++;
   }
   console.log(step, offsets);
