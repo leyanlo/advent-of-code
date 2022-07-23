bcf1df504e92b8cd2bef2fca96efa21f849e5efa
Leyan Lo <leyanlo@gmail.com>
2022-07-23 00:18:08 -0700

    feat(2017): day 7 part 2

--- 2017/day-07.js
+++ 2017/day-07.js
@@ -1,30 +1,55 @@
 const fs = require('fs');
 
-var input = `pbga (66)
-xhth (57)
-ebii (61)
-havc (66)
-ktlj (57)
-fwft (72) -> ktlj, cntj, xhth
-qoyq (66)
-padx (45) -> pbga, havc, qoyq
-tknk (41) -> ugml, padx, fwft
-jptl (61)
-ugml (68) -> gyxo, ebii, jptl
-gyxo (61)
-cntj (57)`;
-var input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
+
+function addTotalWeight(programs, bottomName) {
+  const program = programs[bottomName];
+  const aboveWeights = program.aboveNames.reduce((acc, name) => {
+    acc[name] = addTotalWeight(programs, name);
+    return acc;
+  }, {});
+  const counts = Object.values(aboveWeights).reduce((acc, weight) => {
+    acc[weight] = (acc[weight] ?? 0) + 1;
+    return acc;
+  }, {});
+  if (Object.keys(counts).length === 2) {
+    const wrongWeight = +Object.entries(counts).filter(
+      ([, count]) => count === 1
+    )[0][0];
+    const rightWeight = +Object.entries(counts).filter(
+      ([, count]) => count !== 1
+    )[0][0];
+    const wrongName = Object.entries(aboveWeights).filter(
+      ([, weight]) => weight === wrongWeight
+    )[0][0];
+    console.log(programs[wrongName].weight + rightWeight - wrongWeight);
+    // update value to be correct to avoid other errors
+    aboveWeights[wrongName] = rightWeight;
+  }
+  program.totalWeight = Object.values(aboveWeights).reduce(
+    (acc, n) => acc + n,
+    program.weight
+  );
+  return program.totalWeight;
+}
 
 function solve(input) {
-  const programs = input.split('\n').map((line) => {
+  const programs = input.split('\n').reduce((acc, line) => {
     const [left, right] = line.split(' -> ');
     let [, name, weight] = left.match(/^(\w+) \((\d+)\)$/);
     weight = +weight;
     const aboveNames = right?.split(', ') ?? [];
-    return { name, weight, aboveNames };
-  });
-  const names = programs.map(({ name }) => name);
-  const aboveNames = programs.flatMap(({ aboveNames }) => aboveNames);
-  console.log(names.filter((name) => !aboveNames.includes(name))[0]);
+    acc[name] = { weight, aboveNames };
+    return acc;
+  }, {});
+  const aboveNames = Object.values(programs).flatMap(
+    ({ aboveNames }) => aboveNames
+  );
+  const bottomName = Object.keys(programs).filter(
+    (name) => !aboveNames.includes(name)
+  )[0];
+  console.log(bottomName);
+
+  addTotalWeight(programs, bottomName);
 }
 solve(input);
