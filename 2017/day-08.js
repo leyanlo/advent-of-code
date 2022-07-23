c5ecd839752915070d43eab594dcc9879de29f56
Leyan Lo <leyanlo@gmail.com>
2022-07-23 00:29:45 -0700

    feat(2017): day 8 part 1

---
+++ 2017/day-08.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+var input = `b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10`;
+var input = fs.readFileSync('./day-08-input.txt', 'utf8').trimEnd();
+
+function solve(input) {
+  const registers = {};
+  for (const line of input.split('\n')) {
+    let [b, cmd, delta, , a, cmp, value] = line.split(' ');
+    delta = +delta;
+    value = +value;
+    if (eval(`${registers[a] ?? 0}${cmp}${value}`)) {
+      registers[b] = (registers[b] ?? 0) + (cmd === 'inc' ? 1 : -1) * delta;
+    }
+  }
+  console.log(Math.max(...Object.values(registers)));
+}
+solve(input);
