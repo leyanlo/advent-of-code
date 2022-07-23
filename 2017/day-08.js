3cf9cea735970b6149d4bb0a73b0a2626e9d49de
Leyan Lo <leyanlo@gmail.com>
2022-07-23 00:30:45 -0700

    feat(2017): day 8

--- 2017/day-08.js
+++ 2017/day-08.js
@@ -8,14 +8,17 @@ var input = fs.readFileSync('./day-08-input.txt', 'utf8').trimEnd();
 
 function solve(input) {
   const registers = {};
+  let highestValue = 0;
   for (const line of input.split('\n')) {
     let [b, cmd, delta, , a, cmp, value] = line.split(' ');
     delta = +delta;
     value = +value;
     if (eval(`${registers[a] ?? 0}${cmp}${value}`)) {
       registers[b] = (registers[b] ?? 0) + (cmd === 'inc' ? 1 : -1) * delta;
+      highestValue = Math.max(highestValue, registers[b]);
     }
   }
   console.log(Math.max(...Object.values(registers)));
+  console.log(highestValue);
 }
 solve(input);
