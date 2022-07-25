a59c24c0952c24bbd53c0a66df78d5b6559a5b54
Leyan Lo <leyanlo@gmail.com>
2022-07-25 13:36:16 -0700

    feat(2017): day 11 part 2

--- 2017/day-11.js
+++ 2017/day-11.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 
-var input = `ne,ne,ne`;
-var input = `ne,ne,sw,sw`;
-var input = `ne,ne,s,s`;
-var input = `se,sw,se,sw,sw`;
-var input = fs.readFileSync('./day-11-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-11-input.txt', 'utf8').trimEnd();
 
 const toCartesian = {
   n: [0, 1],
@@ -15,14 +11,20 @@ const toCartesian = {
   nw: [-1, 0.5],
 };
 
+function getDist(x, y) {
+  return Math.abs(x) + Math.max(0, Math.abs(y) - Math.abs(x / 2));
+}
+
 function solve(input) {
-  console.log(input.split(','));
   let [x, y] = [0, 0];
+  let maxDist = 0;
   for (const dir of input.split(',')) {
     const [dx, dy] = toCartesian[dir];
     x += dx;
     y += dy;
+    maxDist = Math.max(maxDist, getDist(x, y));
   }
-  console.log(Math.abs(x) + Math.max(0, Math.abs(y) - Math.abs(x / 2)));
+  console.log(getDist(x, y));
+  console.log(maxDist);
 }
 solve(input);
