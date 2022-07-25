47e7c173204029212aff843b8ac9a4f85d2c1606
Leyan Lo <leyanlo@gmail.com>
2022-07-24 21:32:36 -0700

    chore(2017): day 10 cleanup

--- 2017/day-10.js
+++ 2017/day-10.js
@@ -1,28 +1,27 @@
 const fs = require('fs');
 
-var input = `AoC 2017`,
-  listSize = 256;
-var input = fs.readFileSync('./day-10-input.txt', 'utf8').trimEnd(),
-  listSize = 256;
+const input = fs.readFileSync('./day-10-input.txt', 'utf8').trimEnd();
+const listSize = 256;
 
-// function solve(input, listSize) {
-//   const lengths = input.split(',').map(Number);
-//   let list = [...Array(listSize).keys()];
-//   let i = 0;
-//   let skip = 0;
-//   for (const length of lengths) {
-//     for (let di = 0; di < ~~(length / 2); di++) {
-//       const tmp = list[(i + di) % listSize];
-//       list[(i + di) % listSize] = list[(i + length - 1 - di) % listSize];
-//       list[(i + length - 1 - di) % listSize] = tmp;
-//     }
-//     i += length + skip;
-//     skip++;
-//   }
-//   console.log(list[0] * list[1]);
-// }
-// solve(input, listSize);
-function solve(input, listSize) {
+function solve1(input, listSize) {
+  const lengths = input.split(',').map(Number);
+  let list = [...Array(listSize).keys()];
+  let i = 0;
+  let skip = 0;
+  for (const length of lengths) {
+    for (let di = 0; di < ~~(length / 2); di++) {
+      const tmp = list[(i + di) % listSize];
+      list[(i + di) % listSize] = list[(i + length - 1 - di) % listSize];
+      list[(i + length - 1 - di) % listSize] = tmp;
+    }
+    i += length + skip;
+    skip++;
+  }
+  console.log(list[0] * list[1]);
+}
+solve1(input, listSize);
+
+function solve2(input, listSize) {
   const lengths = input
     .split('')
     .map((char) => char.codePointAt(0))
@@ -42,11 +41,9 @@ function solve(input, listSize) {
       skip++;
     }
   }
-  console.log(list);
   const dense = [...Array(16).keys()].map((i) =>
     list.slice(16 * i, 16 * (i + 1)).reduce((acc, n) => acc ^ n)
   );
-  console.log(dense);
   console.log(dense.map((n) => n.toString(16).padStart(2, '0')).join(''));
 }
-solve(input, listSize);
+solve2(input, listSize);
