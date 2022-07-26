522f349396624047349cc7fba847352c9aab5ed9
Leyan Lo <leyanlo@gmail.com>
2022-07-26 15:02:19 -0700

    feat(2017): day 13 part 2

--- 2017/day-13.js
+++ 2017/day-13.js
@@ -1,33 +1,41 @@
 const fs = require('fs');
 
-var input = `0: 3
-1: 2
-4: 4
-6: 4`;
-var input = fs.readFileSync('./day-13-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-13-input.txt', 'utf8').trimEnd();
+
+function getCaughtMap(ranges, delay) {
+  let caughtMap = ranges.map(() => false);
+  for (let depth = 0; depth <= ranges.length; depth++) {
+    const range = ranges[depth];
+    if (!range) {
+      continue;
+    }
+    const tMod = (depth + delay) % (2 * range - 2);
+    const pos = Math.min(tMod, 2 * range - 2 - tMod);
+    if (!pos) {
+      caughtMap[depth] = true;
+    }
+  }
+  return caughtMap;
+}
 
 function solve(input) {
-  const map = input
+  const ranges = input
     .split('\n')
     .map((line) => line.split(': ').map(Number))
     .reduce((acc, [left, right]) => {
       acc[left] = right;
       return acc;
     }, []);
-  console.log(map);
+  console.log(
+    getCaughtMap(ranges, 0)
+      .map((caught, i) => caught * i * ranges[i])
+      .reduce((acc, n) => acc + n)
+  );
 
-  let severity = 0;
-  for (let depth = 0; depth <= map.length; depth++) {
-    const range = map[depth];
-    if (!range) {
-      continue;
-    }
-    const tMod = depth % (2 * range - 2);
-    const pos = Math.min(tMod, 2 * range - 2 - tMod);
-    if (!pos) {
-      severity += depth * range
-    }
+  let delay = 1;
+  while (getCaughtMap(ranges, delay).some(Boolean)) {
+    delay++;
   }
-  console.log(severity)
+  console.log(delay);
 }
 solve(input);
