fb6a4b317520e6f204e2f191bc9a90e1202a2cd8
Leyan Lo <leyanlo@gmail.com>
2022-07-27 16:30:30 -0700

    feat(2017): day 16 part 2

--- 2017/day-16.js
+++ 2017/day-16.js
@@ -3,33 +3,48 @@ const fs = require('fs');
 const input = fs.readFileSync('./day-16-input.txt', 'utf8').trimEnd();
 
 function solve(input) {
+  const moves = input.split(',');
   let programs = [...Array(16).keys()].map((i) =>
     String.fromCharCode('a'.charCodeAt(0) + i)
   );
-  for (const move of input.split(',')) {
-    switch (move[0]) {
-      case 's': {
-        const x = +move.substring(1);
-        programs = [...programs.slice(-x), ...programs.slice(0, -x)];
-        break;
-      }
-      case 'x': {
-        const [a, b] = move.substring(1).split('/').map(Number);
-        const tmp = programs[a];
-        programs[a] = programs[b];
-        programs[b] = tmp;
-        break;
-      }
-      case 'p': {
-        const [a, b] = move.substring(1).split('/');
-        const [ai, bi] = [a, b].map((program) => programs.indexOf(program));
-        const tmp = programs[ai];
-        programs[ai] = programs[bi];
-        programs[bi] = tmp;
-        break;
+  const seen = [programs.join('')];
+  let loopLength = null;
+  while (true) {
+    for (const move of moves) {
+      switch (move[0]) {
+        case 's': {
+          const x = +move.substring(1);
+          programs = [...programs.slice(-x), ...programs.slice(0, -x)];
+          break;
+        }
+        case 'x': {
+          const [a, b] = move.substring(1).split('/').map(Number);
+          const tmp = programs[a];
+          programs[a] = programs[b];
+          programs[b] = tmp;
+          break;
+        }
+        case 'p': {
+          const [a, b] = move.substring(1).split('/');
+          const [ai, bi] = [a, b].map((program) => programs.indexOf(program));
+          const tmp = programs[ai];
+          programs[ai] = programs[bi];
+          programs[bi] = tmp;
+          break;
+        }
       }
     }
+    const next = programs.join('');
+    if (seen.includes(next)) {
+      loopLength = seen.length - seen.indexOf(next);
+      break;
+    }
+    seen.push(next);
   }
-  console.log(programs.join(''))
+  console.log(seen[0]);
+
+  console.log(
+    seen[(seen.length - loopLength + (1000000000 % loopLength)) % seen.length]
+  );
 }
 solve(input);
