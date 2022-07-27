81511c42151419e73fbc88bded38dd1f905d7216
Leyan Lo <leyanlo@gmail.com>
2022-07-26 23:20:23 -0700

    chore(2017): day 15 cleanup

--- 2017/day-15.js
+++ 2017/day-15.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 
-var input = `Generator A starts with 65
-Generator B starts with 8921`;
-var input = fs.readFileSync('./day-15-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-15-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   let [a, b] = input.split('\n').map((line) => +line.match(/\d+/g)[0]);
-//   let nMatches = 0;
-//   for (let i = 0; i < 40000000; i++) {
-//     a = (a * 16807) % 2147483647;
-//     b = (b * 48271) % 2147483647;
-//     nMatches += (a % 65536 === b % 65536);
-//   }
-//   console.log(nMatches);
-// }
-// solve(input);
-function solve(input) {
+function solve1(input) {
+  let [a, b] = input.split('\n').map((line) => +line.match(/\d+/g)[0]);
+  let nMatches = 0;
+  for (let i = 0; i < 40000000; i++) {
+    a = (a * 16807) % 2147483647;
+    b = (b * 48271) % 2147483647;
+    nMatches += a % 65536 === b % 65536;
+  }
+  console.log(nMatches);
+}
+solve1(input);
+
+function solve2(input) {
   let [a, b] = input.split('\n').map((line) => +line.match(/\d+/g)[0]);
   let nMatches = 0;
   for (let i = 0; i < 5000000; i++) {
@@ -31,4 +30,4 @@ function solve(input) {
   }
   console.log(nMatches);
 }
-solve(input);
+solve2(input);
