0718c5ee36cc9303aa28f4717f6f12261fc39fbf
Leyan Lo <leyanlo@gmail.com>
2022-07-26 20:30:44 -0700

    chore(2017): day 15 part 2

--- 2017/day-15.js
+++ 2017/day-15.js
@@ -4,13 +4,30 @@ var input = `Generator A starts with 65
 Generator B starts with 8921`;
 var input = fs.readFileSync('./day-15-input.txt', 'utf8').trimEnd();
 
+// function solve(input) {
+//   let [a, b] = input.split('\n').map((line) => +line.match(/\d+/g)[0]);
+//   let nMatches = 0;
+//   for (let i = 0; i < 40000000; i++) {
+//     a = (a * 16807) % 2147483647;
+//     b = (b * 48271) % 2147483647;
+//     nMatches += (a % 65536 === b % 65536);
+//   }
+//   console.log(nMatches);
+// }
+// solve(input);
 function solve(input) {
   let [a, b] = input.split('\n').map((line) => +line.match(/\d+/g)[0]);
   let nMatches = 0;
-  for (let i = 0; i < 40000000; i++) {
+  for (let i = 0; i < 5000000; i++) {
     a = (a * 16807) % 2147483647;
+    while (a % 4 !== 0) {
+      a = (a * 16807) % 2147483647;
+    }
     b = (b * 48271) % 2147483647;
-    nMatches += (a % 65536 === b % 65536);
+    while (b % 8 !== 0) {
+      b = (b * 48271) % 2147483647;
+    }
+    nMatches += a % 65536 === b % 65536;
   }
   console.log(nMatches);
 }
