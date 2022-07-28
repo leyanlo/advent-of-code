a3309b389a544036ecfb71c4de2c3717fd8dd80c
Leyan Lo <leyanlo@gmail.com>
2022-07-28 11:01:50 -0700

    chore(2017): day 17 cleanup

--- 2017/day-17.js
+++ 2017/day-17.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 
-var input = `3`;
-var input = fs.readFileSync('./day-17-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-17-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const steps = +input;
-//   const buffer = [0];
-//   let pos = 0;
-//   for (let i = 1; i <= 2017; i++) {
-//     pos = (pos + 1 + steps) % buffer.length;
-//     buffer.splice(pos + 1, 0, i);
-//   }
-//   console.log(buffer[buffer.indexOf(2017) + 1]);
-// }
-// solve(input);
-function solve(input) {
+function solve1(input) {
+  const steps = +input;
+  const buffer = [0];
+  let pos = 0;
+  for (let i = 1; i <= 2017; i++) {
+    pos = (pos + 1 + steps) % buffer.length;
+    buffer.splice(pos + 1, 0, i);
+  }
+  console.log(buffer[buffer.indexOf(2017) + 1]);
+}
+solve1(input);
+
+function solve2(input) {
   const steps = +input;
   let pos = 0;
   let afterZero = 0;
@@ -26,4 +26,4 @@ function solve(input) {
   }
   console.log(afterZero);
 }
-solve(input);
+solve2(input);
