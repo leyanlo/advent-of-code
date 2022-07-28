434b7e0fc17b4ae95c676b5f55a377e2cfcfca7c
Leyan Lo <leyanlo@gmail.com>
2022-07-28 11:01:24 -0700

    feat(2017): day 17 part 2

--- 2017/day-17.js
+++ 2017/day-17.js
@@ -3,14 +3,27 @@ const fs = require('fs');
 var input = `3`;
 var input = fs.readFileSync('./day-17-input.txt', 'utf8').trimEnd();
 
+// function solve(input) {
+//   const steps = +input;
+//   const buffer = [0];
+//   let pos = 0;
+//   for (let i = 1; i <= 2017; i++) {
+//     pos = (pos + 1 + steps) % buffer.length;
+//     buffer.splice(pos + 1, 0, i);
+//   }
+//   console.log(buffer[buffer.indexOf(2017) + 1]);
+// }
+// solve(input);
 function solve(input) {
   const steps = +input;
-  const buffer = [0];
   let pos = 0;
-  for (let i = 1; i <= 2017; i++) {
-    pos = (pos + 1 + steps) % buffer.length;
-    buffer.splice(pos + 1, 0, i);
+  let afterZero = 0;
+  for (let i = 1; i <= 50000000; i++) {
+    pos = ((pos + steps) % i) + 1;
+    if (pos === 1) {
+      afterZero = i;
+    }
   }
-  console.log(buffer[buffer.indexOf(2017) + 1]);
+  console.log(afterZero);
 }
 solve(input);
