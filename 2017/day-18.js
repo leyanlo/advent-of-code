8bc9cc4d0904a11fe60797d9aff7b15850c015af
Leyan Lo <leyanlo@gmail.com>
2022-07-29 23:15:09 -0700

    chore(2017): day 18 cleanup

--- 2017/day-18.js
+++ 2017/day-18.js
@@ -1,54 +1,47 @@
 const fs = require('fs');
 
-var input = `snd 1
-snd 2
-snd p
-rcv a
-rcv b
-rcv c
-rcv d`;
-var input = fs.readFileSync('./day-18-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-18-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const instructions = input.split('\n').map((line) => line.split(' '));
-//   let lastPlayed = null;
-//   const registers = {};
-//   outer: for (let i = 0; i < instructions.length; i++) {
-//     let [cmd, x, y] = instructions[i];
-//     if (y) {
-//       y = /[a-z]/.test(y) ? registers[y] ?? 0 : +y;
-//     }
-//     switch (cmd) {
-//       case 'snd': // X plays a sound with a frequency equal to the value of X.
-//         lastPlayed = registers[x];
-//         break;
-//       case 'set': // X Y sets register X to the value of Y.
-//         registers[x] = y;
-//         break;
-//       case 'add': // X Y increases register X by the value of Y.
-//         registers[x] = (registers[x] ?? 0) + y;
-//         break;
-//       case 'mul': // X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
-//         registers[x] = (registers[x] ?? 0) * y;
-//         break;
-//       case 'mod': // X Y sets register X to the remainder of dividing the value contained in register X by the value of Y (that is, it sets X to the result of X modulo Y).
-//         registers[x] = (registers[x] ?? 0) % y;
-//         break;
-//       case 'rcv': // X recovers the frequency of the last sound played, but only when the value of X is not zero. (If it is zero, the command does nothing.)
-//         if (registers[x]) {
-//           console.log(lastPlayed);
-//           break outer;
-//         }
-//         break;
-//       case 'jgz': // X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
-//         if (registers[x]) {
-//           i += y - 1;
-//         }
-//         break;
-//     }
-//   }
-// }
-// solve(input);
+function solve1(input) {
+  const instructions = input.split('\n').map((line) => line.split(' '));
+  let lastPlayed = null;
+  const registers = {};
+  outer: for (let i = 0; i < instructions.length; i++) {
+    let [cmd, x, y] = instructions[i];
+    if (y) {
+      y = /[a-z]/.test(y) ? registers[y] ?? 0 : +y;
+    }
+    switch (cmd) {
+      case 'snd': // X plays a sound with a frequency equal to the value of X.
+        lastPlayed = registers[x];
+        break;
+      case 'set': // X Y sets register X to the value of Y.
+        registers[x] = y;
+        break;
+      case 'add': // X Y increases register X by the value of Y.
+        registers[x] = (registers[x] ?? 0) + y;
+        break;
+      case 'mul': // X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
+        registers[x] = (registers[x] ?? 0) * y;
+        break;
+      case 'mod': // X Y sets register X to the remainder of dividing the value contained in register X by the value of Y (that is, it sets X to the result of X modulo Y).
+        registers[x] = (registers[x] ?? 0) % y;
+        break;
+      case 'rcv': // X recovers the frequency of the last sound played, but only when the value of X is not zero. (If it is zero, the command does nothing.)
+        if (registers[x]) {
+          console.log(lastPlayed);
+          break outer;
+        }
+        break;
+      case 'jgz': // X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
+        if (registers[x]) {
+          i += y - 1;
+        }
+        break;
+    }
+  }
+}
+solve1(input);
 
 function* generator(instructions, id, sndQueue, rcvQueue) {
   const registers = {
@@ -94,18 +87,18 @@ function* generator(instructions, id, sndQueue, rcvQueue) {
 
 const ids = [0, 1];
 
-function solve(input) {
+function solve2(input) {
   const instructions = input.split('\n').map((line) => line.split(' '));
-  const queues = ids.map(() => []);
+  const sndQueues = ids.map(() => []);
   const programs = ids.map((id) =>
-    generator(instructions, id, queues[id], queues[+!id])
+    generator(instructions, id, sndQueues[id], sndQueues[+!id])
   );
-  let sndCounts = ids.map(() => 0);
+  const sndCounts = ids.map(() => 0);
   do {
     for (const id of ids) {
       sndCounts[id] = programs[id].next().value;
     }
-  } while (queues.some((queue) => queue.length));
+  } while (sndQueues.some((sndQueue) => sndQueue.length));
   console.log(sndCounts[1]);
 }
-solve(input);
+solve2(input);
