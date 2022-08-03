a4cde53ab9aacad2911599114bbf975d9f29c90b
Leyan Lo <leyanlo@gmail.com>
2022-08-03 16:47:06 -0700

    chore(2017): day 22 cleanup

--- 2017/day-22.js
+++ 2017/day-22.js
@@ -1,30 +1,26 @@
 const fs = require('fs');
 
-var input = `..#
-#..
-...`;
-var input = fs.readFileSync('./day-22-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-22-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const map = input
-//     .split('\n')
-//     .map((line) => line.split('').map((char) => char === '#'));
-//   let [y, x] = [(map.length - 1) / 2, (map[0].length - 1) / 2];
-//   let [dy, dx] = [-1, 0];
-//   let causedInfection = 0;
-//   for (let i = 0; i < 10000; i++) {
-//     map[y] = map[y] ?? [];
-//     const infected = !!map[y][x];
-//     causedInfection += !infected;
-//     [dy, dx] = infected ? [dx, -dy] : [-dx, dy];
-//     map[y][x] = !map[y][x];
-//     [y, x] = [y + dy, x + dx];
-//   }
-//   console.log(causedInfection);
-// }
-// solve(input);
+function solve1(input) {
+  const map = input
+    .split('\n')
+    .map((line) => line.split('').map((char) => char === '#'));
+  let [y, x] = [(map.length - 1) / 2, (map[0].length - 1) / 2];
+  let [dy, dx] = [-1, 0];
+  let causedInfection = 0;
+  for (let i = 0; i < 10000; i++) {
+    map[y] = map[y] ?? [];
+    [dy, dx] = map[y][x] ? [dx, -dy] : [-dx, dy];
+    map[y][x] = !map[y][x];
+    causedInfection += map[y][x];
+    [y, x] = [y + dy, x + dx];
+  }
+  console.log(causedInfection);
+}
+solve1(input);
 
-function solve(input) {
+function solve2(input) {
   const map = input
     .split('\n')
     .map((line) => line.split('').map((char) => (char === '#') * 2));
@@ -34,7 +30,6 @@ function solve(input) {
   for (let i = 0; i < 10000000; i++) {
     map[y] = map[y] ?? [];
     const state = map[y][x] ?? 0;
-    causedInfection += state === 1;
     [dy, dx] = [
       [-dx, dy],
       [dy, dx],
@@ -42,8 +37,9 @@ function solve(input) {
       [-dy, -dx],
     ][state];
     map[y][x] = ((map[y][x] ?? 0) + 1) % 4;
+    causedInfection += map[y][x] === 2;
     [y, x] = [y + dy, x + dx];
   }
   console.log(causedInfection);
 }
-solve(input);
+solve2(input);
