331a4e3f5daa99c06210b50c51db5cc32b8680cc
Leyan Lo <leyanlo@gmail.com>
2022-08-06 15:43:08 -0700

    feat(2016): day 7 part 2

--- 2016/day-07.js
+++ 2016/day-07.js
@@ -4,6 +4,10 @@ var input = `abba[mnop]qrst
 abcd[bddb]xyyx
 aaaa[qwer]tyui
 ioxxoj[asdfgh]zxcvbn`;
+var input = `aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb`;
 var input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
 
 function hasAbba(str) {
@@ -16,6 +20,38 @@ function hasAbba(str) {
   return false;
 }
 
+// function solve(input) {
+//   console.log(input.split('\n'));
+//   let count = 0;
+//   for (const line of input.split('\n')) {
+//     const insides = line
+//       .match(/\[\w+]/g)
+//       .map((match) => match.substring(1, match.length - 1));
+//     const outsides = line.split(/\[\w+]/);
+//     if (outsides.some(hasAbba) && !insides.some(hasAbba)) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+// solve(input);
+
+function getAbas(str) {
+  const abas = [];
+  for (let i = 0; i <= str.length - 3; i++) {
+    const slice = str.slice(i, i + 3);
+    const [a, b, c] = slice;
+    if (a !== b && a === c) {
+      abas.push(slice);
+    }
+  }
+  return abas;
+}
+
+function getBab(aba) {
+  return `${aba[1]}${aba[0]}${aba[1]}`;
+}
+
 function solve(input) {
   console.log(input.split('\n'));
   let count = 0;
@@ -24,9 +60,13 @@ function solve(input) {
       .match(/\[\w+]/g)
       .map((match) => match.substring(1, match.length - 1));
     const outsides = line.split(/\[\w+]/);
-    if (outsides.some(hasAbba) && !insides.some(hasAbba)) {
+    console.log(outsides.flatMap(getAbas));
+    if (
+      outsides
+        .flatMap(getAbas)
+        .some((aba) => insides.some((inside) => inside.includes(getBab(aba))))
+    )
       count++;
-    }
   }
   console.log(count);
 }
