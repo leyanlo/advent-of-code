9ed154e07ce931755438e4a433483c93d6ef0809
Leyan Lo <leyanlo@gmail.com>
2022-08-05 21:06:38 -0700

    feat(2016): day 3 part 2

--- 2016/day-03.js
+++ 2016/day-03.js
@@ -6,12 +6,23 @@ function solve(input) {
   const triangles = input
     .split('\n')
     .map((line) => line.trim().split(/\s+/).map(Number));
-  console.log(triangles);
   let count = 0;
   for (const triangle of triangles) {
-    const [a, b, c] = triangle.sort((a, b) => a - b);
+    const [a, b, c] = [...triangle].sort((a, b) => a - b);
     if (a + b > c) {
       count++;
+    }
+  }
+  console.log(count);
+
+  count = 0;
+  for (let i = 0; i < triangles.length; i += 3) {
+    for (let j = 0; j < triangles[i].length; j++) {
+      const triangle = triangles.slice(i, i + 3).map((row) => row[j]);
+      const [a, b, c] = triangle.sort((a, b) => a - b);
+      if (a + b > c) {
+        count++;
+      }
     }
   }
   console.log(count);
