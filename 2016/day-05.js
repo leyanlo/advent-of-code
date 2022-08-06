575a77325176c1aa1f4f4fd4bc404a5154a7af63
Leyan Lo <leyanlo@gmail.com>
2022-08-06 12:10:52 -0700

    feat(2016): day 5 part 2

--- 2016/day-05.js
+++ 2016/day-05.js
@@ -4,13 +4,31 @@ const fs = require('fs');
 var input = `abc`;
 var input = fs.readFileSync('./day-05-input.txt', 'utf8').trimEnd();
 
+// function solve(input) {
+//   const chars = [];
+//   let i = 0;
+//   while (chars.length < 8) {
+//     const hash = crypto.createHash('md5').update(`${input}${i}`).digest('hex');
+//     if (hash.startsWith('00000')) {
+//       chars.push(hash[5]);
+//     }
+//     i++;
+//   }
+//   console.log(chars.join(''));
+// }
+// solve(input);
 function solve(input) {
   const chars = [];
   let i = 0;
-  while (chars.length < 8) {
+  let missing = new Set([...Array(8).keys()]);
+  while (missing.size > 0) {
     const hash = crypto.createHash('md5').update(`${input}${i}`).digest('hex');
     if (hash.startsWith('00000')) {
-      chars.push(hash[5]);
+      const idx = +hash[5];
+      if (missing.has(idx)) {
+        chars[idx] = hash[6];
+        missing.delete(idx);
+      }
     }
     i++;
   }
