820961e4522ed651539810a381fbe8ad67422f64
Leyan Lo <leyanlo@gmail.com>
2022-08-07 02:17:39 -0700

    chore(2016): day 9 cleanup

--- 2016/day-09.js
+++ 2016/day-09.js
@@ -1,33 +1,8 @@
 const fs = require('fs');
 
-var input = `X(8x2)(3x3)ABCY`;
-var input = `(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN`;
-var input = fs.readFileSync('./day-09-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-09-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const sequences = [];
-//   for (let i = 0; i < input.length; i++) {
-//     const char = input[i];
-//     if (char === '(') {
-//       const end = input.indexOf(')', i);
-//       const [nChars, times] = input
-//         .slice(i + 1, end)
-//         .split('x')
-//         .map(Number);
-//       const sequence = input.slice(end + 1, end + 1 + nChars);
-//       for (let j = 0; j < times; j++) {
-//         sequences.push(sequence);
-//       }
-//       i += end - i + nChars;
-//     } else {
-//       sequences.push(char);
-//     }
-//   }
-//   console.log(sequences.join('').length);
-// }
-// solve(input);
-
-function decompress(input) {
+function decompress(input, part) {
   let length = 0;
   for (let i = 0; i < input.length; i++) {
     const char = input[i];
@@ -37,7 +12,11 @@ function decompress(input) {
         .slice(i + 1, end)
         .split('x')
         .map(Number);
-      length += times * decompress(input.slice(end + 1, end + 1 + nChars));
+      length +=
+        times *
+        (part === 1
+          ? nChars
+          : decompress(input.slice(end + 1, end + 1 + nChars)));
       i += end - i + nChars;
     } else {
       length++;
@@ -45,7 +24,10 @@ function decompress(input) {
   }
   return length;
 }
-function solve(input) {
-  console.log(decompress(input));
+
+function solve(input, part) {
+  console.log(decompress(input, part));
 }
-solve(input);
+
+solve(input, 1);
+solve(input, 2);
