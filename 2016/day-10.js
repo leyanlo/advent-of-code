8abc3bad2094bc1b3069b61231277017c72a85da
Leyan Lo <leyanlo@gmail.com>
2022-08-07 03:03:17 -0700

    chore(2016): day 10 cleanup

--- 2016/day-10.js
+++ 2016/day-10.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 
-var input = `value 5 goes to bot 2
-bot 2 gives low to bot 1 and high to bot 0
-value 3 goes to bot 1
-bot 1 gives low to output 1 and high to bot 0
-bot 0 gives low to output 2 and high to output 0
-value 2 goes to bot 2`,
-  low = 2,
-  high = 5;
-var input = fs.readFileSync('./day-10-input.txt', 'utf8').trimEnd(),
+const input = fs.readFileSync('./day-10-input.txt', 'utf8').trimEnd(),
   low = 17,
   high = 61;
 
