d60eb6133aacfe338cddeb6c1dadbb945ed4987a
Leyan Lo <leyanlo@gmail.com>
2022-08-06 21:59:03 -0700

    feat(2016): day 8 part 2

--- 2016/day-08.js
+++ 2016/day-08.js
@@ -1,18 +1,11 @@
 const fs = require('fs');
 
-var input = `rect 3x2
-rotate column x=1 by 1
-rotate row y=0 by 4
-rotate column x=1 by 1`,
-  [width, height] = [7, 3];
-var input = fs.readFileSync('./day-08-input.txt', 'utf8').trimEnd(),
+const input = fs.readFileSync('./day-08-input.txt', 'utf8').trimEnd(),
   [width, height] = [50, 6];
 
 const screen = [...Array(height)].map(() => [...Array(width)].fill(0));
 
 function solve(input) {
-  console.log(input);
-  console.log(screen);
   for (const line of input.split('\n')) {
     let [cmd, ...args] = line.split(' ');
     if (cmd === 'rect') {
@@ -42,5 +35,10 @@ function solve(input) {
     }
   }
   console.log(screen.flat().filter(Boolean).length);
+  console.log(
+    screen
+      .map((row) => row.map((pixel) => (pixel ? '#' : '.')).join(''))
+      .join('\n')
+  );
 }
 solve(input, width, height);
