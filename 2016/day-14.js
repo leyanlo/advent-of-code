01bf8e3cae2e9abd9a1666d7e3bee15319fac966
Leyan Lo <leyanlo@gmail.com>
2022-08-08 18:09:29 -0700

    chore(2016): day 14 cleanup

--- 2016/day-14.js
+++ 2016/day-14.js
@@ -1,55 +1,9 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-var input = `abc`;
-var input = fs.readFileSync('./day-14-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-14-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const triples = {
-//     0: [],
-//     1: [],
-//     2: [],
-//     3: [],
-//     4: [],
-//     5: [],
-//     6: [],
-//     7: [],
-//     8: [],
-//     9: [],
-//     a: [],
-//     b: [],
-//     c: [],
-//     d: [],
-//     e: [],
-//     f: [],
-//   };
-//   let i = 0;
-//   const keys = [];
-//   while (keys.length < 64 || i < keys[63] + 1000) {
-//     const hash = crypto.createHash('md5').update(`${input}${i}`).digest('hex');
-//
-//     const quintuples = (hash.match(/([0-f])\1\1\1\1/g) ?? []).map(
-//       (str) => str[0]
-//     );
-//     for (const quintuple of quintuples) {
-//       triples[quintuple] = triples[quintuple].filter((j) => j >= i - 1000);
-//       if (triples[quintuple].length) {
-//         keys.push(...triples[quintuple]);
-//         triples[quintuple].length = 0;
-//         keys.sort((a, b) => a - b);
-//       }
-//     }
-//
-//     const triple = hash.match(/([0-f])\1\1/)?.[1] ?? null;
-//     if (triple) {
-//       triples[triple].push(i);
-//     }
-//     i++;
-//   }
-//   console.log(keys[63]);
-// }
-// solve(input);
-function solve(input) {
+function solve(input, part) {
   const triples = {
     0: [],
     1: [],
@@ -72,8 +26,10 @@ function solve(input) {
   const keys = [];
   while (keys.length < 64 || i < keys[63] + 1000) {
     let hash = crypto.createHash('md5').update(`${input}${i}`).digest('hex');
-    for (let j = 0; j < 2016; j++) {
-      hash = crypto.createHash('md5').update(hash).digest('hex');
+    if (part === 2) {
+      for (let j = 0; j < 2016; j++) {
+        hash = crypto.createHash('md5').update(hash).digest('hex');
+      }
     }
 
     const quintuples = (hash.match(/([0-f])\1\1\1\1/g) ?? []).map(
@@ -96,4 +52,5 @@ function solve(input) {
   }
   console.log(keys[63]);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
