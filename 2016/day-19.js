a3e25262ae624a1f3a86b5437c8a85266206258e
Leyan Lo <leyanlo@gmail.com>
2022-08-11 16:59:43 -0700

    feat(2016): day 19 O(1) solution

--- 2016/day-19.js
+++ 2016/day-19.js
@@ -1,32 +1,20 @@
 const fs = require('fs');
 
-var input = `5`;
-var input = fs.readFileSync('./day-19-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-19-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   console.log(input);
-//   const nElves = +input;
-//   const elves = [...Array(nElves).keys()].map((i) => (i + 1) % nElves);
-//   console.log(elves);
-//   let i = 0;
-//   for (let j = 0; j < nElves - 1; j++) {
-//     elves[i] = elves[elves[i]];
-//     i = elves[i];
-//   }
-//   console.log(i + 1);
-// }
-// solve(input);
-function solve(input) {
-  console.log(input);
+// https://www.youtube.com/watch?v=uCsD3ZGzMgE
+function solve1(input) {
   const nElves = +input;
-  const elves = [...Array(nElves).keys()].map((i) => i + 1);
-  console.log(elves);
-  let i = 0;
-  for (let j = 0; j < nElves - 1; j++) {
-    const remove = (i + ~~(elves.length / 2)) % elves.length;
-    elves.splice(remove, 1);
-    i = (i + (remove > i)) % elves.length;
-  }
-  console.log(elves[0]);
+  const s = nElves.toString(2);
+  console.log(2 * parseInt(s.slice(1), 2) + 1);
 }
-solve(input);
+solve1(input);
+
+function solve2(input) {
+  const nElves = +input;
+  const s = (nElves - 1).toString(3);
+  console.log(
+    +s[0] * (parseInt(s.slice(1), 3) + 1) + (+s[0] - 1) * 3 ** (s.length - 1)
+  );
+}
+solve2(input);
