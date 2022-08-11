c1941af8379b9a05eed0013e41e97c98e2e4ebbb
Leyan Lo <leyanlo@gmail.com>
2022-08-10 17:12:11 -0700

    chore(2016): day 15 cleanup

--- 2016/day-15.js
+++ 2016/day-15.js
@@ -1,23 +1,18 @@
 const fs = require('fs');
 
-var input = `Disc #1 has 5 positions; at time=0, it is at position 4.
-Disc #2 has 2 positions; at time=0, it is at position 1.`;
-var input = fs.readFileSync('./day-15-input.txt', 'utf8').trimEnd();
-// 123524 too low
-// 307650 too low
-// 390012 too high
+const input = fs.readFileSync('./day-15-input.txt', 'utf8').trimEnd();
 
-function mul_inv(a, b) {
-  var b0 = b;
-  var x0 = 0;
-  var x1 = 1;
-  var q, tmp;
-  if (b == 1) {
+function mulInv(a, b) {
+  if (b === 1) {
     return 1;
   }
+
+  const b0 = b;
+  let x0 = 0;
+  let x1 = 1;
   while (a > 1) {
-    q = parseInt(a / b);
-    tmp = a;
+    const q = ~~(a / b);
+    let tmp = a;
     a = b;
     b = tmp % b;
     tmp = x0;
@@ -25,22 +20,17 @@ function mul_inv(a, b) {
     x1 = tmp;
   }
   if (x1 < 0) {
-    x1 = x1 + b0;
+    x1 += b0;
   }
   return x1;
 }
 
 function chineseRemainder(a, n) {
-  var p = 1;
-  var i = 1;
-  var prod = 1;
-  var sm = 0;
-  for (i = 0; i < n.length; i++) {
-    prod = prod * n[i];
-  }
-  for (i = 0; i < n.length; i++) {
-    p = prod / n[i];
-    sm = sm + a[i] * mul_inv(p, n[i]) * p;
+  const prod = n.reduce((acc, n) => acc * n);
+  let sm = 0;
+  for (let i = 0; i < n.length; i++) {
+    const p = prod / n[i];
+    sm += a[i] * mulInv(p, n[i]) * p;
   }
   return sm % prod;
 }
@@ -59,7 +49,6 @@ function solve(input, part) {
       pos: 0,
     });
   }
-  console.log(discs);
   console.log(
     chineseRemainder(
       discs.map(({ n, total, pos }) => 2 * total - n - pos),
