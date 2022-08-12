a6d83b5f3b0ab200bcaf07c436552f9e797754de
Leyan Lo <leyanlo@gmail.com>
2022-08-11 17:23:35 -0700

    chore(2016): day 20 cleanup

--- 2016/day-20.js
+++ 2016/day-20.js
@@ -1,35 +1,14 @@
 const fs = require('fs');
 
-var input = `5-8
-0-2
-4-7`,
-  max = 9;
-var input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd(),
+const input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd(),
   max = 4294967295;
 
-// function solve(input) {
-//   const blacklist = input
-//     .split('\n')
-//     .map((line) => line.split('-').map(Number))
-//     .sort((a, b) => a[0] - b[0]);
-//   console.log(blacklist);
-//   const candidates = blacklist
-//     .map(([, max]) => max + 1)
-//     .filter((ip) => ip <= max);
-//   for (const ip of candidates) {
-//     if (blacklist.every(([min, max]) => ip < min || ip > max)) {
-//       console.log(ip);
-//       break;
-//     }
-//   }
-// }
-// solve(input);
 function solve(input) {
   const blacklist = input
     .split('\n')
     .map((line) => line.split('-').map(Number))
     .sort((a, b) => a[0] - b[0]);
-  console.log(blacklist);
+
   const filtered = [];
   for (const curr of blacklist) {
     const prev = filtered[filtered.length - 1];
@@ -39,7 +18,8 @@ function solve(input) {
       filtered.push(curr);
     }
   }
-  console.log(filtered);
+  console.log(filtered[0][1] + 1);
+
   let count = filtered[0][0];
   for (let i = 1; i < filtered.length; i++) {
     const prev = filtered[i - 1];
