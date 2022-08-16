eca8103fc54833b4b2192eac61f673d116afb90e
Leyan Lo <leyanlo@gmail.com>
2022-08-16 11:35:01 -0700

    feat(2015): day 6 part 2

--- 2015/day-06.js
+++ 2015/day-06.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./day-06-input.txt', 'utf8').trimEnd();
 
-function solve(input) {
+function solve1(input) {
   const grid = [...Array(1000)].map(() => [...Array(1000)].fill(0));
   for (const line of input.split('\n')) {
     let [, cmd, y1, x1, y2, x2] = line.match(
@@ -18,4 +18,26 @@ function solve(input) {
   }
   console.log(grid.flat().reduce((acc, n) => acc + n));
 }
-solve(input);
+solve1(input);
+
+function solve2(input) {
+  const grid = [...Array(1000)].map(() => [...Array(1000)].fill(0));
+  for (const line of input.split('\n')) {
+    let [, cmd, y1, x1, y2, x2] = line.match(
+      /(.+) (\d+),(\d+) through (\d+),(\d+)/
+    );
+    [y1, x1, y2, x2] = [y1, x1, y2, x2].map(Number);
+    for (let y = y1; y <= y2; y++) {
+      for (let x = x1; x <= x2; x++) {
+        grid[y][x] =
+          cmd === 'turn on'
+            ? grid[y][x] + 1
+            : cmd === 'turn off'
+            ? Math.max(grid[y][x] - 1, 0)
+            : grid[y][x] + 2;
+      }
+    }
+  }
+  console.log(grid.flat().reduce((acc, n) => acc + n));
+}
+solve2(input);
