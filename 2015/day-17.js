b555bc0db78ec1672793a39b8b326ca0857b0aea
Leyan Lo <leyanlo@gmail.com>
2022-08-16 20:43:06 -0700

    feat(2015): day 17 part 2

--- 2015/day-17.js
+++ 2015/day-17.js
@@ -2,24 +2,22 @@ require = require('esm')(module);
 const $C = require('js-combinatorics');
 const fs = require('fs');
 
-var input = `20
-15
-10
-5
-5`,
-  total = 25;
-var input = fs.readFileSync('./day-17-input.txt', 'utf8').trimEnd(),
+const input = fs.readFileSync('./day-17-input.txt', 'utf8').trimEnd(),
   total = 150;
 
 function solve(input) {
   const sizes = input.split('\n').map(Number);
-  let nCombos = 0;
+  let totalCombos = 0;
+  let minCombos = 0;
   for (let n = 1; n <= sizes.length; n++) {
     const combinations = [...new $C.Combination(sizes, n)];
-    nCombos += combinations.filter(
+    const nCombos = combinations.filter(
       (combination) => combination.reduce((acc, n) => acc + n) === total
     ).length;
+    minCombos = minCombos || nCombos;
+    totalCombos += nCombos;
   }
-  console.log(nCombos);
+  console.log(totalCombos);
+  console.log(minCombos);
 }
 solve(input);
