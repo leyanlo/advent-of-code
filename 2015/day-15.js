7ce88cd637e44321a48d86c9e66230b796e042c4
Leyan Lo <leyanlo@gmail.com>
2022-08-16 20:04:44 -0700

    chore(2015): day 15 cleanup

--- 2015/day-15.js
+++ 2015/day-15.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 
-var input = `Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
-Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`;
-var input = fs.readFileSync('./day-15-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-15-input.txt', 'utf8').trimEnd();
 
 const props = ['capacity', 'durability', 'flavor', 'texture', 'calories'];
 
-function solve(input) {
-  console.log(input);
+function solve(input, part) {
   const ingredients = input.split('\n').map((line) => {
     const [left, right] = line.split(': ');
     return right.split(', ').reduce((acc, prop) => {
@@ -16,18 +13,17 @@ function solve(input) {
       return acc;
     }, {});
   });
-  console.log(ingredients);
   let maxScore = 0;
   for (let i = 0; i < 100; i++) {
     for (let j = 0; j < 100 - i; j++) {
       for (let k = 0; k < 100 - i - j; k++) {
-        const w = 100 - i - j - k;
+        const l = 100 - i - j - k;
         const calories =
           i * ingredients[0].calories +
           j * ingredients[1].calories +
           k * ingredients[2].calories +
-          w * ingredients[3].calories;
-        if (calories !== 500) {
+          l * ingredients[3].calories;
+        if (part === 2 && calories !== 500) {
           continue;
         }
         maxScore = Math.max(
@@ -40,7 +36,7 @@ function solve(input) {
                 i * ingredients[0][p] +
                   j * ingredients[1][p] +
                   k * ingredients[2][p] +
-                  w * ingredients[3][p]
+                  l * ingredients[3][p]
               )
             )
             .reduce((acc, n) => acc * n)
@@ -50,4 +46,5 @@ function solve(input) {
   }
   console.log(maxScore);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
