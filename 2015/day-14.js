45e06d49afa89b45e1064126f31a0b3d7ee5db21
Leyan Lo <leyanlo@gmail.com>
2022-08-16 17:22:30 -0700

    chore(2015): day 14 cleanup

--- 2015/day-14.js
+++ 2015/day-14.js
@@ -1,38 +1,32 @@
 const fs = require('fs');
 
-var input = `Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
-Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`,
-  total = 1000;
-var input = fs.readFileSync('./day-14-input.txt', 'utf8').trimEnd(),
+const input = fs.readFileSync('./day-14-input.txt', 'utf8').trimEnd(),
   total = 2503;
 
-// function solve(input) {
-//   console.log(input);
-//   const reindeer = input.split('\n').map((line) => {
-//     const [, name, v, tFly, tRest] = line.match(
-//       /(\w+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds./
-//     );
-//     return { name, v: +v, tFly: +tFly, tRest: +tRest };
-//   });
-//   console.log(reindeer);
-//   const dists = reindeer.map(({ name, v, tFly, tRest }) => {
-//     const tCycle = tFly + tRest;
-//     const nCycles = ~~(total / tCycle);
-//     const tRemainder = total % tCycle;
-//     return nCycles * (v * tFly) + Math.min(tRemainder, tFly) * v;
-//   });
-//   console.log(Math.max(...dists))
-// }
-// solve(input);
-function solve(input) {
-  console.log(input);
+function solve1(input) {
   const reindeer = input.split('\n').map((line) => {
     const [, name, v, tFly, tRest] = line.match(
       /(\w+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds./
     );
     return { name, v: +v, tFly: +tFly, tRest: +tRest };
   });
-  console.log(reindeer);
+  const dists = reindeer.map(({ name, v, tFly, tRest }) => {
+    const tCycle = tFly + tRest;
+    const nCycles = ~~(total / tCycle);
+    const tRemainder = total % tCycle;
+    return nCycles * (v * tFly) + Math.min(tRemainder, tFly) * v;
+  });
+  console.log(Math.max(...dists));
+}
+solve1(input);
+
+function solve2(input) {
+  const reindeer = input.split('\n').map((line) => {
+    const [, name, v, tFly, tRest] = line.match(
+      /(\w+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds./
+    );
+    return { name, v: +v, tFly: +tFly, tRest: +tRest };
+  });
   const points = reindeer.reduce((acc, { name }) => {
     acc[name] = 0;
     return acc;
@@ -56,4 +50,4 @@ function solve(input) {
   }
   console.log(Math.max(...Object.values(points)));
 }
-solve(input);
+solve2(input);
