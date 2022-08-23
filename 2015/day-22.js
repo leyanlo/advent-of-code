33aba9bd0b17fb0d43c677868163f0d5c820c6b7
Leyan Lo <leyanlo@gmail.com>
2022-08-23 11:37:31 -0700

    feat(2015): day 22 part 1

---
+++ 2015/day-22.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+
+var input = `Hit Points: 13
+Damage: 8`,
+  player = { hp: 10, mana: 250 };
+var input = `Hit Points: 14
+Damage: 8`,
+  player = { hp: 10, mana: 250 };
+var input = fs.readFileSync('./day-22-input.txt', 'utf8').trimEnd(),
+  player = {
+    hp: 50,
+    mana: 500,
+  };
+
+const costs = {
+  missile: 53,
+  drain: 73,
+  shield: 113,
+  poison: 173,
+  recharge: 229,
+};
+
+function getCost(spells) {
+  return spells.map((spell) => costs[spell]).reduce((acc, n) => acc + n);
+}
+
+function parseInput(input) {
+  const [hp, damage] = input.match(/\d+/g).map(Number);
+  return { hp, damage };
+}
+
+function solve(input) {
+  const boss = parseInput(input);
+  const states = Object.keys(costs).map((spell) => ({
+    players: [{ ...player }, { ...boss }],
+    effectTimers: { shield: 0, poison: 0, recharge: 0 },
+    spells: [spell],
+  }));
+  while (states.length) {
+    const {
+      players: [p1, p2],
+      effectTimers,
+      spells,
+    } = states.shift();
+    const nextSpell = spells[spells.length - 1];
+    const cost = getCost(spells);
+    if (effectTimers[nextSpell] > 0 || costs[nextSpell] > p1.mana) {
+      continue;
+    }
+    p1.mana -= costs[nextSpell];
+    switch (nextSpell) {
+      case 'missile':
+        p2.hp -= 4;
+        break;
+      case 'drain':
+        p2.hp -= 2;
+        p1.hp += 2;
+        break;
+      case 'shield':
+        effectTimers.shield = 7;
+        break;
+      case 'poison':
+        effectTimers.poison = 7;
+        break;
+      case 'recharge':
+        effectTimers.recharge = 6;
+        break;
+    }
+    for (const p of [p2, p1]) {
+      for (const spell of Object.keys(effectTimers)) {
+        effectTimers[spell] = Math.max(0, effectTimers[spell] - 1);
+      }
+      p1.shield = (effectTimers.shield > 0) * 7;
+      if (effectTimers.poison > 0) {
+        p2.hp -= 3;
+      }
+      if (effectTimers.recharge > 0) {
+        p1.mana += 101;
+      }
+      if (p2.hp <= 0) {
+        console.log(cost);
+        return;
+      }
+      if (p === p2) {
+        p1.hp -= p2.damage - (p1.shield ?? 0);
+        if (p1.hp <= 0) {
+          break;
+        }
+      } else {
+        states.push(
+          ...Object.keys(costs).map((spell) => ({
+            players: [{ ...p1 }, { ...p2 }],
+            effectTimers: { ...effectTimers },
+            spells: [...spells, spell],
+          }))
+        );
+      }
+    }
+  }
+}
+solve(input);
