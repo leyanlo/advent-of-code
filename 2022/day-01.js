5da75a4808ff000ba55d26aaa9f702e83eb6a7a4
Leyan Lo <leyanlo@gmail.com>
2022-11-30 21:10:33 -0800

    chore(2022): day 1 cleanup

--- 2022/day-01.js
+++ 2022/day-01.js
@@ -3,18 +3,16 @@ const fs = require('fs');
 const input = fs.readFileSync('./day-01-input.txt', 'utf8').trimEnd();
 
 function solve(input) {
-  const elves = input.split('\n\n').map((elf) =>
-    elf
-      .split('\n')
-      .map(Number)
-      .reduce((acc, n) => acc + n)
-  );
-  console.log(Math.max(...elves));
-  console.log(
-    elves
-      .sort((a, b) => b - a)
-      .slice(0, 3)
-      .reduce((acc, n) => acc + n)
-  );
+  const elves = input
+    .split('\n\n')
+    .map((elf) =>
+      elf
+        .split('\n')
+        .map(Number)
+        .reduce((acc, n) => acc + n)
+    )
+    .sort((a, b) => b - a);
+  console.log(elves[0]);
+  console.log(elves.slice(0, 3).reduce((acc, n) => acc + n));
 }
 solve(input);
