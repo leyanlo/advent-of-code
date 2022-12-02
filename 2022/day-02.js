a6819cb72c5b52d2f9b90a142267dc0c63437ff6
Leyan Lo <leyanlo@gmail.com>
2022-12-01 21:27:54 -0800

    chore(2022): day 2 cleanup

--- 2022/day-02.js
+++ 2022/day-02.js
@@ -1,41 +1,29 @@
 const fs = require('fs');
 
-var input = `A Y
-B X
-C Z`;
-var input = fs.readFileSync('./day-02-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-02-input.txt', 'utf8').trimEnd();
 
-const win = {
+const outcomeScore = {
   A: { X: 3, Y: 6, Z: 0 },
   B: { X: 0, Y: 3, Z: 6 },
   C: { X: 6, Y: 0, Z: 3 },
 };
 
-const shape = { X: 1, Y: 2, Z: 3 };
+const shapeScore = { X: 1, Y: 2, Z: 3 };
 
-// function solve(input) {
-//   let score = 0;
-//   for (const line of input.split('\n')) {
-//     const [a, b] = line.split(' ');
-//     score += win[a][b] + shape[b];
-//   }
-//   console.log(score)
-// }
-// solve(input);
-
-const map2 = {
+const chooseShape = {
   A: { X: 'Z', Y: 'X', Z: 'Y' },
   B: { X: 'X', Y: 'Y', Z: 'Z' },
   C: { X: 'Y', Y: 'Z', Z: 'X' },
 };
 
-function solve(input) {
+function solve(input, part) {
   let score = 0;
   for (const line of input.split('\n')) {
     let [a, b] = line.split(' ');
-    b = map2[a][b];
-    score += win[a][b] + shape[b];
+    if (part === 2) b = chooseShape[a][b];
+    score += outcomeScore[a][b] + shapeScore[b];
   }
   console.log(score);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
