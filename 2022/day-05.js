734c176a341dcde6d343768ca3cb1fa4f7cfeb04
Leyan Lo <leyanlo@gmail.com>
2022-12-04 22:47:23 -0800

    chore(2022): day 5 cleanup
    combine solutions

--- 2022/day-05.js
+++ 2022/day-05.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./day-05-input.txt', 'utf8').trimEnd();
 
-function parseDiagram(diagram) {
+function solve(input, part) {
+  const [diagram, moves] = input.split('\n\n');
   const stacks = [];
   for (const line of diagram.split('\n').slice(0, -1)) {
     for (let i = 0; i < line.length; i += 4) {
@@ -12,30 +13,13 @@ function parseDiagram(diagram) {
       }
     }
   }
-  return stacks;
-}
-
-function solve1(input) {
-  const [diagram, moves] = input.split('\n\n');
-  const stacks = parseDiagram(diagram);
   for (const move of moves.split('\n')) {
     const [n, from, to] = move.match(/\d+/g).map(Number);
-    for (let i = 0; i < n; i++) {
-      stacks[to - 1].push(stacks[from - 1].pop());
-    }
-  }
-  console.log(stacks.map((stack) => stack[stack.length - 1]).join(''));
-}
-solve1(input);
-
-function solve2(input) {
-  const [diagram, moves] = input.split('\n\n');
-  const stacks = parseDiagram(diagram);
-  for (const move of moves.split('\n')) {
-    const [n, from, to] = move.match(/\d+/g).map(Number);
-    stacks[to - 1].push(...stacks[from - 1].slice(-n));
+    const crates = stacks[from - 1].slice(-n);
+    stacks[to - 1].push(...(part === 2 ? crates : crates.reverse()));
     stacks[from - 1].length -= n;
   }
   console.log(stacks.map((stack) => stack[stack.length - 1]).join(''));
 }
-solve2(input);
+solve(input, 1);
+solve(input, 2);
