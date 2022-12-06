caae29f0bb831fbb3d52fd4d18d22f94f296ecef
Leyan Lo <leyanlo@gmail.com>
2022-12-05 21:32:44 -0800

    chore(2022): day 6 cleanup
    Avoid allocating new array

--- 2022/day-06.js
+++ 2022/day-06.js
@@ -5,8 +5,7 @@ const input = fs.readFileSync('./day-06-input.txt', 'utf8').trimEnd();
 function solve(input, part) {
   const n = part === 2 ? 14 : 4;
   for (let i = 0; i < input.length; i++) {
-    const chars = input.slice(i, i + n);
-    if (new Set(chars).size === chars.length) {
+    if (new Set(input.substring(i, i + n)).size === n) {
       console.log(i + n);
       break;
     }
