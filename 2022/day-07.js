592f49eae3ad4081e3e8a8fbefe5640d55b7c854
Leyan Lo <leyanlo@gmail.com>
2022-12-06 22:12:29 -0800

    chore(2022): day 7 cleanup

--- 2022/day-07.js
+++ 2022/day-07.js
@@ -1,123 +1,49 @@
 const fs = require('fs');
 
-var input = `$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k`;
-var input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
-
-function cleanTree(tree) {
-  for (const key in tree) {
-    if (key === '_parent') {
-      delete tree[key];
-    } else if (typeof tree[key] === 'object') {
-      cleanTree(tree[key]);
-    }
-  }
-}
+const input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
 
 function calcSizes(tree) {
   tree._size = 0;
   for (const key in tree) {
-    if (key.startsWith('_')) {
-    } else if (typeof tree[key] === 'object') {
-      calcSizes(tree[key]);
-    } else {
-      tree._size += tree[key];
-    }
+    tree._size +=
+      key[0] === '_'
+        ? 0
+        : typeof tree[key] === 'object'
+        ? calcSizes(tree[key])
+        : tree[key];
   }
-  if (tree._parent) {
-    tree._parent._size += tree._size;
-  }
+  return tree._size;
 }
 
-function findAll(tree) {
+function sumAll(tree) {
   let sum = 0;
   if (tree._size <= 100000) {
     sum += tree._size;
   }
   for (const key in tree) {
-    if (key.startsWith('_')) {
-    } else if (typeof tree[key] === 'object') {
-      sum += findAll(tree[key]);
-    } else {
+    if (key[0] !== '_' && typeof tree[key] === 'object') {
+      sum += sumAll(tree[key]);
     }
   }
   return sum;
 }
 
-function findEnough(tree, toDelete, currBest = tree._size) {
-  if (tree._size >= toDelete && tree._size < currBest) {
-    currBest = tree._size;
+function findSmallest(
+  tree,
+  minDelete = tree._size - 40000000,
+  smallest = tree._size
+) {
+  if (tree._size >= minDelete && tree._size < smallest) {
+    smallest = tree._size;
   }
   for (const key in tree) {
-    if (key.startsWith('_')) {
-    } else if (typeof tree[key] === 'object') {
-      currBest = findEnough(tree[key], toDelete, currBest);
-    } else {
+    if (key[0] !== '_' && typeof tree[key] === 'object') {
+      smallest = findSmallest(tree[key], minDelete, smallest);
     }
   }
-  return currBest;
+  return smallest;
 }
 
-// function solve(input) {
-//   const tree = {};
-//   let currTree = tree;
-//   const lines = input.split('\n');
-//   for (let i = 1; i < lines.length; i++) {
-//     const line = lines[i];
-//     const [, cmd, dir] = line.split(' ');
-//     if (cmd === 'ls') {
-//       while (i < lines.length - 1) {
-//         i++;
-//         const parts = lines[i].split(' ');
-//         if (parts[0] === '$') {
-//           i--;
-//           break;
-//         } else if (parts[0] === 'dir') {
-//           currTree[parts[1]] = {
-//             _parent: currTree,
-//           };
-//         } else {
-//           currTree[parts[1]] = parseInt(parts[0], 10);
-//         }
-//       }
-//     } else {
-//       // cmd === 'cd'
-//       if (dir === '..') {
-//         currTree = currTree._parent;
-//       } else {
-//         currTree = currTree[dir];
-//       }
-//     }
-//   }
-//   // cleanTree(tree);
-//   // console.log(JSON.stringify(tree));
-//   calcSizes(tree);
-//   // cleanTree(tree);
-//   // console.log(JSON.stringify(tree));
-//   console.log(findAll(tree));
-// }
-// solve(input);
 function solve(input) {
   const tree = {};
   let currTree = tree;
@@ -126,8 +52,7 @@ function solve(input) {
     const line = lines[i];
     const [, cmd, dir] = line.split(' ');
     if (cmd === 'ls') {
-      while (i < lines.length - 1) {
-        i++;
+      for (i++; i < lines.length; i++) {
         const parts = lines[i].split(' ');
         if (parts[0] === '$') {
           i--;
@@ -137,25 +62,15 @@ function solve(input) {
             _parent: currTree,
           };
         } else {
-          currTree[parts[1]] = parseInt(parts[0], 10);
+          currTree[parts[1]] = +parts[0];
         }
       }
     } else {
-      // cmd === 'cd'
-      if (dir === '..') {
-        currTree = currTree._parent;
-      } else {
-        currTree = currTree[dir];
-      }
+      currTree = dir === '..' ? currTree._parent : currTree[dir];
     }
   }
-  // cleanTree(tree);
-  // console.log(JSON.stringify(tree));
   calcSizes(tree);
-  // cleanTree(tree);
-  // console.log(JSON.stringify(tree));
-  // console.log(findAll(tree));
-  const toDelete = tree._size - 40000000;
-  console.log(findEnough(tree, toDelete));
+  console.log(sumAll(tree));
+  console.log(findSmallest(tree));
 }
 solve(input);
