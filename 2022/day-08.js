746007673281da8a1754dcbae4aff722576e40ce
Leyan Lo <leyanlo@gmail.com>
2022-12-08 00:14:48 -0800

    chore(2022): day 8 cleanup

--- 2022/day-08.js
+++ 2022/day-08.js
@@ -1,81 +1,61 @@
 const fs = require('fs');
 
-var input = `30373
-25512
-65332
-33549
-35390`;
-var input = fs.readFileSync('./day-08-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-08-input.txt', 'utf8').trimEnd();
 
 const dirs = [
+  [-1, 0],
+  [0, -1],
   [0, 1],
   [1, 0],
-  [0, -1],
-  [-1, 0],
 ];
 
-// function solve(input) {
-//   const map = input.split('\n').map((line) => line.split('').map(Number));
-//   const visibles = map.map((row, i) =>
-//     row.map(
-//       (_, j) =>
-//         +(i === 0 || j === 0 || i === map.length - 1 || j === map[i].length - 1)
-//     )
-//   );
-//   console.log(map);
-//   console.log(visibles);
-//   for (let i = 1; i < map.length - 1; i++) {
-//     for (let j = 1; j < map[i].length - 1; j++) {
-//       outer: for (const [di, dj] of dirs) {
-//         if (di) {
-//           for (let i2 = i + di; i2 >= 0 && i2 < map.length; i2 += di) {
-//             if (map[i][j] <= map[i2][j]) {
-//               continue outer;
-//             }
-//           }
-//         } else {
-//           for (let j2 = j + dj; j2 >= 0 && j2 < map[i].length; j2 += dj) {
-//             if (map[i][j] <= map[i][j2]) {
-//               continue outer;
-//             }
-//           }
-//         }
-//         visibles[i][j] = 1;
-//         break outer;
-//       }
-//     }
-//   }
-//   console.log(visibles);
-//   console.log(visibles.flat().reduce((acc, n) => acc + n));
-// }
-// solve(input);
-function solve(input) {
+function solve1(input) {
   const map = input.split('\n').map((line) => line.split('').map(Number));
-  const scores = map.map((row, i) => row.map((_, j) => 1));
-  console.log(map);
-  for (let i = 1; i < map.length - 1; i++) {
-    for (let j = 1; j < map[i].length - 1; j++) {
+  const visibles = map.map((row) => row.map(() => 0));
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
       outer: for (const [di, dj] of dirs) {
-        if (di) {
-          for (let i2 = i + di; i2 >= 0 && i2 < map.length; i2 += di) {
-            if (map[i][j] <= map[i2][j]) {
-              scores[i][j] *= Math.abs(i - i2);
-              continue outer;
-            }
+        let [i2, j2] = [i + di, j + dj];
+        for (
+          ;
+          i2 >= 0 && j2 >= 0 && i2 < map.length && j2 < map[i2].length;
+          i2 += di, j2 += dj
+        ) {
+          if (map[i2][j2] >= map[i][j]) {
+            continue outer;
           }
-          scores[i][j] *= Math.abs(i - (di > 0 ? map.length - 1 : 0));
-        } else {
-          for (let j2 = j + dj; j2 >= 0 && j2 < map[i].length; j2 += dj) {
-            if (map[i][j] <= map[i][j2]) {
-              scores[i][j] *= Math.abs(j - j2);
-              continue outer;
-            }
-          }
-          scores[i][j] *= Math.abs(j - (dj > 0 ? map[i].length - 1 : 0));
         }
+        visibles[i][j] = 1;
+        break;
+      }
+    }
+  }
+  console.log(visibles.flat().reduce((acc, n) => acc + n));
+}
+solve1(input);
+
+function solve2(input) {
+  const map = input.split('\n').map((line) => line.split('').map(Number));
+  const scores = map.map((row) => row.map(() => 1));
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      for (const [di, dj] of dirs) {
+        let [i2, j2] = [i + di, j + dj];
+        for (
+          ;
+          i2 >= 0 && j2 >= 0 && i2 < map.length && j2 < map[i2].length;
+          i2 += di, j2 += dj
+        ) {
+          if (map[i2][j2] >= map[i][j]) {
+            i2 += di;
+            j2 += dj;
+            break;
+          }
+        }
+        scores[i][j] *= Math.abs(i2 - di - i) + Math.abs(j2 - dj - j);
       }
     }
   }
   console.log(Math.max(...scores.flat()));
 }
-solve(input);
+solve2(input);
