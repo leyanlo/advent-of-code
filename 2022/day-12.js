954b98c9f70f5d4522e4316e0314494e2984fb86
Leyan Lo <leyanlo@gmail.com>
2022-12-11 21:46:35 -0800

    chore(2022-11): clean up

--- 2022/day-12.js
+++ 2022/day-12.js
@@ -1,12 +1,6 @@
 const fs = require('fs');
 
-var input = `Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi`;
-var input = fs.readFileSync('./day-12-input.txt', 'utf8').trimEnd();
-//280 wrong
+const input = fs.readFileSync('./day-12-input.txt', 'utf8').trimEnd();
 
 const dirs = [
   [0, 1],
@@ -15,69 +9,26 @@ const dirs = [
   [-1, 0],
 ];
 
-// function solve(input) {
-//   console.log(input);
-//   let start, end;
-//   const map = input.split('\n').map((line, i) =>
-//     line.split('').map((char, j) => {
-//       let elevation = char.codePointAt(0) - 'a'.codePointAt(0);
-//       if (char === 'S') {
-//         elevation = 0;
-//         start = [i, j];
-//       } else if (char === 'E') {
-//         elevation = 25;
-//         end = [i, j];
-//       }
-//       return elevation;
-//     })
-//   );
-//   console.log(map, start, end);
-//
-//   let queue = [{ pos: [...start], steps: 0 }];
-//   const seen = [];
-//   while (queue[0].pos[0] !== end[0] || queue[0].pos[1] !== end[1]) {
-//     const {
-//       pos: [i, j],
-//       steps,
-//     } = queue.shift();
-//     if (seen[i]?.[j]) {
-//       continue;
-//     }
-//     for (const [di, dj] of dirs) {
-//       if (
-//         map[i + di]?.[j + dj] === undefined ||
-//         map[i + di][j + dj] > map[i][j] + 1 ||
-//         seen[i + di]?.[j + dj]
-//       ) {
-//         continue;
-//       }
-//       queue.push({ pos: [i + di, j + dj], steps: steps + 1 });
-//     }
-//     seen[i] = seen[i] ?? [];
-//     seen[i][j] = 1;
-//     queue.sort((a, b) => a.steps - b.steps);
-//   }
-//   console.log(queue[0].steps);
-// }
-// solve(input);
-function solve(input) {
+function solve(input, part) {
   const starts = [];
   let end;
   const map = input.split('\n').map((line, i) =>
     line.split('').map((char, j) => {
-      let elevation = char.codePointAt(0) - 'a'.codePointAt(0);
-      if (char === 'S' || !elevation) {
+      let elevation;
+      if (char === 'S' || (part === 2 && char === 'a')) {
         elevation = 0;
         starts.push([i, j]);
       } else if (char === 'E') {
         elevation = 25;
         end = [i, j];
+      } else {
+        elevation = char.codePointAt(0) - 'a'.codePointAt(0);
       }
       return elevation;
     })
   );
 
-  let queue = starts.map((start) => ({ pos: [...start], steps: 0 }));
+  const queue = starts.map((start) => ({ pos: start, steps: 0 }));
   const seen = [];
   while (queue[0].pos[0] !== end[0] || queue[0].pos[1] !== end[1]) {
     const {
@@ -99,8 +50,8 @@ function solve(input) {
     }
     seen[i] = seen[i] ?? [];
     seen[i][j] = 1;
-    queue.sort((a, b) => a.steps - b.steps);
   }
   console.log(queue[0].steps);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
