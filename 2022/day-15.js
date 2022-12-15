6d8d86e07f42ac1b5ba85e4fbb37ad3df5011016
Leyan Lo <leyanlo@gmail.com>
2022-12-15 00:42:08 -0800

    chore(2022-08): skip checking parallel lines for part 2

--- 2022/day-15.js
+++ 2022/day-15.js
@@ -65,11 +65,11 @@ function solve2(input) {
     for (let j = i + 1; j < diamonds.length; j++) {
       const d2 = diamonds[j];
       for (let i2 = 0; i2 < 4; i2++) {
-        for (let j2 = 0; j2 < 4; j2++) {
+        for (const j2 of [i2 + 1, i2 + 3]) {
           const [xi, yi] = intersect(
             d1[i2],
             d1[(i2 + 1) % 4],
-            d2[j2],
+            d2[j2 % 4],
             d2[(j2 + 1) % 4]
           );
           if (
