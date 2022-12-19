15278bb5efc4cf17d965c3ad3d4ea22c9c7580b3
Leyan Lo <leyanlo@gmail.com>
2022-12-19 00:54:08 -0800

    feat(2022-19): solve

--- 2022/day-19.js
+++ 2022/day-19.js
@@ -2,74 +2,101 @@ const fs = require('fs');
 
 var input = `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`;
-// var input = fs.readFileSync('./day-19-input.txt', 'utf8').trimEnd();
+var input = fs.readFileSync('./day-19-input.txt', 'utf8').trimEnd();
 
-function solve(input) {
-  const blueprints = input
-    .split('\n')
-    .map((blueprint) => blueprint.match(/\d+/g).map(Number))
-    .map(([, a, b, c, d, e, f]) => [
-      [a, 0, 0, 0],
-      [b, 0, 0, 0],
-      [c, d, 0, 0],
-      [e, 0, f, 0],
-    ]);
-  console.log(...blueprints);
+var input = `Blueprint 1: Each ore robot costs 2 ore. Each clay robot costs 2 ore. Each obsidian robot costs 2 ore and 17 clay. Each geode robot costs 2 ore and 10 obsidian.
+Blueprint 2: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 15 clay. Each geode robot costs 3 ore and 8 obsidian.
+Blueprint 3: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 16 clay. Each geode robot costs 2 ore and 18 obsidian.
+Blueprint 4: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 17 clay. Each geode robot costs 2 ore and 13 obsidian.
+Blueprint 5: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 5 clay. Each geode robot costs 3 ore and 15 obsidian.
+Blueprint 6: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 17 clay. Each geode robot costs 4 ore and 20 obsidian.
+Blueprint 7: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 9 clay. Each geode robot costs 2 ore and 9 obsidian.
+Blueprint 8: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 12 clay. Each geode robot costs 3 ore and 8 obsidian.
+Blueprint 9: Each ore robot costs 2 ore. Each clay robot costs 2 ore. Each obsidian robot costs 2 ore and 8 clay. Each geode robot costs 2 ore and 14 obsidian.
+Blueprint 10: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 12 clay. Each geode robot costs 3 ore and 15 obsidian.
+Blueprint 11: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 19 clay. Each geode robot costs 2 ore and 18 obsidian.
+Blueprint 12: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 16 clay. Each geode robot costs 2 ore and 9 obsidian.
+Blueprint 13: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 18 clay. Each geode robot costs 2 ore and 11 obsidian.
+Blueprint 14: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 18 clay. Each geode robot costs 3 ore and 13 obsidian.
+Blueprint 15: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 9 clay. Each geode robot costs 3 ore and 7 obsidian.
+Blueprint 16: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 7 clay. Each geode robot costs 3 ore and 10 obsidian.
+Blueprint 17: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 6 clay. Each geode robot costs 2 ore and 20 obsidian.
+Blueprint 18: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 19 clay. Each geode robot costs 3 ore and 13 obsidian.
+Blueprint 19: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 14 clay. Each geode robot costs 4 ore and 15 obsidian.
+Blueprint 20: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 9 clay. Each geode robot costs 3 ore and 15 obsidian.
+Blueprint 21: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 10 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 22: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 16 clay. Each geode robot costs 3 ore and 14 obsidian.
+Blueprint 23: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 20 clay. Each geode robot costs 2 ore and 12 obsidian.
+Blueprint 24: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 6 clay. Each geode robot costs 2 ore and 16 obsidian.
+Blueprint 25: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 20 clay. Each geode robot costs 2 ore and 15 obsidian.
+Blueprint 26: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 20 clay. Each geode robot costs 3 ore and 9 obsidian.
+Blueprint 27: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 13 clay. Each geode robot costs 3 ore and 15 obsidian.
+Blueprint 28: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 11 clay. Each geode robot costs 3 ore and 14 obsidian.
+Blueprint 29: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 15 clay. Each geode robot costs 4 ore and 9 obsidian.
+Blueprint 30: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 7 clay. Each geode robot costs 3 ore and 8 obsidian.`;
 
-  for (let i = 0; i < blueprints.length; i++) {
-    const blueprint = blueprints[i];
+function getMaxGeodes(costs, tMax) {
+  let maxGeodes = 0;
+  const queue = [[[1, 0, 0, 0], [0, 0, 0, 0], 0]];
+  const seen = new Set();
+  while (queue.length) {
+    const state = queue.shift();
+    const [robots, collected, t] = state;
 
-    const queue = [[[1, 0, 0, 0], [0, 0, 0, 0], 0]];
-    const ans = [];
-    while (queue.length) {
-      const [nRobots, collected, t] = queue.shift();
+    maxGeodes = Math.max(maxGeodes, collected[3]);
+    if (t === tMax) continue;
 
-      if (t === 24) {
-        ans.push([nRobots, collected, t]);
-        break;
-      }
-
-      // spend
-      const canBuild = [...Array(4)].fill(0);
-      for (let j = 0; j < 4; j++) {
-        const cost = blueprint[j];
-
-        if (cost.every((c, k) => c <= collected[k])) {
-          canBuild[j] = 1;
-        }
-      }
-      if (t === 6) debugger
-
-      // collect
-      nRobots.forEach((n, j) => {
-        collected[j] += n;
-      });
-
-      canBuild.forEach((can, j) => {
-        if (can) {
-          queue.push([
-            nRobots.map((n, k) => (k === j ? n + 1 : n)),
-            collected.map((c, k) => c - blueprint[j][k]),
-            t + 1,
-          ]);
-        }
-      });
-      queue.push([nRobots, collected, t + 1]);
-      queue.sort(
-        (a, b) =>
-          b[0][3] - a[0][3] ||
-          b[0][2] - a[0][2] ||
-          b[0][1] - a[0][1] ||
-          b[0][0] - a[0][0] ||
-          b[1][3] - a[1][3] ||
-          b[1][2] - a[1][2] ||
-          b[1][1] - a[1][1] ||
-          b[1][0] - a[1][0]
+    // cap robots and resources to reduce search space
+    for (let i = 0; i < 3; i++) {
+      robots[i] = Math.min(
+        robots[i],
+        Math.max(...costs.map((cost) => cost[i] ?? 0))
       );
-      // TODO: filter out worse cases
+      collected[i] = Math.min(
+        collected[i],
+        (tMax - t) * Math.max(...costs.map((cost) => cost[i] ?? 0)) -
+          robots[i] * (tMax - t - 1)
+      );
     }
-    console.log(ans);
-    break;
+
+    if (seen.has(JSON.stringify(state))) continue;
+    seen.add(JSON.stringify(state));
+
+    queue.push([robots, collected.map((c, i) => c + robots[i]), t + 1]);
+    costs.forEach((cost, i) => {
+      if (cost.every((c, j) => c <= collected[j])) {
+        queue.push([
+          robots.map((r, j) => r + (j === i)),
+          collected.map((c, j) => c - (cost[j] ?? 0) + robots[j]),
+          t + 1,
+        ]);
+      }
+    });
   }
+  return maxGeodes;
 }
-solve(input);
+
+function solve1(input) {
+  console.log(
+    input
+      .split('\n')
+      .map((line) => line.match(/\d+/g).slice(1).map(Number))
+      .map(([a, b, c, d, e, f]) => [[a], [b], [c, d], [e, 0, f]])
+      .map((costs, i) => (i + 1) * getMaxGeodes(costs, 24))
+      .reduce((acc, n) => acc + n)
+  );
+}
+solve1(input);
+
+function solve2(input) {
+  console.log(
+    input
+      .split('\n')
+      .map((line) => line.match(/\d+/g).slice(1).map(Number))
+      .slice(0, 3)
+      .map(([a, b, c, d, e, f]) => [[a], [b], [c, d], [e, 0, f]])
+      .map((costs) => getMaxGeodes(costs, 32))
+      .reduce((acc, n) => acc * n)
+  );
+}
+solve2(input);
