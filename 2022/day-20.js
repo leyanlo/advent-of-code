51d2db72a5a6d9bd8ba4df537b5371807b21e6f2
Leyan Lo <leyanlo@gmail.com>
2022-12-19 22:04:17 -0800

    feat(2022-20): part 2

--- 2022/day-20.js
+++ 2022/day-20.js
@@ -16,11 +16,29 @@ function swap(nums, i, j) {
   nums[(j + len) % len] = tmp;
 }
 
+// function solve(input) {
+//   const nums = input.split('\n').map(Number);
+//   const file = nums.map((n, idx) => ({ n, idx }));
+//   for (let i = 0; i < file.length; i++) {
+//     const idx = file.findIndex(({ idx }) => idx === i);
+//     const [line] = file.splice(idx, 1);
+//     file.splice((idx + line.n) % file.length, 0, line);
+//   }
+//
+//   const zeroIdx = file.findIndex(({ n }) => n === 0);
+//   console.log(
+//     [1000, 2000, 3000]
+//       .map((di) => file[(zeroIdx + di) % file.length].n)
+//       .reduce((acc, n) => acc + n)
+//   );
+// }
+// solve(input);
 function solve(input) {
   const nums = input.split('\n').map(Number);
-  const file = nums.map((n, idx) => ({ n, idx }));
-  for (let i = 0; i < file.length; i++) {
-    const idx = file.findIndex(({ idx }) => idx === i);
+  const file = nums.map((n, idx) => ({ n: n * 811589153, idx }));
+
+  for (let i = 0; i < file.length * 10; i++) {
+    const idx = file.findIndex(({ idx }) => idx === i % file.length);
     const [line] = file.splice(idx, 1);
     file.splice((idx + line.n) % file.length, 0, line);
   }
