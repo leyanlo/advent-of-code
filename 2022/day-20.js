c2a640974bbd3e39c046672906b683c32e4160b8
Leyan Lo <leyanlo@gmail.com>
2022-12-19 22:30:31 -0800

    chore(2022-20): clean up

--- 2022/day-20.js
+++ 2022/day-20.js
@@ -1,53 +1,43 @@
 const fs = require('fs');
 
-var input = `1
-2
--3
-3
--2
-0
-4`;
-var input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd();
 
-function swap(nums, i, j) {
-  const len = nums.length;
-  const tmp = nums[(i + len) % len];
-  nums[(i + len) % len] = nums[(j + len) % len];
-  nums[(j + len) % len] = tmp;
-}
-
-// function solve(input) {
-//   const nums = input.split('\n').map(Number);
-//   const file = nums.map((n, idx) => ({ n, idx }));
-//   for (let i = 0; i < file.length; i++) {
-//     const idx = file.findIndex(({ idx }) => idx === i);
-//     const [line] = file.splice(idx, 1);
-//     file.splice((idx + line.n) % file.length, 0, line);
-//   }
-//
-//   const zeroIdx = file.findIndex(({ n }) => n === 0);
-//   console.log(
-//     [1000, 2000, 3000]
-//       .map((di) => file[(zeroIdx + di) % file.length].n)
-//       .reduce((acc, n) => acc + n)
-//   );
-// }
-// solve(input);
-function solve(input) {
-  const nums = input.split('\n').map(Number);
-  const file = nums.map((n, idx) => ({ n: n * 811589153, idx }));
-
-  for (let i = 0; i < file.length * 10; i++) {
-    const idx = file.findIndex(({ idx }) => idx === i % file.length);
-    const [line] = file.splice(idx, 1);
-    file.splice((idx + line.n) % file.length, 0, line);
+function solve1(input) {
+  const nums = input
+    .split('\n')
+    .map(Number)
+    .map((n, idx) => ({ n, idx }));
+  for (let i = 0; i < nums.length; i++) {
+    const numIdx = nums.findIndex(({ idx }) => idx === i);
+    const [num] = nums.splice(numIdx, 1);
+    nums.splice((numIdx + num.n) % nums.length, 0, num);
   }
 
-  const zeroIdx = file.findIndex(({ n }) => n === 0);
+  const zeroIdx = nums.findIndex(({ n }) => n === 0);
   console.log(
     [1000, 2000, 3000]
-      .map((di) => file[(zeroIdx + di) % file.length].n)
+      .map((n) => nums[(zeroIdx + n) % nums.length].n)
       .reduce((acc, n) => acc + n)
   );
 }
-solve(input);
+solve1(input);
+
+function solve2(input) {
+  const nums = input
+    .split('\n')
+    .map(Number)
+    .map((n, idx) => ({ n: n * 811589153, idx }));
+  for (let i = 0; i < nums.length * 10; i++) {
+    const numIdx = nums.findIndex(({ idx }) => idx === i % nums.length);
+    const [num] = nums.splice(numIdx, 1);
+    nums.splice((numIdx + num.n) % nums.length, 0, num);
+  }
+
+  const zeroIdx = nums.findIndex(({ n }) => n === 0);
+  console.log(
+    [1000, 2000, 3000]
+      .map((di) => nums[(zeroIdx + di) % nums.length].n)
+      .reduce((acc, n) => acc + n)
+  );
+}
+solve2(input);
