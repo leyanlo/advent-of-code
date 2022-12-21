aa1742c27bc6aae3bdb0e1cfa9ddbdf401b83a7f
Leyan Lo <leyanlo@gmail.com>
2022-12-20 23:34:54 -0800

    chore(2022-21): clean up

--- 2022/day-21.js
+++ 2022/day-21.js
@@ -1,66 +1,10 @@
 const fs = require('fs');
 
-var input = `root: pppw + sjmn
-dbpl: 5
-cczh: sllz + lgvd
-zczc: 2
-ptdq: humn - dvpt
-dvpt: 3
-lfqf: 4
-humn: 5
-ljgn: 2
-sjmn: drzm * dbpl
-sllz: 4
-pppw: cczh / lfqf
-lgvd: ljgn * ptdq
-drzm: hmdt - zczc
-hmdt: 32`;
-var input = fs.readFileSync('./day-21-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-21-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   console.log(input);
-//   const map = {};
-//   let lines = input.split('\n');
-//   while (map.root === undefined) {
-//     for (const line of lines) {
-//       const [monkey, job] = line.split(': ');
-//       if (/\d+/.test(job)) {
-//         map[monkey] = +job;
-//         continue;
-//       }
-//
-//       const [m1, op, m2] = job.split(' ');
-//       if (map[m1] === undefined || map[m2] === undefined) {
-//         continue;
-//       }
-//
-//       map[monkey] = [m1, m2]
-//         .map((m) => map[m])
-//         .reduce((acc, n) =>
-//           op === '+'
-//             ? acc + n
-//             : op === '-'
-//             ? acc - n
-//             : op === '*'
-//             ? acc * n
-//             : acc / n
-//         );
-//     }
-//     lines = lines.filter((line) => map[line.slice(0, 4)] === undefined);
-//   }
-//   console.log(map.root)
-// }
-// solve(input);
-function solve(input) {
+function solve1(input) {
   const map = {};
   let lines = input.split('\n');
-  lines = lines.map((line) =>
-    line.startsWith('root')
-      ? line.replace('+', '=')
-      : line.startsWith('humn')
-      ? line.replace(/\d+/, '3782852515583')
-      : line
-  );
   while (map.root === undefined) {
     for (const line of lines) {
       const [monkey, job] = line.split(': ');
@@ -83,13 +27,57 @@ function solve(input) {
             ? acc - n
             : op === '*'
             ? acc * n
-            : op === '/'
-            ? acc / n
-            : acc - n
+            : acc / n
         );
     }
     lines = lines.filter((line) => map[line.slice(0, 4)] === undefined);
   }
   console.log(map.root);
 }
-solve(input);
+solve1(input);
+
+function solve2(input) {
+  // guess 0 or 1 and see how far both are off
+  const diffs = [0, 1].map((guess) => {
+    const map = {};
+    let lines = input
+      .split('\n')
+      .map((line) =>
+        line.startsWith('root')
+          ? line.replace('+', '-')
+          : line.startsWith('humn')
+          ? line.replace(/\d+/, guess)
+          : line
+      );
+    while (map.root === undefined) {
+      for (const line of lines) {
+        const [monkey, job] = line.split(': ');
+        if (/\d+/.test(job)) {
+          map[monkey] = +job;
+          continue;
+        }
+
+        const [m1, op, m2] = job.split(' ');
+        if (map[m1] === undefined || map[m2] === undefined) {
+          continue;
+        }
+
+        map[monkey] = [m1, m2]
+          .map((m) => map[m])
+          .reduce((acc, n) =>
+            op === '+'
+              ? acc + n
+              : op === '-'
+              ? acc - n
+              : op === '*'
+              ? acc * n
+              : acc / n
+          );
+      }
+      lines = lines.filter((line) => map[line.slice(0, 4)] === undefined);
+    }
+    return map.root;
+  });
+  console.log(diffs[0] / (diffs[0] - diffs[1]));
+}
+solve2(input);
