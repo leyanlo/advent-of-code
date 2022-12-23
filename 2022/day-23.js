2532ffee70e86e714917965663f83a1e14625388
Leyan Lo <leyanlo@gmail.com>
2022-12-22 22:42:05 -0800

    chore(2022-23): clean up

--- 2022/day-23.js
+++ 2022/day-23.js
@@ -1,21 +1,6 @@
 const fs = require('fs');
 
-var input = `.....
-..##.
-..#..
-.....
-..##.
-.....`;
-var input = `....#..
-..###.#
-#...#.#
-.#...##
-#.###..
-##.#.##
-.#..#..`;
-var input = fs.readFileSync('./day-23-input.txt', 'utf8').trimEnd();
-// 6480 wrong
-// 4132 wrong
+const input = fs.readFileSync('./day-23-input.txt', 'utf8').trimEnd();
 
 const neighbors = [
   [-1, -1],
@@ -28,31 +13,41 @@ const neighbors = [
   [1, 1],
 ];
 
-function solve(input) {
-  console.log(input);
-  // const map = input
-  //   .split('\n')
-  //   .map((line) =>
-  //     line
-  //       .split('')
-  //       .map((char) => +(char === '#'))
-  //       .reduce((acc, char, j) => {
-  //         acc[j] = char;
-  //         return acc;
-  //       }, {})
-  //   )
-  //   .reduce((acc, line, i) => {
-  //     acc[i] = line;
-  //     return acc;
-  //   }, {});
+const addProposal = [
+  (map, x, y, proposals) => {
+    if (!map[y - 1]?.[x] && !map[y - 1]?.[x - 1] && !map[y - 1]?.[x + 1]) {
+      proposals.push([[x, y].join(), [x, y - 1].join()]);
+      return true;
+    }
+  },
+  (map, x, y, proposals) => {
+    if (!map[y + 1]?.[x] && !map[y + 1]?.[x - 1] && !map[y + 1]?.[x + 1]) {
+      proposals.push([[x, y].join(), [x, y + 1].join()]);
+      return true;
+    }
+  },
+  (map, x, y, proposals) => {
+    if (!map[y - 1]?.[x - 1] && !map[y][x - 1] && !map[y + 1]?.[x - 1]) {
+      proposals.push([[x, y].join(), [x - 1, y].join()]);
+      return true;
+    }
+  },
+  (map, x, y, proposals) => {
+    if (!map[y - 1]?.[x + 1] && !map[y][x + 1] && !map[y + 1]?.[x + 1]) {
+      proposals.push([[x, y].join(), [x + 1, y].join()]);
+      return true;
+    }
+  },
+];
+
+function solve(input, part) {
   const map = input
     .split('\n')
     .map((line) => line.split('').map((char) => +(char === '#')));
-  // console.log(map);
   const nElves = map.flat().filter(Boolean).length;
 
-  let round = 0;
-  while (true) {
+  let round = 1;
+  while (part === 2 || round <= 10) {
     let proposals = [];
     for (let y in map) {
       y = +y;
@@ -63,56 +58,20 @@ function solve(input) {
             continue;
           }
           for (let i = round; i < round + 4; i++) {
-            if (
-              i % 4 === 0 &&
-              !map[y - 1]?.[x] &&
-              !map[y - 1]?.[x - 1] &&
-              !map[y - 1]?.[x + 1]
-            ) {
-              proposals.push([[x, y].join(), [x, y - 1].join()]);
-              continue outer;
-            }
-            if (
-              i % 4 === 1 &&
-              !map[y + 1]?.[x] &&
-              !map[y + 1]?.[x - 1] &&
-              !map[y + 1]?.[x + 1]
-            ) {
-              proposals.push([[x, y].join(), [x, y + 1].join()]);
-              continue outer;
-            }
-            if (
-              i % 4 === 2 &&
-              !map[y - 1]?.[x - 1] &&
-              !map[y][x - 1] &&
-              !map[y + 1]?.[x - 1]
-            ) {
-              proposals.push([[x, y].join(), [x - 1, y].join()]);
-              continue outer;
-            }
-            if (
-              i % 4 === 3 &&
-              !map[y - 1]?.[x + 1] &&
-              !map[y][x + 1] &&
-              !map[y + 1]?.[x + 1]
-            ) {
-              proposals.push([[x, y].join(), [x + 1, y].join()]);
+            if (addProposal[(i - 1) % 4](map, x, y, proposals)) {
               continue outer;
             }
           }
         }
       }
     }
-    // console.log(proposals);
 
     const counts = proposals.reduce((acc, [from, to]) => {
       acc[to] = (acc[to] ?? 0) + 1;
       return acc;
     }, {});
-    // console.log(counts);
 
     proposals = proposals.filter(([from, to]) => counts[to] === 1);
-    // console.log(proposals);
 
     if (!proposals.length) {
       break;
@@ -125,30 +84,30 @@ function solve(input) {
       map[y2] = map[y2] ?? [];
       map[y2][x2] = 1;
     }
-    // console.log(map);
     round++;
   }
 
-  let yMin = 0,
-    yMax = 0,
-    xMin = 0,
-    xMax = 0;
-  for (let y in map) {
-    y = +y;
-    for (let x in map[y]) {
-      x = +x;
-      if (map[y][x]) {
-        yMin = Math.min(y, yMin);
-        yMax = Math.max(y, yMax);
-        xMin = Math.min(x, xMin);
-        xMax = Math.max(x, xMax);
+  if (part === 2) {
+    console.log(round);
+  } else {
+    let yMin = 0;
+    let yMax = 0;
+    let xMin = 0;
+    let xMax = 0;
+    for (let y in map) {
+      y = +y;
+      for (let x in map[y]) {
+        x = +x;
+        if (map[y][x]) {
+          yMin = Math.min(y, yMin);
+          yMax = Math.max(y, yMax);
+          xMin = Math.min(x, xMin);
+          xMax = Math.max(x, xMax);
+        }
       }
     }
+    console.log((yMax - yMin + 1) * (xMax - xMin + 1) - nElves);
   }
-  console.log(map)
-  console.log(yMin, yMax, xMin, xMax);
-  console.log(nElves);
-  console.log((yMax - yMin + 1) * (xMax - xMin + 1) - nElves);
-  console.log(round+1);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
