7d434050d08f813fd9e1ebbfcefedf069eea12a6
Leyan Lo <leyanlo@gmail.com>
2022-12-25 02:08:06 -0800

    chore(2022-24): simplify everything

--- 2022/day-25.js
+++ 2022/day-25.js
@@ -3,50 +3,20 @@ const fs = require('fs');
 const input = fs.readFileSync('./day-25-input.txt', 'utf8').trimEnd();
 
 function toDec(str) {
-  let carry = 0;
   let n = 0;
-  let pow = 0;
-  for (let i = str.length - 1; i >= 0; i--) {
-    const char = str[i];
-    switch (char) {
-      case '=':
-        carry -= 2 * 5 ** pow;
-        break;
-      case '-':
-        carry -= 5 ** pow;
-        break;
-      default:
-        n += +char * 5 ** pow + carry;
-        carry = 0;
-    }
-    pow++;
+  for (let i = 0; i < str.length; i++) {
+    n += ('=-012'.indexOf(str.at(-1 - i)) - 2) * 5 ** i;
   }
   return n;
 }
 
 function toSnafu(n) {
   let str = [];
-  let pow = 0;
   while (n > 0) {
-    str[pow] = (str[pow] ?? 0) + (n % 5);
-    if (str[pow] >= 5) {
-      str[pow + 1] = 1;
-      str[pow] -= 5;
-    }
-    switch (str[pow]) {
-      case 3:
-        str[pow] = '=';
-        str[pow + 1] = (str[pow + 1] ?? 0) + 1;
-        break;
-      case 4:
-        str[pow] = '-';
-        str[pow + 1] = (str[pow + 1] ?? 0) + 1;
-        break;
-    }
-    n = Math.floor(n / 5);
-    pow++;
+    str.unshift('012=-'[n % 5]);
+    n = Math.round(n / 5);
   }
-  return str.reverse().join('');
+  return str.join('');
 }
 
 function solve(input) {
