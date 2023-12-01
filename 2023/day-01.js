153b4d18906bbd065bb2215d727e4e6e770a9f56
Leyan Lo <leyanlo@gmail.com>
2023-11-30 22:36:20 -0800

    chore(2023-01): cleanup

--- 2023/day-01.js
+++ 2023/day-01.js
@@ -1,31 +1,7 @@
 const fs = require('fs');
 
-// var input = `1abc2
-// pqr3stu8vwx
-// a1b2c3d4e5f
-// treb7uchet`;
-var input = `two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen`;
-var input = fs.readFileSync('./day-01-input.txt', 'utf8').trimEnd();
-// 52844 wrong
-// 52840 right
+const input = fs.readFileSync('./day-01-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   console.log(input);
-//   let sum = 0;
-//   for (const line of input.split('\n')) {
-//     console.log([...line.match(/\d/g)].map(Number));
-//     const nums = [...line.match(/\d/g)];
-//     console.log()
-//     sum += +(nums[0] + nums[nums.length - 1])
-//   }
-//   console.log(sum)
-// }
 const wordToNum = {
   one: 1,
   two: 2,
@@ -39,22 +15,22 @@ const wordToNum = {
 };
 const words = Object.keys(wordToNum);
 
-function solve(input) {
+function solve(input, part) {
   let sum = 0;
   for (let line of input.split('\n')) {
-    console.log(line);
-    for (const word of words) {
-      line = line.replaceAll(
-        word,
-        `${word.at(0)}${wordToNum[word]}${word.at(-1)}`
-      );
+    if (part === 2) {
+      for (const word of words) {
+        line = line.replaceAll(
+          word,
+          `${word.at(0)}${wordToNum[word]}${word.at(-1)}`
+        );
+      }
     }
-    console.log(line);
     const nums = line.match(/\d/g);
-    const val = +(nums[0] + nums[nums.length - 1]);
-    console.log(val);
+    const val = +`${nums.at(0)}${nums.at(-1)}`;
     sum += val;
   }
   console.log(sum);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
