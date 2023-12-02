77fdd072c71e2df8e105efd91428a481f2008010
Leyan Lo <leyanlo@gmail.com>
2023-12-01 21:26:14 -0800

    chore(2023-02): cleanup

--- 2023/day-02.js
+++ 2023/day-02.js
@@ -1,62 +1,37 @@
 const fs = require('fs');
 
-var input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
-var input = fs.readFileSync('./day-02-input.txt', 'utf8').trimEnd();
-// 2206 wrong
-// 149 wrong
+const input = fs.readFileSync('./day-02-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   // console.log(input);
-//
-//   let sum = 0;
-//   outer: for (const line of input.split('\n')) {
-//     console.log(line);
-//     let [game, sets] = line.split(': ');
-//     game = +game.match(/\d+/);
-//     console.log(game);
-//
-//     sets = sets.split('; ');
-//     let nRed = 12;
-//     let nGreen = 13;
-//     let nBlue = 14;
-//     for (const set of sets) {
-//       console.log(set);
-//       const cubes = set.split(', ');
-//       for (const cube of cubes) {
-//         let [n, color] = cube.split(' ');
-//         n = +n;
-//         switch (color) {
-//           case 'red':
-//             if (n > nRed) continue outer;
-//             break;
-//           case 'green':
-//             if (n > nGreen) continue outer;
-//             break;
-//           case 'blue':
-//             if (n > nBlue) continue outer;
-//             break;
-//         }
-//       }
-//     }
-//     console.log(game, 'POSSIBLE!');
-//     sum += game;
-//   }
-//   console.log(sum);
-// }
-function solve(input) {
-  // console.log(input);
-
+function solve1(input) {
   let sum = 0;
   outer: for (const line of input.split('\n')) {
-    console.log(line);
     let [game, sets] = line.split(': ');
     game = +game.match(/\d+/);
-    console.log(game);
+    sets = sets.split('; ');
+    for (const set of sets) {
+      const cubes = set.split(', ');
+      const totals = {
+        red: 12,
+        green: 13,
+        blue: 14,
+      };
+      for (const cube of cubes) {
+        let [n, color] = cube.split(' ');
+        n = +n;
+        if (n > totals[color]) continue outer;
+      }
+    }
+    sum += game;
+  }
+  console.log(sum);
+}
+solve1(input);
 
+function solve2(input) {
+  let sum = 0;
+  for (const line of input.split('\n')) {
+    let [game, sets] = line.split(': ');
+    game = +game.match(/\d+/);
     sets = sets.split('; ');
     const mins = {
       red: 0,
@@ -64,7 +39,6 @@ function solve(input) {
       blue: 0,
     };
     for (const set of sets) {
-      console.log(set);
       const cubes = set.split(', ');
       for (const cube of cubes) {
         let [n, color] = cube.split(' ');
@@ -76,4 +50,4 @@ function solve(input) {
   }
   console.log(sum);
 }
-solve(input);
+solve2(input);
