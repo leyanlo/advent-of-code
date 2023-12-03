7cf23da04a17cd12e9012b0cd3c54eccae9d73d1
Leyan Lo <leyanlo@gmail.com>
2023-12-02 21:24:30 -0800

    chore(2023-03): cleanup

--- 2023/day-03.js
+++ 2023/day-03.js
@@ -1,16 +1,6 @@
 const fs = require('fs');
 
-var input = `467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..`;
-var input = fs.readFileSync('./day-03-input.txt', 'utf8').trimEnd();
+const input = fs.readFileSync('./day-03-input.txt', 'utf8').trimEnd();
 
 const dirs = [
   [-1, -1],
@@ -23,66 +13,63 @@ const dirs = [
   [1, 1],
 ];
 
-// function solve(input) {
-//   console.log(input);
-//   const map = input.split('\n').map((line) => line.split(''));
-//   let sum = 0;
-//   for (let i = 0; i < map.length; i++) {
-//     for (let j = 0; j < map[i].length; j++) {
-//       if (/[^\d^.]/.test(map[i][j])) {
-//         console.log(map[i][j]);
-//         for (let [di, dj] of dirs) {
-//           if (/\d/.test(map[i + di][j + dj])) {
-//             const digits = [map[i + di][j + dj]];
-//             for (let jj = j + dj - 1; jj >= 0; jj--) {
-//               if (/\d/.test(map[i + di][jj])) {
-//                 digits.unshift(map[i + di][jj]);
-//                 map[i + di][jj] = '.';
-//               } else {
-//                 break;
-//               }
-//             }
-//             for (let jj = j + dj + 1; jj < map[i + di].length; jj++) {
-//               if (/\d/.test(map[i + di][jj])) {
-//                 digits.push(map[i + di][jj]);
-//                 map[i + di][jj] = '.';
-//               } else {
-//                 break;
-//               }
-//             }
-//             sum += +digits.join('');
-//           }
-//         }
-//       }
-//     }
-//   }
-//   console.log(sum)
-// }
-// solve(input);
-function solve(input) {
-  console.log(input);
+function solve1(input) {
+  const map = input.split('\n').map((line) => line.split(''));
+  let sum = 0;
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      if (/[^\d^.]/.test(map[i][j])) {
+        for (let [di, dj] of dirs) {
+          if (/\d/.test(map[i + di][j + dj])) {
+            const digits = [map[i + di][j + dj]];
+            for (let j2 = j + dj - 1; j2 >= 0; j2--) {
+              if (/\d/.test(map[i + di][j2])) {
+                digits.unshift(map[i + di][j2]);
+                map[i + di][j2] = '.';
+              } else {
+                break;
+              }
+            }
+            for (let j2 = j + dj + 1; j2 < map[i + di].length; j2++) {
+              if (/\d/.test(map[i + di][j2])) {
+                digits.push(map[i + di][j2]);
+                map[i + di][j2] = '.';
+              } else {
+                break;
+              }
+            }
+            sum += +digits.join('');
+          }
+        }
+      }
+    }
+  }
+  console.log(sum);
+}
+solve1(input);
+
+function solve2(input) {
   const map = input.split('\n').map((line) => line.split(''));
   let sum = 0;
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
       if (/\*/.test(map[i][j])) {
-        console.log(map[i][j]);
-        let gears = [];
+        const gears = [];
         for (let [di, dj] of dirs) {
           if (/\d/.test(map[i + di][j + dj])) {
             const digits = [map[i + di][j + dj]];
-            for (let jj = j + dj - 1; jj >= 0; jj--) {
-              if (/\d/.test(map[i + di][jj])) {
-                digits.unshift(map[i + di][jj]);
-                map[i + di][jj] = '.';
+            for (let j2 = j + dj - 1; j2 >= 0; j2--) {
+              if (/\d/.test(map[i + di][j2])) {
+                digits.unshift(map[i + di][j2]);
+                map[i + di][j2] = '.';
               } else {
                 break;
               }
             }
-            for (let jj = j + dj + 1; jj < map[i + di].length; jj++) {
-              if (/\d/.test(map[i + di][jj])) {
-                digits.push(map[i + di][jj]);
-                map[i + di][jj] = '.';
+            for (let j2 = j + dj + 1; j2 < map[i + di].length; j2++) {
+              if (/\d/.test(map[i + di][j2])) {
+                digits.push(map[i + di][j2]);
+                map[i + di][j2] = '.';
               } else {
                 break;
               }
@@ -98,4 +85,4 @@ function solve(input) {
   }
   console.log(sum);
 }
-solve(input);
+solve2(input);
