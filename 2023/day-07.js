16273a38019d8ccc38b9d3ab7d8328132df8bc2e
Leyan Lo <leyanlo@gmail.com>
2023-12-07 00:19:46 -0800

    chore(2023-07): cleanup more. move ranking logic outside of compare function

--- 2023/day-07.js
+++ 2023/day-07.js
@@ -2,72 +2,26 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./day-07-input.txt', 'utf8').trimEnd();
 
-function compare1(lineA, lineB) {
+function solve1(input) {
   // prettier-ignore
   const cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 
-  const hexA = lineA[0].map((card) => cards.indexOf(card).toString(16));
-  const hexB = lineB[0].map((card) => cards.indexOf(card).toString(16));
-
-  const mapA = {};
-  for (const char of hexA) {
-    mapA[char] = (mapA[char] ?? 0) + 1;
-  }
-  const mapB = {};
-  for (const char of hexB) {
-    mapB[char] = (mapB[char] ?? 0) + 1;
-  }
-
-  const countsA = Object.values(mapA).sort().reverse();
-  const countsB = Object.values(mapB).sort().reverse();
-
-  return (
-    countsA.join().localeCompare(countsB.join()) ||
-    hexA.join().localeCompare(hexB.join())
-  );
-}
-
-function compare2(lineA, lineB) {
-  // prettier-ignore
-  const cards = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
-
-  const nJokersA = lineA[0].filter((card) => card === 'J').length;
-  const nJokersB = lineB[0].filter((card) => card === 'J').length;
-
-  const hexA = lineA[0].map((card) => cards.indexOf(card).toString(16));
-  const hexB = lineB[0].map((card) => cards.indexOf(card).toString(16));
-
-  const mapA = {};
-  for (const char of hexA) {
-    mapA[char] = (mapA[char] ?? 0) + 1;
-  }
-  mapA[0] = 0;
-  const mapB = {};
-  for (const char of hexB) {
-    mapB[char] = (mapB[char] ?? 0) + 1;
-  }
-  mapB[0] = 0;
-
-  let countsA = Object.values(mapA).sort().reverse();
-  countsA[0] += nJokersA;
-  let countsB = Object.values(mapB).sort().reverse();
-  countsB[0] += nJokersB;
-
-  return (
-    countsA.join().localeCompare(countsB.join()) ||
-    hexA.join().localeCompare(hexB.join())
-  );
-}
-
-function solve(input, part) {
   const lines = input.split('\n');
   for (let i = 0; i < lines.length; i++) {
     let [hand, bid] = lines[i].split(' ');
-    hand = hand.split('');
+    hand = hand.split('').map((card) => cards.indexOf(card).toString(16));
     bid = +bid;
-    lines[i] = [hand, bid];
+
+    const map = {};
+    for (const card of hand) {
+      map[card] = (map[card] ?? 0) + 1;
+    }
+    const counts = Object.values(map).sort((a, b) => b - a);
+
+    lines[i] = [[counts[0], counts[1] ?? 0, ...hand].join(), bid];
   }
-  lines.sort(part === 1 ? compare1 : compare2);
+
+  lines.sort(([a], [b]) => a.localeCompare(b));
 
   let sum = 0;
   for (let i = 0; i < lines.length; i++) {
@@ -75,5 +29,37 @@ function solve(input, part) {
   }
   console.log(sum);
 }
-solve(input, 1);
-solve(input, 2);
+solve1(input);
+
+function solve2(input) {
+  // prettier-ignore
+  const cards = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
+
+  const lines = input.split('\n');
+  for (let i = 0; i < lines.length; i++) {
+    let [hand, bid] = lines[i].split(' ');
+    hand = hand.split('').map((card) => cards.indexOf(card).toString(16));
+    bid = +bid;
+
+    const nJokers = hand.filter((card) => card === '0').length;
+
+    const map = {};
+    for (const card of hand) {
+      map[card] = (map[card] ?? 0) + 1;
+    }
+    map[0] = 0;
+    const counts = Object.values(map).sort((a, b) => b - a);
+    counts[0] += nJokers;
+
+    lines[i] = [[counts[0], counts[1] ?? 0, ...hand].join(), bid];
+  }
+
+  lines.sort(([a], [b]) => a.localeCompare(b));
+
+  let sum = 0;
+  for (let i = 0; i < lines.length; i++) {
+    sum += lines[i][1] * (i + 1);
+  }
+  console.log(sum);
+}
+solve2(input);
