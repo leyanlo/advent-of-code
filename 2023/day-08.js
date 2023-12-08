2e0e90018b953106e90b1235d94dc1e9b95d57e9
Leyan Lo <leyanlo@gmail.com>
2023-12-07 21:47:06 -0800

    chore(all): cleanup gcd()

--- 2023/day-08.js
+++ 2023/day-08.js
@@ -3,10 +3,7 @@ const fs = require('fs');
 const input = fs.readFileSync('./day-08-input.txt', 'utf8').trimEnd();
 
 function gcd(...nums) {
-  function _gcd(a, b) {
-    return !b ? a : gcd(b, a % b);
-  }
-  return nums.reduce((acc, n) => _gcd(acc, n));
+  return nums.reduce((acc, n) => (!n ? acc : gcd(n, acc % n)));
 }
 
 function lcm(...nums) {
