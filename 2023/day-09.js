21dbd1468bbd577effe59a6645992592158fbb85
Leyan Lo <leyanlo@gmail.com>
2023-12-08 21:44:43 -0800

    chore(2023-09): cleanup

--- 2023/day-09.js
+++ 2023/day-09.js
@@ -1,58 +1,49 @@
 const fs = require('fs');
 
-var input = `0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45`;
-var input = fs.readFileSync('./day-09-input.txt', 'utf8').trimEnd();
-// 1443108778 wrong
+const input = fs.readFileSync('./day-09-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   console.log(input);
-//   let sum = 0;
-//   for (const line of input.split('\n')) {
-//     let nums = line.match(/[-\d]+/g).map(Number);
-//     const nexts = [nums];
-//     while (nums.some(Boolean)) {
-//       const next = [];
-//       for (let i = 0; i < nums.length - 1; i++) {
-//         next.push(nums[i + 1] - nums[i]);
-//       }
-//       nums = next;
-//       nexts.push(next);
-//     }
-//     console.log(nexts);
-//     let carry = 0;
-//     for (let i = nexts.length - 2; i >= 0; i--) {
-//       carry += nexts[i].at(-1);
-//     }
-//     console.log(carry);
-//     sum += carry;
-//   }
-//   console.log(sum);
-// }
-// solve(input);
-function solve(input) {
-  console.log(input);
+function solve1(input) {
   let sum = 0;
   for (const line of input.split('\n')) {
-    let nums = line.match(/[-\d]+/g).map(Number);
-    const nexts = [nums];
-    while (nums.some(Boolean)) {
+    let curr = line.match(/[-\d]+/g).map(Number);
+    const rows = [curr];
+    while (curr.some(Boolean)) {
       const next = [];
-      for (let i = 0; i < nums.length - 1; i++) {
-        next.push(nums[i + 1] - nums[i]);
+      for (let i = 0; i < curr.length - 1; i++) {
+        next.push(curr[i + 1] - curr[i]);
       }
-      nums = next;
-      nexts.push(next);
+      curr = next;
+      rows.push(next);
     }
-    console.log(nexts);
     let carry = 0;
-    for (let i = nexts.length - 2; i >= 0; i--) {
-      carry = nexts[i][0] - carry;
+    for (let i = rows.length - 2; i >= 0; i--) {
+      carry += rows[i].at(-1);
     }
-    console.log(carry);
     sum += carry;
   }
   console.log(sum);
 }
-solve(input);
+solve1(input);
+
+function solve2(input) {
+  let sum = 0;
+  for (const line of input.split('\n')) {
+    let curr = line.match(/[-\d]+/g).map(Number);
+    const rows = [curr];
+    while (curr.some(Boolean)) {
+      const next = [];
+      for (let i = 0; i < curr.length - 1; i++) {
+        next.push(curr[i + 1] - curr[i]);
+      }
+      curr = next;
+      rows.push(next);
+    }
+    let carry = 0;
+    for (let i = rows.length - 2; i >= 0; i--) {
+      carry = rows[i][0] - carry;
+    }
+    sum += carry;
+  }
+  console.log(sum);
+}
+solve2(input);
