21dfeeb7fc1f7adb8d2026afd927aec36d3f7dcc
Leyan Lo <leyanlo@gmail.com>
2023-12-12 00:29:56 -0800

    chore(2023-12): cleanup and remove unnecessary memos

--- 2023/day-12.js
+++ 2023/day-12.js
@@ -1,62 +1,6 @@
 const fs = require('fs');
 
-var input = `???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1`;
-var input = fs.readFileSync('./day-12-input.txt', 'utf8').trimEnd();
-// 4964259839627 correct
-
-// function solve(input) {
-//   // console.log(input);
-//   let sum = 0;
-//   for (const line of input.split('\n')) {
-//     let [left, right] = line.split(' ');
-//     left = left.split('');
-//     right = right.split(',').map(Number);
-//     // console.log({ left, right });
-//
-//     const qs = left
-//       .map((char, i) => [char, i])
-//       .filter(([char]) => char === '?')
-//       .map(([, i]) => i);
-//     // console.log({ qs });
-//
-//     let nArrangements = 0;
-//     for (let i = 0; i < 2 ** qs.length; i++) {
-//       const perm = qs.filter((_, j) => (2 ** j) & i);
-//       // console.log('perm', perm);
-//       const str = left
-//         .map((char, j) =>
-//           char === '?' ? (perm.includes(j) ? '#' : '.') : char
-//         )
-//         .join('');
-//       // console.log('str', str);
-//       // console.log(
-//       //   'str',
-//       //   str
-//       //     .split(/\.+/g)
-//       //     .filter(Boolean)
-//       //     .map((s) => s.length)
-//       //     .join(),
-//       //   right.join()
-//       // );
-//       nArrangements += +(
-//         str
-//           .split(/\.+/g)
-//           .filter(Boolean)
-//           .map((s) => s.length)
-//           .join() === right.join()
-//       );
-//     }
-//     console.log('nArrangements', nArrangements);
-//     sum += nArrangements
-//   }
-//   console.log(sum)
-// }
-// solve(input);
+const input = fs.readFileSync('./day-12-input.txt', 'utf8').trimEnd();
 
 function countArrangements(
   springs,
@@ -76,10 +20,10 @@ function countArrangements(
   }
 
   if (springIdx >= springs.length) {
-    return memoize(+(groupIdx === groups.length));
+    return +(groupIdx === groups.length);
   }
   if (groupIdx >= groups.length) {
-    return memoize(+(springs.indexOf('#', springIdx) === -1));
+    return +(springs.indexOf('#', springIdx) === -1);
   }
 
   if (springs[springIdx] === '.') {
@@ -159,22 +103,17 @@ function countArrangements(
   }
 }
 
-function solve(input) {
-  // console.log(input);
+function solve(input, nCopies) {
   let sum = 0;
   for (const line of input.split('\n')) {
     let [springs, groups] = line.split(' ');
-    springs = (springs + '?').repeat(5).slice(0, -1);
-    groups = (groups + ',').repeat(5).slice(0, -1);
-    springs = springs.split('');
-    groups = groups.split(',').map(Number);
-    // console.log('springs', springs);
-    // console.log('groups', groups);
+    springs = Array(nCopies).fill(springs).join('?').split('');
+    groups = Array(nCopies).fill(groups).join(',').split(',').map(Number);
 
     const nArrangements = countArrangements(springs, groups);
-    console.log('nArrangements', nArrangements);
     sum += nArrangements;
   }
   console.log(sum);
 }
-solve(input);
+solve(input, 1);
+solve(input, 5);
