9b935bd1fedf85b246b3360ed9e19e04063be517
Leyan Lo <leyanlo@gmail.com>
2023-12-12 23:11:20 -0800

    feat(2023-13): add binary solution

---
+++ 2023/day-13-binary.js
@@ -0,0 +1,63 @@
+import { readFileSync } from 'node:fs';
+
+const input = readFileSync('./day-13-input.txt', 'utf8').trimEnd();
+
+function rotate(map) {
+  return map[0].map((_, j) => map.map((_, i) => map[i][j]));
+}
+
+function getErrors(a, b) {
+  [a, b] = [a, b].sort((a, b) => b - a);
+  let n = a ^ b;
+  let count = 0;
+  while (n) {
+    count += n & 1;
+    n >>= 1;
+  }
+  return count;
+}
+
+function getReflection(map, maxErrors) {
+  outer: for (let i = 0; i < map.length - 1; i++) {
+    let nErrors = getErrors(map[i], map[i + 1]);
+    if (nErrors <= maxErrors) {
+      for (let j = i - 1; j >= 0; j--) {
+        const delta = i - j;
+        if (!map[j] || !map[i + 1 + delta]) {
+          break;
+        }
+        nErrors += getErrors(map[j], map[i + 1 + delta]);
+        if (nErrors > maxErrors) {
+          continue outer;
+        }
+      }
+      if (nErrors === maxErrors) {
+        return i + 1;
+      }
+    }
+  }
+  return 0;
+}
+
+function solve(input, maxErrors) {
+  let sum = 0;
+  for (let map of input.split('\n\n')) {
+    map = map.split('\n').map((row) => row.split(''));
+    let rot = rotate(map);
+
+    map = map.map((row) =>
+      parseInt(row.map((char) => +(char === '#')).join(''), 2)
+    );
+    rot = rot.map((row) =>
+      parseInt(row.map((char) => +(char === '#')).join(''), 2)
+    );
+
+    const hReflect = getReflection(map, maxErrors);
+    const vReflect = getReflection(rot, maxErrors);
+    sum += hReflect * 100;
+    sum += vReflect;
+  }
+  console.log(sum);
+}
+solve(input, 0);
+solve(input, 1);
