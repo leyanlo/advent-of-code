881580715f5ece8c805d6c2be2cbca30f6a7220e
Leyan Lo <leyanlo@gmail.com>
2023-12-20 02:12:05 -0500

    chore(2023-20): cleanup

--- 2023/day-20.js
+++ 2023/day-20.js
@@ -1,102 +1,8 @@
 import { readFileSync } from 'node:fs';
 
-var input = `broadcaster -> a, b, c
-%a -> b
-%b -> c
-%c -> inv
-&inv -> a`;
-var input = `broadcaster -> a
-%a -> inv, con
-&inv -> b
-%b -> con
-&con -> output`;
-var input = readFileSync('./day-20-input.txt', 'utf8').trimEnd();
-// 239918617350720 too low
+const input = readFileSync('./day-20-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   // console.log(input);
-//   const map = {};
-//   const initialQueue = [];
-//   for (const line of input.split('\n')) {
-//     let [l, r] = line.split(' -> ');
-//     if (l === 'broadcaster') {
-//       initialQueue.push(
-//         ...r.split(', ').map((module) => ({ module, pulse: 0, from: 'button' }))
-//       );
-//     } else if (l[0] === '%') {
-//       map[l.substring(1)] = {
-//         kind: 'ff',
-//         outputs: r.split(', '),
-//         state: 0,
-//       };
-//     } else {
-//       map[l.substring(1)] = {
-//         kind: 'conj',
-//         outputs: r.split(', '),
-//         inputs: [],
-//       };
-//     }
-//   }
-//
-//   for (const module in map) {
-//     const outputs = map[module].outputs;
-//     for (const output of outputs) {
-//       if (map[output]?.kind === 'conj') {
-//         map[output].inputs.push({ module, state: 0 });
-//       }
-//     }
-//   }
-//   // console.log('map', map);
-//   // console.log('initialQueue', initialQueue);
-//
-//   const nPulses = [0, 0];
-//   for (let i = 0; i < 1000; i++) {
-//     const queue = [...initialQueue];
-//     nPulses[0]++;
-//     while (queue.length) {
-//       const { module, pulse, from } = queue.shift();
-//       nPulses[pulse]++;
-//       const input = map[module];
-//       if (!input) continue;
-//
-//       if (input.kind === 'ff') {
-//         if (!pulse) {
-//           input.state = +!input.state;
-//           queue.push(
-//             ...input.outputs.map((m) => ({
-//               module: m,
-//               pulse: input.state,
-//               from: module,
-//             }))
-//           );
-//         }
-//       } else {
-//         input.inputs = input.inputs.map((i) =>
-//           i.module === from
-//             ? {
-//                 module: from,
-//                 state: pulse,
-//               }
-//             : i
-//         );
-//         queue.push(
-//           ...input.outputs.map((m) => ({
-//             module: m,
-//             pulse: +!input.inputs.every((i) => i.state),
-//             from: module,
-//           }))
-//         );
-//       }
-//     }
-//     // console.log('map', map);
-//     // console.log('nPulses', nPulses);
-//   }
-//   console.log('nPulses', nPulses);
-//   console.log(nPulses[0] * nPulses[1]);
-// }
-// solve(input);
 function solve(input) {
-  // console.log(input);
   const map = {};
   const initialQueue = [];
   for (const line of input.split('\n')) {
@@ -128,10 +34,85 @@ function solve(input) {
       }
     }
   }
-  // console.log('map', map);
-  // console.log('initialQueue', initialQueue);
 
-  // these lead to gq
+  const nPulses = [0, 0];
+  for (let i = 0; i < 1000; i++) {
+    const queue = [...initialQueue];
+    nPulses[0]++;
+    while (queue.length) {
+      const { module, pulse, from } = queue.shift();
+      nPulses[pulse]++;
+      const input = map[module];
+      if (!input) continue;
+
+      if (input.kind === 'ff') {
+        if (!pulse) {
+          input.state = +!input.state;
+          queue.push(
+            ...input.outputs.map((m) => ({
+              module: m,
+              pulse: input.state,
+              from: module,
+            }))
+          );
+        }
+      } else {
+        input.inputs = input.inputs.map((i) =>
+          i.module === from
+            ? {
+                module: from,
+                state: pulse,
+              }
+            : i
+        );
+        queue.push(
+          ...input.outputs.map((m) => ({
+            module: m,
+            pulse: +!input.inputs.every((i) => i.state),
+            from: module,
+          }))
+        );
+      }
+    }
+  }
+  console.log(nPulses[0] * nPulses[1]);
+}
+solve(input);
+
+function solve2(input) {
+  const map = {};
+  const initialQueue = [];
+  for (const line of input.split('\n')) {
+    let [l, r] = line.split(' -> ');
+    if (l === 'broadcaster') {
+      initialQueue.push(
+        ...r.split(', ').map((module) => ({ module, pulse: 0, from: 'button' }))
+      );
+    } else if (l[0] === '%') {
+      map[l.substring(1)] = {
+        kind: 'ff',
+        outputs: r.split(', '),
+        state: 0,
+      };
+    } else {
+      map[l.substring(1)] = {
+        kind: 'conj',
+        outputs: r.split(', '),
+        inputs: [],
+      };
+    }
+  }
+
+  for (const module in map) {
+    const outputs = map[module].outputs;
+    for (const output of outputs) {
+      if (map[output]?.kind === 'conj') {
+        map[output].inputs.push({ module, state: 0 });
+      }
+    }
+  }
+
+  // these lead to gq, which lead to rx
   const buttonCounts = {
     xj: null,
     qs: null,
@@ -178,11 +159,8 @@ function solve(input) {
         );
       }
     }
-    // console.log('map', map);
-    // console.log('nPulses', nPulses);
     i++;
   }
-  console.log('buttonCounts', buttonCounts);
   console.log(Object.values(buttonCounts).reduce((acc, n) => acc * (n + 1), 1));
 }
-solve(input);
+solve2(input);
