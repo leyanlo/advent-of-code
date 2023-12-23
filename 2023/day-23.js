252cf503b879c97ebc3819d6ea1ca6b5c74231d6
Leyan Lo <leyanlo@gmail.com>
2023-12-23 02:26:42 -0500

    chore(2023-23): cleanup

--- 2023/day-23.js
+++ 2023/day-23.js
@@ -1,7 +1,5 @@
 import { readFileSync } from 'node:fs';
 
-import { Heap } from 'heap-js';
-
 const input = readFileSync('./day-23-input.txt', 'utf8').trimEnd();
 
 const dirs = {
@@ -11,66 +9,43 @@ const dirs = {
   '<': [0, -1],
 };
 
-// function solve(input) {
-//   // console.log(input);
-//   const map = [];
-//   for (const line of input.split('\n')) {
-//     map.push(line.split(''));
-//   }
-//   // console.log(map);
-//
-//   const heap = new Heap((a, b) => b.path.length - a.path.length);
-//   // const heap = [];
-//   heap.push({ path: [[0, 1].join()] });
-//   let maxLen = 0;
-//   while (heap.size()) {
-//     const { path } = heap.pop();
-//     const [r, c] = path.at(-1).split(',').map(Number);
-//     const char = map[r][c];
-//
-//     for (const dirKey in dirs) {
-//       if (char in dirs && char !== dirKey) continue;
-//
-//       const [dr, dc] = dirs[dirKey];
-//       const r2 = r + dr;
-//       const c2 = c + dc;
-//       const key = [r2, c2].join();
-//       if (path.includes(key)) continue;
-//
-//       switch (map[r2]?.[c2]) {
-//         case '.':
-//         case '^':
-//         case '>':
-//         case 'v':
-//         case '<':
-//           heap.add({ path: path.concat(key) });
-//           break;
-//         case '#':
-//       }
-//     }
-//     if (
-//       r === map.length - 1 &&
-//       c === map[r].length - 2 &&
-//       path.length - 1 > maxLen
-//     ) {
-//       // console.log(
-//       //   map
-//       //     .map((row, r) =>
-//       //       row
-//       //         .map((char, c) => (path.includes([r, c].join()) ? 'O' : char))
-//       //         .join('')
-//       //     )
-//       //     .join('\n'),
-//       //   '\n'
-//       // );
-//       maxLen = Math.max(maxLen, path.length - 1);
-//     }
-//   }
-//   console.log(maxLen);
-// }
-// solve(input);
+function solve1(input) {
+  const map = input.split('\n').map((line) => line.split(''));
 
-function solve(input) {
+  let maxLen = 0;
+  const queue = [[[0, 1].join()]];
+  while (queue.length) {
+    const path = queue.shift();
+    const [r, c] = path.at(-1).split(',').map(Number);
+    const char = map[r][c];
+
+    for (const dirChar in dirs) {
+      if (char in dirs && char !== dirChar) continue;
+
+      const [dr, dc] = dirs[dirChar];
+      const r2 = r + dr;
+      const c2 = c + dc;
+      const node = [r2, c2].join();
+      if (path.includes(node)) continue;
+
+      switch (map[r2]?.[c2]) {
+        case '.':
+        case '^':
+        case '>':
+        case 'v':
+        case '<':
+          queue.push(path.concat(node));
+      }
+    }
+    if (r === map.length - 1) {
+      maxLen = Math.max(maxLen, path.length - 1);
+    }
+  }
+  console.log(maxLen);
+}
+solve1(input);
+
+function solve2(input) {
   const map = input.split('\n').map((line) =>
     line.split('').map((char) => {
       switch (char) {
@@ -95,13 +70,10 @@ function solve(input) {
     function move(nSteps, prevDir, r, c) {
       if (map[r]?.[c] !== '.') return;
 
-      let edgeCount = 0;
-      for (const [dr, dc] of Object.values(dirs)) {
-        if (map[r + dr]?.[c + dc] === '.') {
-          edgeCount++;
-        }
-      }
-      if (nSteps > 0 && (edgeCount > 2 || r === 0 || r === map.length - 1)) {
+      const nEdges = Object.values(dirs).filter(
+        ([dr, dc]) => map[r + dr]?.[c + dc] === '.'
+      ).length;
+      if (nSteps && (nEdges > 2 || !r || r === map.length - 1)) {
         const nextNode = [r, c].join();
         if (!nodes.includes(nextNode)) {
           nodes.push(nextNode);
@@ -110,8 +82,8 @@ function solve(input) {
         return;
       }
 
-      if (prevDir !== 'v' && r > 0) move(nSteps + 1, '^', r - 1, c);
-      if (prevDir !== '^' && r < map.length - 1)
+      if (prevDir !== 'v' && r) move(nSteps + 1, '^', r - 1, c);
+      if (prevDir !== '^' && r !== map.length - 1)
         move(nSteps + 1, 'v', r + 1, c);
       if (prevDir !== '>') move(nSteps + 1, '<', r, c - 1);
       if (prevDir !== '<') move(nSteps + 1, '>', r, c + 1);
@@ -119,8 +91,6 @@ function solve(input) {
 
     move(0, null, r, c);
   }
-
-  console.log(nodes, distances);
 
   let maxSteps = 0;
   function move(steps, node, path) {
@@ -138,4 +108,4 @@ function solve(input) {
   move(0, start, []);
   console.log(maxSteps);
 }
-solve(input);
+solve2(input);
