eab548bad5dd09cc526056d2e877c656f5e1da0d
Leyan Lo <leyanlo@gmail.com>
2024-12-02 00:43:04 -0500

    chore(2024-02): cleanup

--- 2024/day-02.js
+++ 2024/day-02.js
@@ -2,51 +2,29 @@ import { readFileSync } from 'node:fs';
 
 const input = readFileSync('./day-02-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const lines = input.split('\n');
-//   let counts = 0;
-//   outer: for (let line of lines) {
-//     const nums = line.split(' ').map(Number);
-//     const posDir = nums[1] > nums[0] ? 1 : 0;
-//     for (let i = 1; i < nums.length; i++) {
-//       if (
-//         posDir
-//           ? nums[i] <= nums[i - 1] || nums[i] - 3 > nums[i - 1]
-//           : nums[i] >= nums[i - 1] || nums[i] + 3 < nums[i - 1]
-//       ) {
-//         continue outer;
-//       }
-//     }
-//     counts++;
-//   }
-//   console.log(counts);
-// }
-// solve(input);
-
-function isSafe(nums, nErrs = 0) {
-  const posDir = nums[1] > nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    const num = nums[i];
-    const prevNum = nums[i - 1];
-    if (
-      posDir
-        ? num <= prevNum || num - 3 > prevNum
-        : num >= prevNum || num + 3 < prevNum
-    ) {
-      nErrs++;
-      if (nErrs > 1) {
-        return false;
+function solve(input, part) {
+  function isSafe(nums, nErrs = 0) {
+    const posDir = nums[1] > nums[0];
+    for (let i = 1; i < nums.length; i++) {
+      const num = nums[i];
+      const prevNum = nums[i - 1];
+      if (
+        posDir
+          ? num <= prevNum || num - 3 > prevNum
+          : num >= prevNum || num + 3 < prevNum
+      ) {
+        if (part === 1 || nErrs === 1) {
+          return false;
+        }
+        const next1 = nums.toSpliced(i - 1, 1);
+        const next2 = nums.toSpliced(i, 1);
+        const next3 = nums.toSpliced(0, 1);
+        return isSafe(next1, 1) || isSafe(next2, 1) || isSafe(next3, 1);
       }
-      const next1 = nums.toSpliced(i - 1, 1);
-      const next2 = nums.toSpliced(i, 1);
-      const next3 = nums.toSpliced(0, 1);
-      return isSafe(next1, 1) || isSafe(next2, 1) || isSafe(next3, 1);
     }
+    return true;
   }
-  return true;
-}
 
-function solve(input) {
   const lines = input.split('\n');
   let counts = 0;
   for (let line of lines) {
@@ -55,4 +33,5 @@ function solve(input) {
   }
   console.log(counts);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
