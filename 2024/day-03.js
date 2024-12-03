99e8295b57c862ad3964eccbd25934ba89d2f2d8
Leyan Lo <leyanlo@gmail.com>
2024-12-03 00:29:46 -0500

    chore(2024-03): cleanup

--- 2024/day-03.js
+++ 2024/day-03.js
@@ -1,33 +1,18 @@
 import { readFileSync } from 'node:fs';
 
-var input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
-var input = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
 var input = readFileSync('./day-03-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   console.log(input);
-//   console.log(input.match(/mul\((\d{1,3}),(\d{1,3})\)/g));
-//   const matches = input.match(/mul\((\d{1,3}),(\d{1,3})\)/g);
-//   let sum = 0;
-//   for (const match of matches) {
-//     const [a, b] = match.match(/\d+/g);
-//     sum += +a * +b;
-//   }
-//   console.log(sum);
-// }
-// solve(input);
-
-function solve(input) {
-  console.log(input);
-  input = input.replace(/don't\(\)(.|\n)+?($|do\(\))/g, '');
-  console.log(input);
-  console.log(input.match(/mul\((\d{1,3}),(\d{1,3})\)/g));
-  const matches = input.match(/mul\((\d{1,3}),(\d{1,3})\)/g);
+function solve(input, part) {
+  if (part === 2) {
+    input = input.replace(/don't\(\)[^]+?($|do\(\))/g, '');
+  }
+  const matches = input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g);
+  const nums = Array.from(matches).map((match) => [+match[1], +match[2]]);
   let sum = 0;
-  for (const match of matches) {
-    const [a, b] = match.match(/\d+/g);
-    sum += +a * +b;
+  for (const [a, b] of nums) {
+    sum += a * b;
   }
   console.log(sum);
 }
-solve(input);
+solve(input, 1);
+solve(input, 2);
