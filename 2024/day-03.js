7255edf2f6172378163034fb45de0c860ae2fcd2
Leyan Lo <leyanlo@gmail.com>
2024-12-03 22:14:05 -0800

    chore(2024-03): use /s flag for regex for dot to match newlines

--- 2024/day-03.js
+++ 2024/day-03.js
@@ -4,7 +4,7 @@ var input = readFileSync('./day-03-input.txt', 'utf8').trimEnd();
 
 function solve(input, part) {
   if (part === 2) {
-    input = input.replace(/don't\(\)[^]+?($|do\(\))/g, '');
+    input = input.replace(/don't\(\).+?($|do\(\))/gs, '');
   }
   const matches = input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g);
   const nums = Array.from(matches).map((match) => [+match[1], +match[2]]);
