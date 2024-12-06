bc4a51e14b911cd6b73cc656f2f9c11c88389d93
Leyan Lo <leyanlo@gmail.com>
2024-12-05 22:29:26 -0800

    chore(2024-06): cleanup and speed up

--- 2024/day-06.js
+++ 2024/day-06.js
@@ -1,16 +1,6 @@
 import { readFileSync } from 'node:fs';
 
-var input = `....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#...`;
-var input = readFileSync('./day-06-input.txt', 'utf8').trimEnd();
+const input = readFileSync('./day-06-input.txt', 'utf8').trimEnd();
 
 const DIRS = [
   [-1, 0],
@@ -19,77 +9,60 @@ const DIRS = [
   [0, -1],
 ];
 
-// function solve(input) {
-//   let map = input.split('\n').map((row) => row.split(''));
-//   let dirIdx = 0;
-//   let [dy, dx] = DIRS[0];
-//   let y, x;
-//   outer: for (let i = 0; i < map.length; i++) {
-//     for (let j = 0; j < map[0].length; j++) {
-//       if (map[i][j] === '^') {
-//         [y, x] = [i, j];
-//         break outer;
-//       }
-//     }
-//   }
-//
-//   map = map.map((row) => row.map((cell) => (cell === '#' ? 1 : 0)));
-//
-//   let count = 0;
-//   const seen = map.map((row) => row.map(() => 0));
-//
-//   while (map[y]?.[x] !== undefined) {
-//     if (!seen[y][x]) {
-//       seen[y][x] = 1;
-//       count++;
-//     }
-//
-//     let y2 = y + dy;
-//     let x2 = x + dx;
-//     while (map[y2]?.[x2] === 1) {
-//       dirIdx = (dirIdx + 1) % 4;
-//       [dy, dx] = DIRS[dirIdx];
-//       y2 = y + dy;
-//       x2 = x + dx;
-//     }
-//     [y, x] = [y2, x2];
-//   }
-//   console.log(count);
-// }
-// solve(input);
-
 function solve(input) {
-  let map = input.split('\n').map((row) => row.split(''));
   let start;
-  outer: for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[0].length; j++) {
-      if (map[i][j] === '^') {
+  const map = input.split('\n').map((row, i) =>
+    row.split('').map((char, j) => {
+      if (char === '^') {
         start = [i, j];
-        break outer;
       }
-    }
-  }
+      return char === '#' ? 1 : 0;
+    })
+  );
 
-  map = map.map((row) => row.map((cell) => (cell === '#' ? 1 : 0)));
-
+  let dirIdx = 0;
+  let [dy, dx] = DIRS[0];
+  let [y, x] = start;
   let count = 0;
+  const seen = map.map((row) => row.map(() => 0));
+
+  while (map[y]?.[x] !== undefined) {
+    if (!seen[y][x]) {
+      seen[y][x] = 1;
+      count++;
+    }
+
+    let y2 = y + dy;
+    let x2 = x + dx;
+    while (map[y2]?.[x2] === 1) {
+      dirIdx = (dirIdx + 1) % 4;
+      [dy, dx] = DIRS[dirIdx];
+      y2 = y + dy;
+      x2 = x + dx;
+    }
+    [y, x] = [y2, x2];
+  }
+  console.log(count);
+
+  count = 0;
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[0].length; j++) {
-      if (i === 6 && j === 3) {
-        debugger;
+      if (!seen[i][j]) {
+        continue;
       }
+
       let dirIdx = 0;
       let [dy, dx] = DIRS[0];
       let [y, x] = start;
-      const seen = map.map((row) => row.map(() => []));
+      const seenDir = map.map((row) => row.map(() => []));
 
       while (map[y]?.[x] !== undefined) {
-        if (seen[y][x].includes(dirIdx)) {
+        if (seenDir[y][x].includes(dirIdx)) {
           count++;
           break;
         }
 
-        seen[y][x].push(dirIdx);
+        seenDir[y][x].push(dirIdx);
 
         let y2 = y + dy;
         let x2 = x + dx;
