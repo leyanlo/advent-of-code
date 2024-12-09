39caf829f1a54b1977fc2a29ed4c566504f2c014
Leyan Lo <leyanlo@gmail.com>
2024-12-08 22:32:10 -0800

    chore(2024-09): clean up

--- 2024/day-09.js
+++ 2024/day-09.js
@@ -1,54 +1,45 @@
 import { readFileSync } from 'node:fs';
 
-var input = `2333133121414131402`;
-var input = readFileSync('./day-09-input.txt', 'utf8').trimEnd();
-// 15868383645408 wrong
+const input = readFileSync('./day-09-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   const arr = [];
-//   for (let i = 0; i < input.length; i++) {
-//     const n = +input[i];
-//     for (let j = 0; j < n; j++) {
-//       arr.push(i % 2 === 0 ? i / 2 : -1);
-//     }
-//   }
-//   console.log(arr.join(''));
-//
-//   let left = arr.findIndex((n) => n === -1);
-//   let right = arr.findLastIndex((n) => n !== -1);
-//   while (left !== -1 && right !== -1 && left < right) {
-//     [arr[left], arr[right]] = [arr[right], arr[left]];
-//     left = arr.findIndex((n, j) => j > left && n === -1);
-//     right = arr.findLastIndex((n, j) => j < right && n !== -1);
-//   }
-//   console.log(arr.slice(-10));
-//
-//   console.log(arr.reduce((acc, n, i) => acc + i * Math.max(0, n)));
-// }
-// solve(input);
+function solve1(input) {
+  const blocks = [];
+  for (let i = 0; i < input.length; i++) {
+    const n = +input[i];
+    for (let j = 0; j < n; j++) {
+      blocks.push(i % 2 === 0 ? i / 2 : -1);
+    }
+  }
 
-function solve(input) {
-  const arr = [];
+  let left = blocks.findIndex((n) => n === -1);
+  let right = blocks.findLastIndex((n) => n !== -1);
+  while (left !== -1 && right !== -1 && left < right) {
+    [blocks[left], blocks[right]] = [blocks[right], blocks[left]];
+    left = blocks.findIndex((n, j) => n === -1 && j > left);
+    right = blocks.findLastIndex((n, j) => n !== -1 && j < right);
+  }
+  console.log(blocks.reduce((acc, n, i) => acc + i * Math.max(0, n)));
+}
+solve1(input);
+
+function solve2(input) {
+  const blocks = [];
   const sizes = [];
   for (let i = 0; i < input.length; i++) {
     const n = +input[i];
     for (let j = 0; j < n; j++) {
-      arr.push(i % 2 === 0 ? i / 2 : -1);
+      blocks.push(i % 2 === 0 ? i / 2 : -1);
       sizes.push(n);
     }
   }
-  console.log(sizes.join(''));
-  console.log(arr.join('').replaceAll('-1', '.'));
 
-  let right = arr.findLastIndex((n) => n !== -1);
-  let left = arr.findIndex((n, i) => n === -1 && sizes[i] >= sizes[right]);
-  outer: while (right !== -1 && left !== -1) {
-    // console.log({ right });
-    if (right === 20) debugger;
+  let right = blocks.findLastIndex((n) => n !== -1);
+  let left = blocks.findIndex((n, i) => n === -1 && sizes[i] >= sizes[right]);
+  outer: while (true) {
     const rSize = sizes[right];
     const lSize = sizes[left];
     for (let i = 0; i < rSize; i++) {
-      [arr[left], arr[right]] = [arr[right], arr[left]];
+      [blocks[left], blocks[right]] = [blocks[right], blocks[left]];
       [sizes[left], sizes[right]] = [sizes[right], sizes[left]];
       left++;
       right--;
@@ -58,24 +49,23 @@ function solve(input) {
       left++;
     }
 
-    right = arr.findLastIndex((n, i) => i <= right && n !== -1);
-    left = arr.findIndex(
-      (n, i) => n === -1 && sizes[i] >= sizes[right] && i < right
+    right = blocks.findLastIndex((n, i) => n !== -1 && i <= right);
+    left = blocks.findIndex(
+      (n, i) => n === -1 && i < right && sizes[i] >= sizes[right]
     );
     while (left === -1) {
-      right = arr.findLastIndex(
-        (n, i) => i <= right - sizes[right] && n !== -1
+      right = blocks.findLastIndex(
+        (n, i) => n !== -1 && i <= right - sizes[right]
       );
-      left = arr.findIndex(
-        (n, i) => n === -1 && sizes[i] >= sizes[right] && i < right
+      left = blocks.findIndex(
+        (n, i) => n === -1 && i < right && sizes[i] >= sizes[right]
       );
 
       if (right === -1) {
         break outer;
       }
     }
-    // console.log(arr.join('').replaceAll('-1', '.'));
   }
-  console.log(arr.reduce((acc, n, i) => acc + i * Math.max(0, n)));
+  console.log(blocks.reduce((acc, n, i) => acc + i * Math.max(0, n)));
 }
-solve(input);
+solve2(input);
