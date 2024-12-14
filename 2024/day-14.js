8a40a67985e145b492e1ec94c51775cbb7098a37
Leyan Lo <leyanlo@gmail.com>
2024-12-13 22:07:40 -0800

    chore(2024-14): cleanup

--- 2024/day-14.js
+++ 2024/day-14.js
@@ -1,22 +1,8 @@
 import { readFileSync } from 'node:fs';
 
-var input = `p=0,4 v=3,-3
-p=6,3 v=-1,-3
-p=10,3 v=-1,2
-p=2,0 v=2,-1
-p=0,0 v=1,3
-p=3,0 v=-2,-2
-p=7,6 v=-1,-3
-p=3,0 v=-1,-2
-p=9,3 v=2,3
-p=7,3 v=-1,2
-p=2,4 v=2,-3
-p=9,5 v=-3,-3`,
-  w = 11,
-  h = 7;
-var input = readFileSync('./day-14-input.txt', 'utf8').trimEnd(),
-  w = 101,
-  h = 103;
+const input = readFileSync('./day-14-input.txt', 'utf8').trimEnd();
+const w = 101;
+const h = 103;
 
 function mod(a, b) {
   return a < 0 ? b - (-a % b) : a % b;
@@ -63,53 +49,58 @@ function mod(a, b) {
 // }
 // solve(input, w, h);
 
-function solve(input, w, h) {
-  console.log(input);
+function solve(input, w, h, part) {
   const robots = input
     .split('\n')
     .map((line) => line.match(/-?\d+/g).map(Number));
-  console.log(robots);
 
   const map = Array.from({ length: h }).map(() =>
     Array.from({ length: w }).map(() => 0)
   );
 
   let t = 0;
-  for (const [px, py] of robots) {
-    map[py][px] = 1;
-  }
-  console.log(
-    t,
-    map
-      .map((row) => row.map((cell) => (cell ? '◻️' : '◼️')).join(''))
-      .join('\n')
-  );
-  for (const [px, py] of robots) {
-    map[py][px] = 0;
-  }
-
   while (true) {
-    if (t > 10000) break;
     t++;
-
     for (const robot of robots) {
       const [px, py, vx, vy] = robot;
       robot[0] = mod(px + vx, w);
       robot[1] = mod(py + vy, h);
     }
 
-    for (const [px, py] of robots) {
-      map[py][px] = 1;
-    }
-    console.log(
-      t,
-      map
-        .map((row) => row.map((cell) => (cell ? '◻️' : '◼️')).join(''))
-        .join('\n')
-    );
-    for (const [px, py] of robots) {
-      map[py][px] = 0;
+    if (part === 1 && t === 100) {
+      const counts = [
+        [0, 0],
+        [0, 0],
+      ];
+      for (const [px, py] of robots) {
+        const x = px < w / 2 - 1 ? 0 : px > w / 2 ? 1 : -1;
+        const y = py < h / 2 - 1 ? 0 : py > h / 2 ? 1 : -1;
+        if (x !== -1 && y !== -1) {
+          counts[x][y]++;
+        }
+      }
+      console.log(counts.flat().reduce((acc, n) => acc * n));
+
+      break;
+    } else {
+      for (const [px, py] of robots) {
+        map[py][px] = 1;
+      }
+      if (map.flat().join('').includes('11111111')) {
+        console.log(
+          map
+            .map((row) => row.map((cell) => (cell ? '⬜️' : '⬛️')).join(''))
+            .join('\n')
+        );
+        console.log(t);
+
+        break;
+      }
+      for (const [px, py] of robots) {
+        map[py][px] = 0;
+      }
     }
   }
 }
-solve(input, w, h);
+solve(input, w, h, 1);
+solve(input, w, h, 2);
