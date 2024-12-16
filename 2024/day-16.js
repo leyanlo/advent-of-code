52a91d636652c91b3f69cffba19bfe8c4dc1ea96
Leyan Lo <leyanlo@gmail.com>
2024-12-15 22:24:57 -0800

    chore(2024-16): clean up

--- 2024/day-16.js
+++ 2024/day-16.js
@@ -1,38 +1,6 @@
 import { readFileSync } from 'node:fs';
 
-var input = `###############
-#.......#....E#
-#.#.###.#.###.#
-#.....#.#...#.#
-#.###.#####.#.#
-#.#.#.......#.#
-#.#.#####.###.#
-#...........#.#
-###.#.#####.#.#
-#...#.....#.#.#
-#.#.#.###.#.#.#
-#.....#...#.#.#
-#.###.#.#.#.#.#
-#S..#.....#...#
-###############`;
-var input=`#################
-#...#...#...#..E#
-#.#.#.#.#.#.#.#.#
-#.#.#.#...#...#.#
-#.#.#.#.###.#.#.#
-#...#.#.#.....#.#
-#.#.#.#.#.#####.#
-#.#...#.#.#.....#
-#.#.#####.#.###.#
-#.#.#.......#...#
-#.#.###.#####.###
-#.#.#...#.....#.#
-#.#.#.#####.###.#
-#.#.#.........#.#
-#.#.#.#########.#
-#S#.............#
-#################`
-var input = readFileSync('./day-16-input.txt', 'utf8').trimEnd();
+const input = readFileSync('./day-16-input.txt', 'utf8').trimEnd();
 
 const DIRS = [
   [0, 1],
@@ -44,61 +12,6 @@ const DIRS = [
 function mod(a, b) {
   return a < 0 ? b - (-a % b) : a % b;
 }
-
-// function solve(input) {
-//   let start;
-//   const map = input.split('\n').map((line, i) =>
-//     line.split('').map((char, j) => {
-//       if (char === 'S') {
-//         start = [i, j];
-//       }
-//       return char;
-//     })
-//   );
-//
-//   let queue = [[...start, 0, 0]];
-//   const seen = map.map((row) =>
-//     row.map(() => DIRS.map(() => Number.POSITIVE_INFINITY))
-//   );
-//   let minScore = Number.POSITIVE_INFINITY;
-//   while (queue.length !== 0) {
-//     const nextQueue = [];
-//     for (const [i, j, dirIdx, score] of queue) {
-//       if (score >= minScore) {
-//         continue;
-//       }
-//
-//       if (map[i][j] === 'E') {
-//         minScore = Math.min(minScore, score);
-//       }
-//
-//       if (seen[i][j][dirIdx] < score) {
-//         continue;
-//       }
-//       seen[i][j][dirIdx] = score;
-//
-//       for (let dirIdx2 = 0; dirIdx2 < 4; dirIdx2++) {
-//         if (dirIdx2 === mod(dirIdx + 2, 4)) {
-//           continue;
-//         }
-//
-//         const [di, dj] = DIRS[dirIdx2];
-//         const [i2, j2] = [i + di, j + dj];
-//         if ('.E'.includes(map[i2][j2])) {
-//           nextQueue.push([
-//             i2,
-//             j2,
-//             dirIdx2,
-//             score + 1 + (dirIdx2 !== dirIdx) * 1000,
-//           ]);
-//         }
-//       }
-//     }
-//     queue = nextQueue.sort((a, b) => a[3] - b[3]);
-//   }
-//   console.log(minScore)
-// }
-// solve(input);
 
 function solve(input) {
   let start;
@@ -129,6 +42,7 @@ function solve(input) {
         if (score === minScore) {
           (paths[minScore] ??= []).push(path);
         }
+        continue;
       }
 
       if (seen[i][j][dirIdx] < score) {
@@ -154,10 +68,10 @@ function solve(input) {
         }
       }
     }
-    queue = nextQueue.sort((a, b) => a[3] - b[3]);
+    queue = nextQueue;
   }
+  console.log(minScore);
   console.log(
-    minScore,
     new Set(paths[minScore].flat().map((coords) => coords.join())).size
   );
 }
