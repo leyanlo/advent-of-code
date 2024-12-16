56d280f2e6fcca2a919a89a4b3c51cead82f68f9
Leyan Lo <leyanlo@gmail.com>
2024-12-15 22:29:19 -0800

    chore(2024-15): clean up part 2

--- 2024/day-15.js
+++ 2024/day-15.js
@@ -59,9 +59,7 @@ function solve1(input) {
         }
         if (map[i3][j3] === '.') {
           curr = [i2, j2];
-          queue.sort(([ai, aj], [bi, bj]) =>
-            di ? di * (bi - ai) : dj * (bj - aj)
-          );
+          queue.reverse();
           for ([i3, j3] of queue) {
             map[i3 + di][j3 + dj] = 'O';
             map[i3][j3] = '.';
@@ -82,7 +80,7 @@ function solve1(input) {
   }
   console.log(sum);
 }
-solve1(input, 0);
+solve1(input);
 
 function solve2(input) {
   let [map, moves] = input.split('\n\n');
@@ -104,7 +102,8 @@ function solve2(input) {
   let curr = start;
   DEBUG && logMap('Initial state:', map, curr);
   moves = moves.replaceAll('\n', '').split('');
-  for (const move of moves) {
+  for (let moveIdx = 0; moveIdx < moves.length; moveIdx++) {
+    const move = moves[moveIdx];
     const [di, dj] = DIRS[move];
     const [i, j] = curr;
     let [i2, j2] = [i + di, j + dj];
@@ -117,80 +116,67 @@ function solve2(input) {
       case '[':
       case ']': {
         if (di === 0) {
+          const queue = [];
           let [i3, j3] = [i2, j2];
           while ('[]'.includes(map[i3][j3])) {
+            queue.push([i3, j3]);
             j3 += dj;
           }
           if (map[i3][j3] === '.') {
             curr = [i2, j2];
-            let prev = map[i2][j2];
-            map[i2][j2] = '.';
-            while (i2 !== i3 || j2 !== j3) {
-              i2 += di;
-              j2 += dj;
-              [prev, map[i2][j2]] = [map[i2][j2], prev];
+            queue.reverse();
+            for ([i3, j3] of queue) {
+              map[i3 + di][j3 + dj] = map[i3][j3];
+              map[i3][j3] = '.';
             }
           }
         } else {
-          const start = [
-            [i2, j2],
-            map[i2][j2] === '[' ? [i2, j + 1] : [i2, j - 1],
-          ];
-          let frontier = [...start];
-          while (frontier.some(([i3, j3]) => '[]'.includes(map[i3][j3]))) {
-            const nextFrontier = [];
+          const queue =
+            map[i2][j2] === '['
+              ? [
+                  [i2, j2],
+                  [i2, j + 1],
+                ]
+              : [
+                  [i2, j - 1],
+                  [i2, j2],
+                ];
+          let frontier = queue;
+          while (frontier.length !== 0) {
+            const nextFrontierSet = new Set();
             for (let [i3, j3] of frontier) {
-              if (map[i3][j3] === '.') continue;
-
               i3 += di;
               switch (map[i3][j3]) {
                 case '#':
                   break outer;
-                case '.':
-                  nextFrontier.push([i3, j3]);
-                  break;
                 case '[':
-                  nextFrontier.push([i3, j3], [i3, j3 + 1]);
+                  nextFrontierSet.add([i3, j3].join());
+                  nextFrontierSet.add([i3, j3 + 1].join());
                   break;
                 case ']':
-                  nextFrontier.push([i3, j3], [i3, j3 - 1]);
+                  nextFrontierSet.add([i3, j3 - 1].join());
+                  nextFrontierSet.add([i3, j3].join());
                   break;
               }
             }
-            frontier = nextFrontier;
+            frontier = Array.from(nextFrontierSet).map((key) =>
+              key.split(',').map(Number)
+            );
+            queue.push(...frontier);
           }
 
           curr = [i2, j2];
-          let frontier2 = start.map(([i3, j3]) => [i3, j3, map[i3][j3]]);
-          for (const [i3, j3] of frontier2) {
+          queue.reverse();
+          for (const [i3, j3] of queue) {
+            map[i3 + di][j3 + dj] = map[i3][j3];
             map[i3][j3] = '.';
-          }
-          while (frontier2.length !== 0) {
-            const nextFrontier2 = [];
-            for (let [i3, j3, char] of frontier2) {
-              i3 += di;
-              switch (map[i3][j3]) {
-                case '.':
-                  map[i3][j3] = char;
-                  break;
-                case '[':
-                  nextFrontier2.push([i3, j3, '['], [i3, j3 + 1, ']']);
-                  map[i3][j3] = char;
-                  map[i3][j3 + 1] = '.';
-                  break;
-                case ']':
-                  nextFrontier2.push([i3, j3, ']'], [i3, j3 - 1, '[']);
-                  map[i3][j3] = char;
-                  map[i3][j3 - 1] = '.';
-                  break;
-              }
-            }
-            frontier2 = nextFrontier2;
           }
         }
       }
     }
-    DEBUG && logMap(`Move ${move}:`, map, curr);
+
+    const count = map.flat().filter((char) => char === '[').length;
+    DEBUG && logMap(`Move ${move} (${moveIdx}) (${count}):`, map, curr);
   }
 
   let sum = 0;
@@ -203,4 +189,4 @@ function solve2(input) {
   }
   console.log(sum);
 }
-solve2(input, 0);
+solve2(input);
