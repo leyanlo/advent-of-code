319c4ba62f5528865668cb1910868a01a467dd40
Leyan Lo <leyanlo@gmail.com>
2024-12-23 01:12:36 -0800

    chore(2024-23): clean up more

--- 2024/day-23.js
+++ 2024/day-23.js
@@ -14,9 +14,9 @@ function solve1(input) {
   for (const node in map) {
     for (const [a, b] of connections) {
       if (map[a].includes(node) && map[b].includes(node)) {
-        const arr = [a, b, node];
-        if (arr.some((n) => n.startsWith('t'))) {
-          sets.add(arr.sort().join());
+        const set = [a, b, node];
+        if (set.some((n) => n.startsWith('t'))) {
+          sets.add(set.sort().join());
         }
       }
     }
@@ -36,10 +36,7 @@ function solve2(input) {
   const sets = connections;
   for (const node in map) {
     for (const set of sets) {
-      if (
-        !set.includes(node) &&
-        set.every((node2) => map[node2].includes(node))
-      ) {
+      if (set.every((node2) => map[node2].includes(node))) {
         set.push(node);
       }
     }
