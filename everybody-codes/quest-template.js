4aea95486d1d8f78cde345106de8cd73e6dae239
Leyan Lo <leyanlo@gmail.com>
2024-12-27 01:41:00 -0800

    chore(ec): add quest template

---
+++ everybody-codes/quest-template.js
@@ -0,0 +1,13 @@
+var input = ``;
+var part = 1;
+
+// var input = ``;
+// var part = 2;
+
+// var input = ``;
+// var part = 3;
+
+function solve(input, part) {
+  console.log(input);
+}
+solve(input, part);
