74609834e7ecc99750ac5a7dbd614bd824404c36
Leyan Lo <leyanlo@gmail.com>
2025-12-02 16:44:28 -0800

    chore(2025-02): clean up

--- 2025/day-02.js
+++ 2025/day-02.js
@@ -3,23 +3,23 @@ import { readFileSync } from 'node:fs';
 const input = readFileSync('./day-02-input.txt', 'utf8').trimEnd();
 
 function solve1(input) {
-  const invalids = [];
+  let sum = 0;
   const ranges = input.split(',').map((range) => range.split('-').map(Number));
   for (const [start, end] of ranges) {
     for (let id = start; id <= end; id++) {
       const str = id.toString();
       const len = str.length;
       if (str.substring(0, len / 2) === str.substring(len / 2)) {
-        invalids.push(id);
+        sum += id;
       }
     }
   }
-  console.log(invalids.reduce((acc, n) => acc + n, 0));
+  console.log(sum);
 }
 solve1(input);
 
 function solve2(input) {
-  const invalids = [];
+  let sum = 0;
   const ranges = input.split(',').map((range) => range.split('-').map(Number));
   for (const [start, end] of ranges) {
     for (let id = start; id <= end; id++) {
@@ -27,12 +27,12 @@ function solve2(input) {
       const len = str.length;
       for (let l = 1; l <= len / 2; l++) {
         if (len % l === 0 && str.substring(0, l).repeat(len / l) === str) {
-          invalids.push(id);
+          sum += id;
           break;
         }
       }
     }
   }
-  console.log(invalids.reduce((acc, n) => acc + n, 0));
+  console.log(sum);
 }
 solve2(input);
