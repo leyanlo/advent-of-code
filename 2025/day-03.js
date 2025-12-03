e7ebce910bd43d54ab3194ba0f0af103a5979528
Leyan Lo <leyanlo@gmail.com>
2025-12-02 23:26:24 -0800

    chore(2025-03): clean up with slice

--- 2025/day-03.js
+++ 2025/day-03.js
@@ -6,18 +6,13 @@ function solve(input, nDigits) {
   const lines = input.split('\n').map((line) => line.split('').map(Number));
   let sum = 0;
   for (const line of lines) {
-    const maxDigits = line.slice(-nDigits);
-    const maxDigitsIdx = Array.from(
-      { length: nDigits },
-      (_, i) => line.length - nDigits + i
-    );
+    const maxDigits = [];
+    let left = 0;
     for (let i = 0; i < nDigits; i++) {
-      for (let j = maxDigitsIdx[i]; j > (maxDigitsIdx[i - 1] ?? -1); j--) {
-        if (line[j] >= maxDigits[i]) {
-          maxDigits[i] = line[j];
-          maxDigitsIdx[i] = j;
-        }
-      }
+      const right = line.length - nDigits + i;
+      const maxDigit = Math.max(...line.slice(left, right + 1));
+      maxDigits.push(maxDigit);
+      left = line.indexOf(maxDigit, left) + 1;
     }
     sum += +maxDigits.join('');
   }
