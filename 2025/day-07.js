a0cb377833b977daa76f0d49346ba68301127ec9
Leyan Lo <leyanlo@gmail.com>
2025-12-06 21:23:49 -0800

    chore(2025-07): clean up

--- 2025/day-07.js
+++ 2025/day-07.js
@@ -1,62 +1,46 @@
 import { readFileSync } from 'node:fs';
 
-var input = `.......S.......
-...............
-.......^.......
-...............
-......^.^......
-...............
-.....^.^.^.....
-...............
-....^.^...^....
-...............
-...^.^...^.^...
-...............
-..^...^.....^..
-...............
-.^.^.^.^.^...^.
-...............`;
-var input = readFileSync('./day-07-input.txt', 'utf8').trimEnd();
+const input = readFileSync('./day-07-input.txt', 'utf8').trimEnd();
 
-// function solve(input) {
-//   let nSplits = 0;
-//   const lines = input.split('\n').map(line => line.split(''));
-//   let beams = [lines[0].indexOf('S')];
-//   for (let i = 1;  i< lines.length; i++) {
-//     const nextBeams = new Set()
-//     for (const beam of beams) {
-//       if (lines[i][beam] === '^') {
-//         nextBeams.add(beam - 1);
-//         nextBeams.add(beam + 1);
-//         nSplits++;
-//       } else {
-//         nextBeams.add(beam);
-//       }
-//     }
-//     beams = Array.from(nextBeams);
-//   }
-//   console.log(nSplits);
-// }
-// solve(input);
-
-function solve(input) {
+function solve1(input) {
   const lines = input.split('\n').map((line) => line.split(''));
-  let beams = { [lines[0].indexOf('S')]: 1 };
+  let nSplits = 0;
+  let beams = [lines[0].indexOf('S')];
   for (let i = 1; i < lines.length; i++) {
-    const nextBeams = {};
-    for (const [beam, t] of Object.entries(beams)) {
+    const nextBeams = new Set();
+    for (const beam of beams) {
       if (lines[i][beam] === '^') {
-        nextBeams[+beam - 1] ??= 0;
-        nextBeams[+beam - 1] += t;
-        nextBeams[+beam + 1] ??= 0;
-        nextBeams[+beam + 1] += t;
+        nextBeams.add(beam - 1);
+        nextBeams.add(beam + 1);
+        nSplits++;
       } else {
-        nextBeams[beam] ??= 0;
-        nextBeams[beam] += t;
+        nextBeams.add(beam);
+      }
+    }
+    beams = Array.from(nextBeams);
+  }
+  console.log(nSplits);
+}
+solve1(input);
+
+function solve2(input) {
+  const lines = input.split('\n').map((line) => line.split(''));
+  const zeroes = Array.from({ length: lines[0].length }, () => 0);
+  let beams = [...zeroes];
+  beams[lines[0].indexOf('S')] = 1;
+  for (let i = 1; i < lines.length; i++) {
+    const nextBeams = [...zeroes];
+    for (let j = 0; j < beams.length; j++) {
+      const beam = beams[j];
+      if (lines[i][j] === '^') {
+        nextBeams[j - 1] += beam;
+        nextBeams[j + 1] += beam;
+      } else {
+        nextBeams[j] += beam;
       }
     }
     beams = nextBeams;
   }
   console.log(Object.values(beams).reduce((a, b) => a + b, 0));
 }
-solve(input);
+solve2(input);
