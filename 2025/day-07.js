a328663c5a3b627879986e07cda66e9862c730f8
Leyan Lo <leyanlo@gmail.com>
2025-12-06 22:08:44 -0800

    chore(2025-07): clean up 4

--- 2025/day-07.js
+++ 2025/day-07.js
@@ -2,41 +2,23 @@ import { readFileSync } from 'node:fs';
 
 const input = readFileSync('./day-07-input.txt', 'utf8').trimEnd();
 
-function solve1(input) {
+function solve(input) {
   const lines = input.split('\n');
   let nSplits = 0;
-  let beams = [lines[0].indexOf('S')];
-  for (let i = 1; i < lines.length; i++) {
-    const nextBeams = new Set();
-    for (const beam of beams) {
-      if (lines[i][beam] === '^') {
-        nextBeams.add(beam - 1);
-        nextBeams.add(beam + 1);
-        nSplits++;
-      } else {
-        nextBeams.add(beam);
-      }
-    }
-    beams = Array.from(nextBeams);
-  }
-  console.log(nSplits);
-}
-solve1(input);
-
-function solve2(input) {
-  const lines = input.split('\n');
   let timelines = Array.from({ length: lines[0].length }, () => 0);
   timelines[lines[0].indexOf('S')] = 1;
   for (let i = 1; i < lines.length; i++) {
     for (let j = 0; j < timelines.length; j++) {
       const t = timelines[j];
-      if (lines[i][j] === '^') {
+      if (t && lines[i][j] === '^') {
         timelines[j - 1] += t;
         timelines[j] = 0;
         timelines[j + 1] += t;
+        nSplits++;
       }
     }
   }
+  console.log(nSplits);
   console.log(Object.values(timelines).reduce((a, b) => a + b, 0));
 }
-solve2(input);
+solve(input);
