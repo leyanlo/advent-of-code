9aa0ebcce155534ae6d9136b2a73dc9f4bdb97bd
Leyan Lo <leyanlo@gmail.com>
2025-12-07 21:48:54 -0800

    chore(2025-08): clean up

--- 2025/day-08.js
+++ 2025/day-08.js
@@ -1,26 +1,6 @@
 import { readFileSync } from 'node:fs';
 
-var input = `162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689`;
-var input = readFileSync('./day-08-input.txt', 'utf8').trimEnd();
+const input = readFileSync('./day-08-input.txt', 'utf8').trimEnd();
 
 function getCombos(coords) {
   // return pairs of coords sorted by distance
@@ -37,52 +17,8 @@ function getCombos(coords) {
     }
   }
   combos.sort((a, b) => a.distance - b.distance);
-  console.log(combos);
   return combos;
 }
-
-// function solve(input) {
-//   const coords = input.split('\n').map((line) => line.split(',').map(Number));
-//   const combos = getCombos(coords);
-//   const circuits = coords.reduce((acc, coord, i) => {
-//     acc[`${coord}`] = i;
-//     return acc;
-//   }, {});
-//   const counts = coords.map(() => 1);
-//   for (let i = 0; i <= 1000; i++) {
-//     const combo = combos[i];
-//     const [a, b] = combo.pair;
-//     const keyA = `${a}`;
-//     const keyB = `${b}`;
-//     if (circuits[keyA] && circuits[keyB]) {
-//       if (circuits[keyA] !== circuits[keyB]) {
-//         // merge circuits
-//         const oldId = circuits[keyB];
-//         const newId = circuits[keyA];
-//         for (const key in circuits) {
-//           if (circuits[key] === oldId) {
-//             circuits[key] = newId;
-//           }
-//         }
-//         counts[newId] += counts[oldId];
-//         counts[oldId] = 0;
-//       }
-//       continue; // both already assigned
-//     }
-//     if (circuits[keyA]) {
-//       circuits[keyB] = circuits[keyA];
-//       counts[circuits[keyA]]++;
-//     } else {
-//       circuits[keyA] = circuits[keyB];
-//       counts[circuits[keyB]]++;
-//       counts[circuits[keyA]]--;
-//     }
-//   }
-//   console.log(circuits, counts);
-//   counts.sort((a, b) => b - a);
-//   console.log(counts.slice(0, 3).reduce((acc, n) => acc * n, 1));
-// }
-// solve(input);
 
 function solve(input) {
   const coords = input.split('\n').map((line) => line.split(',').map(Number));
@@ -92,7 +28,17 @@ function solve(input) {
     return acc;
   }, {});
   const counts = coords.map(() => 1);
+  let i = -1;
   for (const combo of combos) {
+    i++;
+    if (i === 1000) {
+      console.log(
+        counts
+          .toSorted((a, b) => b - a)
+          .slice(0, 3)
+          .reduce((acc, n) => acc * n, 1)
+      );
+    }
     const [a, b] = combo.pair;
     const keyA = `${a}`;
     const keyB = `${b}`;
