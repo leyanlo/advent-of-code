e662f5cd019c795a1144005552e19153dc8bb554
Leyan Lo <leyanlo@gmail.com>
2025-12-07 21:57:36 -0800

    chore(2025-08): clean up 2

--- 2025/day-08.js
+++ 2025/day-08.js
@@ -2,36 +2,33 @@ import { readFileSync } from 'node:fs';
 
 const input = readFileSync('./day-08-input.txt', 'utf8').trimEnd();
 
+// get all combos of coordinates sorted ascending by distance
 function getCombos(coords) {
-  // return pairs of coords sorted by distance
   const combos = [];
-  for (let i = 0; i < coords.length; i++) {
+  for (let i = 0; i < coords.length - 1; i++) {
+    const a = coords[i];
     for (let j = i + 1; j < coords.length; j++) {
-      // distance is the square root of the sum of the squares of the differences
+      const b = coords[j];
       const dist = Math.sqrt(
-        (coords[i][0] - coords[j][0]) ** 2 +
-          (coords[i][1] - coords[j][1]) ** 2 +
-          (coords[i][2] - coords[j][2]) ** 2
+        (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
       );
-      combos.push({ pair: [coords[i], coords[j]], distance: dist });
+      combos.push({ coords: [coords[i], coords[j]], dist });
     }
   }
-  combos.sort((a, b) => a.distance - b.distance);
-  return combos;
+  return combos.sort((a, b) => a.dist - b.dist).map((c) => c.coords);
 }
 
 function solve(input) {
   const coords = input.split('\n').map((line) => line.split(',').map(Number));
   const combos = getCombos(coords);
   const circuits = coords.reduce((acc, coord, i) => {
-    acc[`${coord}`] = i;
+    acc[coord] = i;
     return acc;
   }, {});
   const counts = coords.map(() => 1);
-  let i = -1;
-  for (const combo of combos) {
-    i++;
-    if (i === 1000) {
+  let i = 0;
+  for (const [a, b] of combos) {
+    if (i++ === 1000) {
       console.log(
         counts
           .toSorted((a, b) => b - a)
@@ -39,35 +36,21 @@ function solve(input) {
           .reduce((acc, n) => acc * n, 1)
       );
     }
-    const [a, b] = combo.pair;
-    const keyA = `${a}`;
-    const keyB = `${b}`;
-    if (circuits[keyA] && circuits[keyB]) {
-      if (circuits[keyA] !== circuits[keyB]) {
-        // merge circuits
-        const oldId = circuits[keyB];
-        const newId = circuits[keyA];
-        for (const key in circuits) {
-          if (circuits[key] === oldId) {
-            circuits[key] = newId;
-          }
+    if (circuits[a] !== circuits[b]) {
+      // merge circuits
+      const oldId = circuits[b];
+      const newId = circuits[a];
+      for (const key in circuits) {
+        if (circuits[key] === oldId) {
+          circuits[key] = newId;
         }
-        if (counts[oldId] + counts[newId] === coords.length - 1) {
-          console.log(a[0] * b[0]);
-        }
-        counts[newId] += counts[oldId];
-        counts[oldId] = 0;
       }
-      continue; // both already assigned
-    }
-    if (circuits[keyA]) {
-      circuits[keyB] = circuits[keyA];
-      counts[circuits[keyA]]++;
-      counts[circuits[keyB]]--;
-    } else {
-      circuits[keyA] = circuits[keyB];
-      counts[circuits[keyB]]++;
-      counts[circuits[keyA]]--;
+      if (counts[oldId] + counts[newId] === coords.length) {
+        console.log(a[0] * b[0]);
+        break;
+      }
+      counts[newId] += counts[oldId];
+      counts[oldId] = 0;
     }
   }
 }
