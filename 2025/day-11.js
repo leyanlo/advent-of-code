874d1256de44773b90727865b7ac9c436c6f22d9
Leyan Lo <leyanlo@gmail.com>
2025-12-10 22:32:27 -0800

    chore(2025-11): clean up

--- 2025/day-11.js
+++ 2025/day-11.js
@@ -1,29 +1,6 @@
 import { readFileSync } from 'node:fs';
 
-var input = `aaa: you hhh
-you: bbb ccc
-bbb: ddd eee
-ccc: ddd eee fff
-ddd: ggg
-eee: out
-fff: out
-ggg: out
-hhh: ccc fff iii
-iii: out`;
-var input = `svr: aaa bbb
-aaa: fft
-fft: ccc
-bbb: tty
-tty: ccc
-ccc: ddd eee
-ddd: hub
-hub: fff
-eee: dac
-dac: fff
-fff: ggg hhh
-ggg: out
-hhh: out`;
-var input = readFileSync('./day-11-input.txt', 'utf8').trimEnd();
+const input = readFileSync('./day-11-input.txt', 'utf8').trimEnd();
 
 function solve1(input) {
   const lines = input.split('\n');
