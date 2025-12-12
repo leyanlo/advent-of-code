bb7980920f89563686b9ecdfc2c87228bba6631a
Leyan Lo <leyanlo@gmail.com>
2025-12-11 23:31:22 -0800

    chore(2025-12): clean up more

--- 2025/day-12.js
+++ 2025/day-12.js
@@ -3,35 +3,14 @@ import { readFileSync } from 'node:fs';
 const input = readFileSync('./day-12-input.txt', 'utf8').trimEnd();
 
 function solve(input) {
-  const shapes = input
-    .split('\n\n')
-    .slice(0, -1)
-    .map((lines) =>
-      lines
-        .split('\n')
-        .slice(1)
-        .map((line) => line.split('').map((c) => (c === '#' ? 1 : 0)))
-    );
-  const shapeAreas = shapes.map((shape) =>
-    shape.flat().reduce((acc, n) => acc + n, 0)
-  );
-  const regions = input
-    .split('\n\n')
-    .at(-1)
-    .split('\n')
-    .map((line) => {
-      const [w, h, ...shapeCounts] = line.matchAll(/\d+/g).map((m) => +m);
-      return { w, h, shapeCounts };
-    });
+  const regions = input.split('\n\n').at(-1).split('\n');
 
   let count = 0;
-  for (const { w, h, shapeCounts } of regions) {
+  for (const region of regions) {
+    const [w, h, ...shapeCounts] = region.matchAll(/\d+/g).map((m) => +m);
     const regionArea = w * h;
-    const shapesArea = shapeCounts.reduce(
-      (acc, c, i) => acc + shapeAreas[i] * c,
-      0
-    );
-    count += shapesArea <= regionArea;
+    const maxShapeArea = shapeCounts.reduce((acc, c) => acc + 9 * c, 0);
+    count += maxShapeArea <= regionArea;
   }
   console.log(count);
 }
